7b21ab3be45e9259ad9972357d2ca10e7de47d74
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-03-06 01:00:53 -0300

    Delete Scripts directory

--- Scripts/Interactive Activator.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-namespace GuwbaPrimeAdventure.Item.EventItem
-{
-	[DisallowMultipleComponent]
-	internal sealed class InteractiveActivator : Activator, IInteractable
-	{
-		public void Interaction() => this.Activation();
-	};
-};
