fe2330efe0004bfa816a4f08d6c2c89c5039fa04
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-01 19:13:42 -0300

    Update Behavior.cs

--- System/Scripts/Behavior.cs
+++ System/Scripts/Behavior.cs
@@ -11,7 +11,7 @@ namespace GuwbaPrimeAdventure
 	public interface IGrabtable
 	{
 		public void Paralyze();
-		public void Desparalyze();
+		public void Unparalyze();
 	};
 	public interface ICollectable
 	{
