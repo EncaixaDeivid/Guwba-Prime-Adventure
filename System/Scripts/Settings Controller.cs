bcda33e15f7240ee8d83cf3b41f08b4f664c8c44
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-04 11:01:46 -0300

    Update and rename Settings.cs to Settings Controller.cs

---
+++ System/Scripts/Settings Controller.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.IO;
+namespace GuwbaPrimeAdventure
+{
+	public static class SettingsController
+	{
+		private struct Settings
+		{
+			public bool FullScreen;
+			public bool GeneralVolumeToggle;
+			[Range(0f, 1f)] public float GeneralVolume;
+			public bool EffectsVolumeToggle;
+			[Range(0f, 1f)] public float EffectsVolume;
+			public bool MusicVolumeToggle;
+			[Range(0f, 1f)] public float MusicVolume;
+			[Range(0f, .1f)] public float DialogSpeed;
+		}
+		private static readonly string SettingsPath = Application.persistentDataPath + "/Settings.txt";
+		private static Settings LoadSettings()
+		{
+			if (File.Exists(SettingsPath))
+				return ArchiveEncoder.ReadData<Settings>(SettingsPath);
+			return new Settings()
+			{
+				FullScreen = true,
+				GeneralVolumeToggle = true,
+				GeneralVolume = 1f,
+				EffectsVolumeToggle = true,
+				EffectsVolume = 1f,
+				MusicVolumeToggle = true,
+				MusicVolume = 1f,
+				DialogSpeed = .05f
+			};
+		}
+		internal static void SaveSettings()
+		{
+			Settings settings = new()
+			{
+				FullScreen = FullScreen,
+				GeneralVolumeToggle = GeneralVolumeToggle,
+				GeneralVolume = GeneralVolume,
+				EffectsVolumeToggle = EffectsVolumeToggle,
+				EffectsVolume = EffectsVolume,
+				MusicVolumeToggle = MusicVolumeToggle,
+				MusicVolume = MusicVolume,
+				DialogSpeed = DialogSpeed
+			};
+			ArchiveEncoder.WriteData(settings, SettingsPath);
+		}
+		public static bool FullScreen = LoadSettings().FullScreen;
+		public static bool GeneralVolumeToggle = LoadSettings().GeneralVolumeToggle;
+		public static float GeneralVolume = LoadSettings().GeneralVolume;
+		public static bool EffectsVolumeToggle = LoadSettings().EffectsVolumeToggle;
+		public static float EffectsVolume = LoadSettings().EffectsVolume;
+		public static bool MusicVolumeToggle = LoadSettings().MusicVolumeToggle;
+		public static float MusicVolume = LoadSettings().MusicVolume;
+		public static float DialogSpeed = LoadSettings().DialogSpeed;
+	};
+};
