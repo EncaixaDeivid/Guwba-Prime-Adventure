a413ba282e932dfdda06cf74bcd87a6ce7d97006
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-04 11:17:05 -0300

    Update Book.cs

--- Items/Scripts/Book.cs
+++ Items/Scripts/Book.cs
@@ -9,21 +9,21 @@ namespace GuwbaPrimeAdventure.Item
 		private new void Awake()
 		{
 			base.Awake();
-			if (DataFile.Books.ContainsKey(this.gameObject.name))
+			if (SaveController.Books.ContainsKey(this.gameObject.name))
 			{
-				if (DataFile.Books[this.gameObject.name])
+				if (SaveController.Books[this.gameObject.name])
 					this.GetComponent<SpriteRenderer>().sprite = this._bookCacthed;
 				return;
 			}
-			DataFile.Books.Add(this.gameObject.name, false);
+			SaveController.Books.Add(this.gameObject.name, false);
 		}
 		public void Collect()
 		{
-			if (!DataFile.Books[this.gameObject.name])
-				DataFile.Books[this.gameObject.name] = true;
+			if (!SaveController.Books[this.gameObject.name])
+				SaveController.Books[this.gameObject.name] = true;
 			this.GetComponent<SpriteRenderer>().sprite = this._bookCacthed;
 			if (this._saveOnSpecifics)
-				DataFile.GeneralObjects.Add(this.gameObject.name);
+				SaveController.GeneralObjects.Add(this.gameObject.name);
 		}
 	};
 };
