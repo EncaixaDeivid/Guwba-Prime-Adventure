3e1cc42d011e1e2b86fb893b3d562b50483565df
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-07 13:53:52 -0300

    Update Hit Activator.cs

--- Items/Event Items/Scripts/Hit Activator.cs
+++ Items/Event Items/Scripts/Hit Activator.cs
@@ -5,6 +5,7 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 	internal sealed class HitActivator : Activator, IDamageable
 	{
 		[SerializeField] private ushort _biggerDamage;
+		public ushort Health => 0;
 		public bool Damage(ushort damage)
 		{
 			if (damage >= this._biggerDamage)
