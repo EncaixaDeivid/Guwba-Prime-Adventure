4417a5cf5f237fa660f868a8584df34198138207
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-08 16:14:00 -0300

    Update Extra Life.cs

--- Items/Scripts/Extra Life.cs
+++ Items/Scripts/Extra Life.cs
@@ -1,23 +1,27 @@
 using UnityEngine;
+using GuwbaPrimeAdventure.Data;
 namespace GuwbaPrimeAdventure.Item
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(SpriteRenderer), typeof(BoxCollider2D))]
 	internal sealed class ExtraLife : StateController, ICollectable
 	{
+		private SaveFile _saveFile;
 		[SerializeField] private bool _saveOnSpecifics;
 		private new void Awake()
 		{
 			base.Awake();
-			if (SaveController.LifesAcquired.Contains(this.gameObject.name))
+			SaveController.Load(out this._saveFile);
+			if (this._saveFile.lifesAcquired.Contains(this.gameObject.name))
 				Destroy(this.gameObject, 0.001f);
 		}
 		public void Collect()
 		{
-			if (SaveController.Lifes < 99f)
-				SaveController.Lifes += 1;
-			SaveController.LifesAcquired.Add(this.gameObject.name);
-			if (this._saveOnSpecifics)
-				SaveController.GeneralObjects.Add(this.gameObject.name);
+			if (this._saveFile.lifes < 99f)
+				this._saveFile.lifes += 1;
+			this._saveFile.lifesAcquired.Add(this.gameObject.name);
+			if (this._saveOnSpecifics && !this._saveFile.generalObjects.Contains(this.gameObject.name))
+				this._saveFile.generalObjects.Add(this.gameObject.name);
+			SaveController.WriteSave(this._saveFile);
 			Destroy(this.gameObject);
 		}
 	};
