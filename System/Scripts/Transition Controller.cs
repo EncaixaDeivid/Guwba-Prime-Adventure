d6ab41fe4ded2771bc62d8887fae6d6ee9ab5917
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-08 15:24:16 -0300

    Update Transition Controller.cs

--- System/Scripts/Transition Controller.cs
+++ System/Scripts/Transition Controller.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using GuwbaPrimeAdventure.Hud;
+using GuwbaPrimeAdventure.Data;
 namespace GuwbaPrimeAdventure
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform))]
@@ -25,6 +26,7 @@ namespace GuwbaPrimeAdventure
 			this.StartCoroutine(SceneTransicion());
 			IEnumerator SceneTransicion()
 			{
+				SaveController.Load(out SaveFile saveFile);
 				StateController.SetState(false);
 				TransicionHud transicionHud = Instantiate(this._transicionHud);
 				for (float i = 0f; i < 1.1f; i += Time.fixedDeltaTime)
@@ -36,9 +38,9 @@ namespace GuwbaPrimeAdventure
 				string newSceneName = sceneName != "" ? sceneName : this._sceneTransicion;
 				AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(newSceneName, LoadSceneMode.Single);
 				if (newSceneName != this.gameObject.scene.name)
-					for (ushort i = 0; i < SaveController.LevelsCompleted.Length; i++)
+					for (ushort i = 0; i < saveFile.levelsCompleted.Length; i++)
 						if (newSceneName.Contains($"{i}"))
-							SaveController.InternalLastLevelEntered = newSceneName;
+							saveFile.lastLevelEntered = newSceneName;
 				asyncOperation.allowSceneActivation = true;
 			}
 		}
