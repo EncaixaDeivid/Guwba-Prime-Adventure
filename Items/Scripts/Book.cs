98a2805d923e8d227a13be50ae2fc8ec7fe5e198
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-08 16:15:35 -0300

    Update Book.cs

--- Items/Scripts/Book.cs
+++ Items/Scripts/Book.cs
@@ -1,29 +1,33 @@
 using UnityEngine;
+using GuwbaPrimeAdventure.Data;
 namespace GuwbaPrimeAdventure.Item
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(SpriteRenderer), typeof(BoxCollider2D))]
 	internal sealed class Book : StateController, ICollectable
 	{
+		private SaveFile _saveFile;
 		[SerializeField] private Sprite _bookCacthed;
 		[SerializeField] private bool _saveOnSpecifics;
 		private new void Awake()
 		{
 			base.Awake();
-			if (SaveController.Books.ContainsKey(this.gameObject.name))
+			SaveController.Load(out this._saveFile);
+			if (this._saveFile.books.ContainsKey(this.gameObject.name))
 			{
-				if (SaveController.Books[this.gameObject.name])
+				if (this._saveFile.books[this.gameObject.name])
 					this.GetComponent<SpriteRenderer>().sprite = this._bookCacthed;
 				return;
 			}
-			SaveController.Books.Add(this.gameObject.name, false);
+			this._saveFile.books.Add(this.gameObject.name, false);
 		}
 		public void Collect()
 		{
-			if (!SaveController.Books[this.gameObject.name])
-				SaveController.Books[this.gameObject.name] = true;
+			if (!this._saveFile.books[this.gameObject.name])
+				this._saveFile.books[this.gameObject.name] = true;
 			this.GetComponent<SpriteRenderer>().sprite = this._bookCacthed;
-			if (this._saveOnSpecifics)
-				SaveController.GeneralObjects.Add(this.gameObject.name);
+			if (this._saveOnSpecifics && !this._saveFile.generalObjects.Contains(this.gameObject.name))
+				this._saveFile.generalObjects.Add(this.gameObject.name);
+			SaveController.WriteSave(this._saveFile);
 		}
 	};
 };
