4bf1b8154e31b4420bf945b26763fedc67f727ef
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-09 13:04:35 -0300

    Update Visual Guwba.cs

--- Guwba/Scripts/Visual Guwba.cs
+++ Guwba/Scripts/Visual Guwba.cs
@@ -98,7 +98,10 @@ namespace GuwbaPrimeAdventure.Guwba
 			this._invencibility = true;
 			this._vitality -= (short)damage;
 			for (ushort i = (ushort)this._guwbaHud.Vitality.Length; i > (this._vitality >= 0f ? this._vitality : 0f); i--)
-				this._guwbaHud.Vitality[i - 1].style.backgroundColor = new StyleColor(Color.);
+			{
+				this._guwbaHud.Vitality[i - 1].style.backgroundColor = new StyleColor(Color.black);
+				this._guwbaHud.Vitality[i - 1].style.opacity = 0.25f;
+			}
 			if (this._vitality <= 0f)
 			{
 				this._vitality = 0;
