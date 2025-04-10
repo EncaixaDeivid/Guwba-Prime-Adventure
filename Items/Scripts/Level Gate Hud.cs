a7e3dc635cb0c6bd6e70ca2ed8d1c627fa8bfe43
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-10 16:54:32 -0300

    Update Level Gate Hud.cs

--- Items/Scripts/Level Gate Hud.cs
+++ Items/Scripts/Level Gate Hud.cs
@@ -6,11 +6,11 @@ namespace GuwbaPrimeAdventure.Item
 	internal sealed class LevelGateHud : MonoBehaviour
 	{
 		private static LevelGateHud _instance;
-		private GroupBox _baseElement;
+		private VisualElement _rootElement;
 		private Button _level, _boss;
 		private Label _lifeText, _coinText;
-		[SerializeField] private string _baseElementGroup, _levelButton, _bossButton, _lifeLabel, _coinLabel;
-		internal GroupBox BaseElement => this._baseElement;
+		[SerializeField] private string _rootElementGroup, _levelButton, _bossButton, _lifeLabel, _coinLabel;
+		internal VisualElement RootElement => this._rootElement;
 		internal Button Level => this._level;
 		internal Button Boss => this._boss;
 		internal Label Life => this._lifeText;
@@ -20,12 +20,12 @@ namespace GuwbaPrimeAdventure.Item
 			if (_instance)
 				Destroy(_instance.gameObject);
 			_instance = this;
-			UIDocument hudDocument = this.GetComponent<UIDocument>();
-			this._baseElement = hudDocument.rootVisualElement.Q<GroupBox>(this._baseElementGroup);
-			this._level = hudDocument.rootVisualElement.Q<Button>(this._levelButton);
-			this._boss = hudDocument.rootVisualElement.Q<Button>(this._bossButton);
-			this._lifeText = hudDocument.rootVisualElement.Q<Label>(this._lifeLabel);
-			this._coinText = hudDocument.rootVisualElement.Q<Label>(this._coinLabel);
+			VisualElement root = this.GetComponent<UIDocument>().rootVisualElement;
+			this._rootElement = root.Q<VisualElement>(this._rootElementGroup);
+			this._level = root.Q<Button>(this._levelButton);
+			this._boss = root.Q<Button>(this._bossButton);
+			this._lifeText = root.Q<Label>(this._lifeLabel);
+			this._coinText = root.Q<Label>(this._coinLabel);
 		}
 	};
 };
