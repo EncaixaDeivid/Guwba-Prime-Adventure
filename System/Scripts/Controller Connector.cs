5d876ba1f65f8fdc485afc782f31b2085221149d
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-10 12:46:21 -0300

    Update Controller Connector.cs

--- System/Scripts/Controller Connector.cs
+++ System/Scripts/Controller Connector.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 namespace GuwbaPrimeAdventure
 {
-	internal abstract class ControllerConnector : MonoBehaviour
+	public abstract class ControllerConnector : MonoBehaviour
 	{
+		protected void Awake<ControllerInstance>() where ControllerInstance : ControllerConnector
+		{
+			ControllerConnector instance = this.GetComponent<ControllerInstance>();
+			if (!instance || instance is not MenuController && instance is not ConfigurationController && instance is not DeathScreenController)
+				Destroy(this.gameObject, 0.001f);
+		}
 		protected void Connect<Controller>() where Controller : ControllerConnector
 		{
 			Controller controller = this.GetComponent<Controller>();
