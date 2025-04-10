e2ea79f5d2a9656eea462ed976fe6a4ba7ee07fa
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-10 14:15:10 -0300

    Update Death Screen Controller.cs

--- System/Scripts/Death Screen Controller.cs
+++ System/Scripts/Death Screen Controller.cs
@@ -32,7 +32,22 @@ namespace GuwbaPrimeAdventure
 				this._deathScreenHud.GameOver.clicked -= this.GameOver;
 			}
 		}
-		protected override void Event() { }
+		protected override void Event()
+		{
+			SaveController.Load(out SaveFile saveFile);
+			this.Connect<ConfigurationController>();
+			this._deathScreenHud = Instantiate(this._deathScreenHudObject, this.transform);
+			this._deathScreenHud.Continue.clicked += this.Continue;
+			this._deathScreenHud.OutLevel.clicked += this.OutLevel;
+			this._deathScreenHud.GameOver.clicked += this.GameOver;
+			if (saveFile.lifes < 0f)
+			{
+				this._deathScreenHud.Text.text = "Fim de Jogo";
+				this._deathScreenHud.Continue.style.display = DisplayStyle.None;
+				this._deathScreenHud.OutLevel.style.display = DisplayStyle.None;
+				this._deathScreenHud.GameOver.style.display = DisplayStyle.Flex;
+			}
+		}
 		private Action Continue => () => this.GetComponent<TransitionController>().Transicion(this.gameObject.scene.name);
 		private Action OutLevel => () => this.GetComponent<TransitionController>().Transicion();
 		private Action GameOver => () =>
@@ -40,21 +55,6 @@ namespace GuwbaPrimeAdventure
 			SaveController.RefreshData();
 			this.GetComponent<TransitionController>().Transicion();
 		};
-		public static void Death()
-		{
-			SaveController.Load(out SaveFile saveFile);
-			_instance.Connect<ConfigurationController>();
-			_instance._deathScreenHud = Instantiate(_instance._deathScreenHudObject, _instance.transform);
-			_instance._deathScreenHud.Continue.clicked += _instance.Continue;
-			_instance._deathScreenHud.OutLevel.clicked += _instance.OutLevel;
-			_instance._deathScreenHud.GameOver.clicked += _instance.GameOver;
-			if (saveFile.lifes < 0f)
-			{
-				_instance._deathScreenHud.Text.text = "Fim de Jogo";
-				_instance._deathScreenHud.Continue.style.display = DisplayStyle.None;
-				_instance._deathScreenHud.OutLevel.style.display = DisplayStyle.None;
-				_instance._deathScreenHud.GameOver.style.display = DisplayStyle.Flex;
-			}
-		}
+		public static void Death() => _instance.Event();
 	};
 };
