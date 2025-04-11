490b4227b125d00fc58fd7c5faea805dbe6b3fed
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-11 13:54:44 -0300

    Create Connector.cs

---
+++ System/Object Connection/Connector.cs
@@ -0,0 +1,24 @@
+namespace GuwbaPrimeAdventure.Connection
+{
+	public enum ConnectionObject
+	{
+		All,
+		Enemy,
+		Boss,
+		Item,
+		EventItem,
+		Hud,
+		Controller
+	};
+	public enum ConnectionState
+	{
+		None,
+		Enable,
+		Disable
+	};
+	public interface IConnector
+	{
+		public ConnectionObject ConnectionObject { get; }
+		public void Receive(DataConnection data);
+	};
+};
