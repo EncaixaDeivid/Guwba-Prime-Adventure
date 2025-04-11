36b78b30733522635a56f80e8f81c95674c376f1
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-11 10:36:24 -0300

    Update Command Guwba.cs

--- Guwba/Scripts/Command Guwba.cs
+++ Guwba/Scripts/Command Guwba.cs
@@ -102,12 +102,14 @@ namespace GuwbaPrimeAdventure.Guwba
 				this._animator.SetBool(this._death, isDead);
 				this._rigidbody.gravityScale = this._gravityScale;
 				this._collider.size = new Vector2(this._collider.size.x, this._deadSize);
+				Sender.Create().SetObjectType(ObjectType.Controller).SetWaitState(WaitState.Enable).SetToggle(true).Send();
 			}
 			else
 			{
 				this.OnEnable();
 				this._animator.SetBool(this._death, isDead);
 				this._collider.size = new Vector2(this._collider.size.x, this._normalSize);
+				Sender.Create().SetObjectType(ObjectType.Controller).SetWaitState(WaitState.Disable).SetToggle(true).Send();
 			}
 		};
 		private Action<InputAction.CallbackContext> Movement => (InputAction.CallbackContext movementAction) =>
