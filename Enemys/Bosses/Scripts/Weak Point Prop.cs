e7ae41fd2532107de4f04878e6d5d65aa3a5fc41
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-14 13:01:53 -0300

    Update Weak Point Prop.cs

--- Enemys/Bosses/Scripts/Weak Point Prop.cs
+++ Enemys/Bosses/Scripts/Weak Point Prop.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using GuwbaPrimeAdventure.Connection;
 namespace GuwbaPrimeAdventure.Enemy.Boss
 {
 	[DisallowMultipleComponent]
@@ -9,7 +10,7 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 		[Header("Weak Point"), SerializeField] private short _vitality;
 		[SerializeField] private ushort _biggerDamage;
 		[SerializeField] private float _timeToDamage;
-  		public ushort Health => (ushort)this._vitality;
+		public ushort Health => (ushort)this._vitality;
 		public bool Damage(ushort damage)
 		{
 			if (this._blockDamage)
@@ -25,17 +26,11 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 				}
 				this._vitality -= (short)damage;
 				if (this._indexReact)
-					if (this._multipleReact)
-						foreach (BossController bossController in this._bossesControllers)
-							bossController.Index(this._indexEvent);
-					else
-						this._bossesControllers[this._bossIndex].Index(this._indexEvent);
-				else if (!this._indexReact)
-					if (this._multipleReact)
-						foreach (BossController bossController in this._bossesControllers)
-							bossController.ReactToDamage();
-					else
-						this._bossesControllers[this._bossIndex].ReactToDamage();
+					Sender.Create().SetConnectionObject(ConnectionObject.Boss).SetConnectionState(ConnectionState.Action)
+						.SetBossType(BossType.All).SetIndex(this._indexEvent).Send();
+				else
+					Sender.Create().SetConnectionObject(ConnectionObject.Boss).SetConnectionState(ConnectionState.Action)
+						.SetBossType(BossType.All).Send();
 				if (this._vitality <= 0)
 				{
 					this._useDestructuion = true;
