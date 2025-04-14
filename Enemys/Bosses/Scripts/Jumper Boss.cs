abe1285d0266ccefdf33ea5dc6a926c7736027f1
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-14 13:00:58 -0300

    Update Jumper Boss.cs

--- Enemys/Bosses/Scripts/Jumper Boss.cs
+++ Enemys/Bosses/Scripts/Jumper Boss.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using GuwbaPrimeAdventure.Connection;
 using GuwbaPrimeAdventure.Guwba;
 namespace GuwbaPrimeAdventure.Enemy.Boss
 {
@@ -28,22 +29,52 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 			_sequentialFallIndex,
 			_useTarget,
 			_randomFollow;
-		private new void Awake() // Set Jump Points
+		private void HighJump(Vector2 otherTarget, bool useTarget)
+		{
+			this.StartCoroutine(FollowTarget());
+			IEnumerator FollowTarget()
+			{
+				yield return new WaitUntil(() => !this.SurfacePerception() && this.enabled);
+				this._usedHigh = true;
+				if (this._randomJumpIndex && !this._sequentialJumpIndex)
+					this._jumpIndex = (ushort)Random.Range(0, this._summonIndexJump.Length - 1);
+				if (this._summonOnJump && (this._bothJump || this._justHighJump))
+					Sender.Create().SetConnectionObject(ConnectionObject.Boss).SetConnectionState(ConnectionState.Action)
+						.SetBossType(BossType.Summoner).SetIndex(this._summonIndexJump[this._jumpIndex]).Send();
+				if (this._sequentialJumpIndex && !this._randomJumpIndex)
+					this._jumpIndex = (ushort)(this._jumpIndex < this._summonIndexJump.Length - 1f ? this._jumpIndex + 1f : 0f);
+				Sender.Create().SetConnectionObject(ConnectionObject.Boss).SetConnectionState(ConnectionState.Action)
+					.SetBossType(BossType.Runner).SetToggle(false).Send();
+				this._rigidybody.linearVelocityX = 0f;
+				float randomDirection = 0f;
+				if (this._randomFollow)
+					randomDirection = Random.Range(-1f, 1f);
+				while (!this.SurfacePerception())
+				{
+					float targetPosition = GuwbaTransformer<CommandGuwba>.Position.x;
+					if (useTarget)
+						targetPosition = otherTarget.x;
+					if (this._randomFollow)
+					{
+						if (randomDirection >= 0f)
+							targetPosition = GuwbaTransformer<CommandGuwba>.Position.x;
+						else if (randomDirection < 0f)
+							targetPosition = otherTarget.x;
+					}
+					float targetDirection = targetPosition - this.transform.position.x;
+					this._movementSide = (short)(targetDirection > 0f ? 1f : -1f);
+					if (this.enabled)
+						this._rigidybody.linearVelocityX = this._movementSide * this._followSpeed;
+					else
+						this._rigidybody.linearVelocityX = 0f;
+					yield return new WaitForFixedUpdate();
+					yield return new WaitUntil(() => this.enabled);
+				}
+			}
+		}
+		private new void Awake()
 		{
 			base.Awake();
-			this._toggleEvent = (bool toggleValue) => this._stopJump = !toggleValue;
-			this._reactToDamageEvent = () =>
-			{
-				if (this._stopMoveReact)
-				{
-					this.Toggle<RunnerBoss>(false);
-					this._rigidybody.linearVelocityX = 0f;
-				}
-				this._collider.isTrigger = true;
-				this._rigidybody.AddForceY(this._strenghtReact);
-				if (this._highReact)
-					HighJump(this._otherTarget, this._useTarget);
-			};
 			for (ushort i = 0; i < this._jumpPointStructures.Length; i++)
 			{
 				JumpPoint jumpPoint = this._jumpPointStructures[i].JumpPointObject;
@@ -62,65 +93,26 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 								if (this._randomJumpIndex && !this._sequentialJumpIndex)
 									this._jumpIndex = (ushort)Random.Range(0, this._summonIndexJump.Length - 1);
 								if (this._summonOnJump && !this._justHighJump)
-									this.Index<SummonerBoss>(this._summonIndexJump[this._jumpIndex]);
+									Sender.Create().SetConnectionObject(ConnectionObject.Boss).SetConnectionState(ConnectionState.Action)
+									.SetBossType(BossType.Summoner).SetIndex(this._summonIndexJump[this._jumpIndex]).Send();
 								if (this._sequentialJumpIndex && !this._randomJumpIndex)
 									this._jumpIndex = (ushort)(this._jumpIndex < this._summonIndexJump.Length - 1f ? this._jumpIndex + 1f : 0f);
 								if (this._jumpPointStructures[index].StopMove)
 								{
-									this.Toggle<RunnerBoss>(false);
+									Sender.Create().SetConnectionObject(ConnectionObject.Boss).SetConnectionState(ConnectionState.Action)
+									.SetBossType(BossType.Runner).SetToggle(false).Send();
 									this._rigidybody.linearVelocityX = 0f;
 								}
 								this._rigidybody.AddForceY(this._jumpPointStructures[index].Strength);
 								if (this._jumpPointStructures[index].High)
-									HighJump(this._jumpPointStructures[index].OtherTarget, this._jumpPointStructures[index].UseTarget);
+									this.HighJump(this._jumpPointStructures[index].OtherTarget, this._jumpPointStructures[index].UseTarget);
 								this._jumpPointStructures[index].RemovalJumpCount = (short)this._jumpPointStructures[index].JumpCount;
 							}
 					}
 				});
 			}
-			void HighJump(Vector2 otherTarget, bool useTarget)
-			{
-				this.StartCoroutine(FollowTarget());
-				IEnumerator FollowTarget()
-				{
-					yield return new WaitUntil(() => !this.SurfacePerception() && this.enabled);
-					this._usedHigh = true;
-					if (this._randomJumpIndex && !this._sequentialJumpIndex)
-						this._jumpIndex = (ushort)Random.Range(0, this._summonIndexJump.Length - 1);
-					if (this._summonOnJump && (this._bothJump || this._justHighJump))
-						this.Index<SummonerBoss>(this._summonIndexJump[this._jumpIndex]);
-					if (this._sequentialJumpIndex && !this._randomJumpIndex)
-						this._jumpIndex = (ushort)(this._jumpIndex < this._summonIndexJump.Length - 1f ? this._jumpIndex + 1f : 0f);
-					this.Toggle<RunnerBoss>(false);
-					this._rigidybody.linearVelocityX = 0f;
-					float randomDirection = 0f;
-					if (this._randomFollow)
-						randomDirection = Random.Range(-1f, 1f);
-					while (!this.SurfacePerception())
-					{
-						float targetPosition = GuwbaTransformer<CommandGuwba>.Position.x;
-						if (useTarget)
-							targetPosition = otherTarget.x;
-						if (this._randomFollow)
-						{
-							if (randomDirection >= 0f)
-								targetPosition = GuwbaTransformer<CommandGuwba>.Position.x;
-							else if (randomDirection < 0f)
-								targetPosition = otherTarget.x;
-						}
-						float targetDirection = targetPosition - this.transform.position.x;
-						this._movementSide = (short)(targetDirection > 0f ? 1f : -1f);
-						if (this.enabled)
-							this._rigidybody.linearVelocityX = this._movementSide * this._followSpeed;
-						else
-							this._rigidybody.linearVelocityX = 0f;
-						yield return new WaitForFixedUpdate();
-						yield return new WaitUntil(() => this.enabled);
-					}
-				}
-			}
 		}
-		private new void FixedUpdate() // Fall Summon
+		private new void FixedUpdate()
 		{
 			if (this._jumped && this.SurfacePerception())
 			{
@@ -129,7 +121,8 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 				{
 					if (this._randomFallIndex && !this._sequentialFallIndex)
 						this._fallIndex = (ushort)Random.Range(0, this._summonIndexFall.Length - 1);
-					this.Index<SummonerBoss>(this._summonIndexFall[this._fallIndex]);
+					Sender.Create().SetConnectionObject(ConnectionObject.Boss).SetConnectionState(ConnectionState.Action)
+						.SetBossType(BossType.Summoner).SetIndex(this._summonIndexFall[this._fallIndex]).Send();
 					if (this._sequentialFallIndex && !this._randomFallIndex)
 						this._fallIndex = (ushort)(this._fallIndex < this._summonIndexFall.Length - 1f ? this._fallIndex + 1f : 0f);
 				}
@@ -141,10 +134,32 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 				{
 					if (this._randomFallIndex && !this._sequentialFallIndex)
 						this._fallIndex = (ushort)Random.Range(0, this._summonIndexFall.Length - 1);
-					this.Index<SummonerBoss>(this._summonIndexFall[this._fallIndex]);
+					Sender.Create().SetConnectionObject(ConnectionObject.Boss).SetConnectionState(ConnectionState.Action)
+						.SetBossType(BossType.Summoner).SetIndex(this._summonIndexFall[this._fallIndex]).Send();
 					if (this._sequentialFallIndex && !this._randomFallIndex)
 						this._fallIndex = (ushort)(this._fallIndex < this._summonIndexFall.Length - 1f ? this._fallIndex + 1f : 0f);
 				}
+			}
+		}
+		public new void Receive(DataConnection data)
+		{
+			base.Receive(data);
+			if (!data.BossType.HasFlag(BossType.Runner) || data.BossType.HasFlag(BossType.None))
+				return;
+			if (data.ConnectionState.HasFlag(ConnectionState.Action) && data.ToggleValue.HasValue && this._hasToggle)
+				this._stopJump = data.ToggleValue.Value;
+			else if (data.ConnectionState.HasFlag(ConnectionState.Action) && this._reactToDamage)
+			{
+				if (this._stopMoveReact)
+				{
+					Sender.Create().SetConnectionObject(ConnectionObject.Boss).SetConnectionState(ConnectionState.Action)
+					.SetBossType(BossType.Runner).SetToggle(false).Send();
+					this._rigidybody.linearVelocityX = 0f;
+				}
+				this._collider.isTrigger = true;
+				this._rigidybody.AddForceY(this._strenghtReact);
+				if (this._highReact)
+					this.HighJump(this._otherTarget, this._useTarget);
 			}
 		}
 		[System.Serializable]
