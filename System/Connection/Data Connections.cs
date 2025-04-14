7fa6586f6b141e985cbe5b032f7ab38443eae085
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-04-14 11:03:34 -0300

    Update Data Connections.cs

--- System/Connection/Data Connections.cs
+++ System/Connection/Data Connections.cs
@@ -3,11 +3,13 @@ namespace GuwbaPrimeAdventure.Connection
 	public struct DataConnection
 	{
 		public ConnectionState ConnectionState { get; private set; }
+		public BossType BossType { get; private set; }
 		public bool? ToggleValue { get; private set; }
 		public uint? IndexValue { get; private set; }
-		internal DataConnection(ConnectionState connectionState, bool? toggleValue, uint? indexValue)
+		internal DataConnection(ConnectionState connectionState, BossType bossType, bool? toggleValue, uint? indexValue)
 		{
 			this.ConnectionState = connectionState;
+			this.BossType = bossType;
 			this.ToggleValue = toggleValue;
 			this.IndexValue = indexValue;
 		}
