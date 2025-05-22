f31786e999507ee7923a5f87aa1d47bb349c9022
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-05-22 16:12:06 -0300

    Update Guwba Hud.cs

--- Guwba/Scripts/Guwba Hud.cs
+++ Guwba/Scripts/Guwba Hud.cs
@@ -7,15 +7,17 @@ namespace GuwbaPrimeAdventure.Guwba
 	{
 		private static GuwbaHud _instance;
 		private VisualElement _rootElement;
-		private VisualElement[] _vitality;
 		private Label _lifeText;
 		private Label _coinText;
 		[SerializeField] private string _rootElementObject;
 		[SerializeField] private string _vitalityVisual;
+		[SerializeField] private string _vitalityPieceVisual;
 		[SerializeField] private string _lifeTextObject;
 		[SerializeField] private string _coinTextObject;
+		[SerializeField] private ushort _totalWidth;
+		[SerializeField] private ushort _vitality;
 		internal VisualElement RootElement => this._rootElement;
-		internal VisualElement[] Vitality => this._vitality;
+		internal VisualElement[] Vitality { get; private set; }
 		internal Label LifeText => this._lifeText;
 		internal Label CoinText => this._coinText;
 		private void Awake()
@@ -28,11 +30,20 @@ namespace GuwbaPrimeAdventure.Guwba
 			_instance = this;
 			VisualElement root = this.GetComponent<UIDocument>().rootVisualElement;
 			this._rootElement = root.Q<VisualElement>(this._rootElementObject);
-			this._vitality = new VisualElement[4];
-			for (ushort i = 1; i <= this._vitality.Length; i++)
-				this._vitality[i - 1] = root.Q<VisualElement>($"{this._vitalityVisual}{i}");
 			this._lifeText = root.Q<Label>(this._lifeTextObject);
 			this._coinText = root.Q<Label>(this._coinTextObject);
+			VisualElement vitality = root.Q<VisualElement>($"{this._vitalityVisual}");
+			vitality.style.width = new StyleLength(new Length(this._totalWidth, LengthUnit.Pixel));
+			VisualElement vitalityPiece = root.Q<VisualElement>($"{this._vitalityPieceVisual}");
+			this.Vitality = new VisualElement[this._vitality];
+			for (ushort i = 0; i < this._vitality; i++)
+			{
+				VisualElement vitalityPieceClone = new() { name = vitalityPiece.name };
+				vitality.Add(vitalityPieceClone);
+				vitality[i + 1].style.width = new StyleLength(new Length(this._totalWidth / this._vitality, LengthUnit.Pixel));
+				this.Vitality[i] = vitality[i + 1];
+			}
+			vitality.Remove(vitalityPiece);
 		}
 	};
 };
