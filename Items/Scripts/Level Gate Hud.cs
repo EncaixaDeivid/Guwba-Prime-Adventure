c042329264a85568181e055fc7d9bd421adfea38
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-05-27 20:54:20 -0300

    Update Level Gate Hud.cs

--- Items/Scripts/Level Gate Hud.cs
+++ Items/Scripts/Level Gate Hud.cs
@@ -6,26 +6,30 @@ namespace GuwbaPrimeAdventure.Item
 	internal sealed class LevelGateHud : MonoBehaviour
 	{
 		private static LevelGateHud _instance;
-		private VisualElement _rootElement;
-		private Button _level, _boss;
-		private Label _lifeText, _coinText;
-		[SerializeField] private string _rootElementGroup, _levelButton, _bossButton, _lifeLabel, _coinLabel;
-		internal VisualElement RootElement => this._rootElement;
-		internal Button Level => this._level;
-		internal Button Boss => this._boss;
-		internal Label Life => this._lifeText;
-		internal Label Coin => this._coinText;
+		[SerializeField] private string _rootElementGroup;
+		[SerializeField] private string _levelButton;
+		[SerializeField] private string _bossButton;
+		[SerializeField] private string _scenesButton;
+		[SerializeField] private string _lifeLabel;
+		[SerializeField] private string _coinLabel;
+		internal VisualElement RootElement { get; private set; }
+		internal Button Level { get; private set; }
+		internal Button Boss { get; private set; }
+		internal Button Scenes { get; private set; }
+		internal Label Life { get; private set; }
+		internal Label Coin { get; private set; }
 		private void Awake()
 		{
 			if (_instance)
 				Destroy(_instance.gameObject);
 			_instance = this;
 			VisualElement root = this.GetComponent<UIDocument>().rootVisualElement;
-			this._rootElement = root.Q<VisualElement>(this._rootElementGroup);
-			this._level = root.Q<Button>(this._levelButton);
-			this._boss = root.Q<Button>(this._bossButton);
-			this._lifeText = root.Q<Label>(this._lifeLabel);
-			this._coinText = root.Q<Label>(this._coinLabel);
+			this.RootElement = root.Q<VisualElement>(this._rootElementGroup);
+			this.Level = root.Q<Button>(this._levelButton);
+			this.Boss = root.Q<Button>(this._bossButton);
+			this.Scenes = root.Q<Button>(this._scenesButton);
+			this.Life = root.Q<Label>(this._lifeLabel);
+			this.Coin = root.Q<Label>(this._coinLabel);
 		}
 	};
 };
