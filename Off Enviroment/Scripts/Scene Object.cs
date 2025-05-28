a7b26f9d2be9fb32d2f6b6f2ca52eb20beb385e2
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-05-27 23:44:17 -0300

    Update Scene Object.cs

--- Off Enviroment/Scripts/Scene Object.cs
+++ Off Enviroment/Scripts/Scene Object.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using System;
 namespace GuwbaPrimeAdventure.OffEnviroment
 {
 	[CreateAssetMenu(fileName = "Scene", menuName = "Scriptable Objects/Scene", order = 2)]
 	internal sealed class SceneObject : ScriptableObject
 	{
-		[SerializeField] private (Texture2D, bool)[] _backgroundImages;
+		[SerializeField] private BackgroundImage[] _backgroundImages;
 		[SerializeField] private float _timeToDesapear = 0f;
-		internal (Texture2D image, bool offDialog)[] BackgroundImages => this._backgroundImages;
+		internal BackgroundImage[] BackgroundImages => this._backgroundImages;
 		internal float TimeToDesapear => this._timeToDesapear;
+		[Serializable]
+		internal struct BackgroundImage
+		{
+			[SerializeField] private Texture2D _image;
+			[SerializeField] private bool _offDialog;
+			internal readonly Texture2D Image => this._image;
+			internal readonly bool OffDialog => this._offDialog;
+		};
 	};
 };
