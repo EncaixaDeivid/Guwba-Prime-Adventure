53369f1d4a39fbaafb7687f9cc5c2e2018e5c278
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-05-27 23:32:44 -0300

    Update Story Scene.cs

--- Off Enviroment/Scripts/Story Scene.cs
+++ Off Enviroment/Scripts/Story Scene.cs
@@ -10,31 +10,33 @@ namespace GuwbaPrimeAdventure.OffEnviroment
 		private ushort _imageIndex = 0;
 		[SerializeField] private StorySceneHud _storySceneHudObject;
 		[SerializeField] private SceneObject _sceneObject;
-		[SerializeField] private float _fadeSpeed;
-		private IEnumerator FadeImage(short wayToGo)
+		private IEnumerator FadeImage(bool appear)
 		{
-			wayToGo = (short)(wayToGo > 0 ? 1 : -1);
-			for (float i = 0f; i <= 1f; i += this._fadeSpeed * Time.fixedDeltaTime)
-				yield return this._storySceneHud.SceneImage.style.opacity = i * wayToGo;
+			if (appear)
+				for (float i = 0f; i <= 1f; i += 0.1f)
+					yield return this._storySceneHud.SceneImage.style.opacity = i;
+			else
+				for (float i = 1f; i >= 0f; i -= 0.1f)
+					yield return this._storySceneHud.SceneImage.style.opacity = i;
 		}
 		internal void ShowScene()
 		{
 			this._storySceneHud = Instantiate(this._storySceneHudObject, this.transform);
 			Texture2D texture = this._sceneObject.BackgroundImages[this._imageIndex].image;
 			this._storySceneHud.SceneImage.style.backgroundImage = Background.FromTexture2D(texture);
-			this.StartCoroutine(this.FadeImage(1));
+			this.StartCoroutine(this.FadeImage(true));
 		}
 		internal IEnumerator NextSlide()
 		{
-			yield return this.FadeImage(0);
+			yield return this.FadeImage(false);
 			this._imageIndex = (ushort)(this._imageIndex < this._sceneObject.BackgroundImages.Length - 1 ? this._imageIndex + 1 : 0);
-			yield return this.FadeImage(1);
+			yield return this.FadeImage(true);
 			if (this._sceneObject.TimeToDesapear > 0f)
 			{
 				yield return new WaitForSeconds(this._sceneObject.TimeToDesapear);
-				yield return this.FadeImage(0);
+				yield return this.FadeImage(false);
 				this._imageIndex = (ushort)(this._imageIndex < this._sceneObject.BackgroundImages.Length - 1 ? this._imageIndex + 1 : 0);
-				yield return this.FadeImage(1);
+				yield return this.FadeImage(true);
 			}
 		}
 		internal void CloseScene() => this.StartCoroutine(this.FadeImage(0));
