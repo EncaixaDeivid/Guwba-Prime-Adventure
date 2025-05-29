74925e1652f8396088b985f053ab838b7e557293
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-05-29 09:32:38 -0300

    Update Guwba Hud.cs

--- Guwba/Scripts/Guwba Hud.cs
+++ Guwba/Scripts/Guwba Hud.cs
@@ -6,9 +6,6 @@ namespace GuwbaPrimeAdventure.Guwba
 	internal sealed class GuwbaHud : MonoBehaviour
 	{
 		private static GuwbaHud _instance;
-		private VisualElement _rootElement;
-		private Label _lifeText;
-		private Label _coinText;
 		[SerializeField] private string _rootElementObject;
 		[SerializeField] private string _vitalityVisual;
 		[SerializeField] private string _vitalityPieceVisual;
@@ -16,10 +13,10 @@ namespace GuwbaPrimeAdventure.Guwba
 		[SerializeField] private string _coinTextObject;
 		[SerializeField] private ushort _totalWidth;
 		[SerializeField] private ushort _vitality;
-		internal VisualElement RootElement => this._rootElement;
+		internal VisualElement RootElement { get; private set; }
 		internal VisualElement[] Vitality { get; private set; }
-		internal Label LifeText => this._lifeText;
-		internal Label CoinText => this._coinText;
+		internal Label LifeText { get; private set; }
+		internal Label CoinText { get; private set; }
 		private void Awake()
 		{
 			if (_instance)
@@ -29,9 +26,9 @@ namespace GuwbaPrimeAdventure.Guwba
 			}
 			_instance = this;
 			VisualElement root = this.GetComponent<UIDocument>().rootVisualElement;
-			this._rootElement = root.Q<VisualElement>(this._rootElementObject);
-			this._lifeText = root.Q<Label>(this._lifeTextObject);
-			this._coinText = root.Q<Label>(this._coinTextObject);
+			this.RootElement = root.Q<VisualElement>(this._rootElementObject);
+			this.LifeText = root.Q<Label>(this._lifeTextObject);
+			this.CoinText = root.Q<Label>(this._coinTextObject);
 			VisualElement vitality = root.Q<VisualElement>($"{this._vitalityVisual}");
 			vitality.style.width = new StyleLength(new Length(this._totalWidth, LengthUnit.Pixel));
 			VisualElement vitalityPiece = root.Q<VisualElement>($"{this._vitalityPieceVisual}");
