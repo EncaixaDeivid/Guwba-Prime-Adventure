72fe74ca3eb804f0047d5f63c4042c8755d7727e
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-05-29 09:35:00 -0300

    Update Death Screen Hud.cs

--- System/Huds/Scripts/Death Screen Hud.cs
+++ System/Huds/Scripts/Death Screen Hud.cs
@@ -6,18 +6,14 @@ namespace GuwbaPrimeAdventure.Hud
 	internal sealed class DeathScreenHud : MonoBehaviour
 	{
 		private static DeathScreenHud _instance;
-		private Label _text;
-		private Button _continue;
-		private Button _outLevel;
-		private Button _gameOver;
 		[SerializeField] private string _textLabel;
 		[SerializeField] private string _continueButton;
 		[SerializeField] private string _outLevelButton;
 		[SerializeField] private string _gameOverButton;
-		public Label Text => this._text;
-		public Button Continue => this._continue;
-		public Button OutLevel => this._outLevel;
-		public Button GameOver => this._gameOver;
+		internal Label Text { get; private set; }
+		internal Button Continue { get; private set; }
+		internal Button OutLevel { get; private set; }
+		internal Button GameOver { get; private set; }
 		private void Awake()
 		{
 			if (_instance)
@@ -27,10 +23,10 @@ namespace GuwbaPrimeAdventure.Hud
 			}
 			_instance = this;
 			VisualElement root = this.GetComponent<UIDocument>().rootVisualElement;
-			this._text = root.Q<Label>(this._textLabel);
-			this._continue = root.Q<Button>(this._continueButton);
-			this._outLevel = root.Q<Button>(this._outLevelButton);
-			this._gameOver = root.Q<Button>(this._gameOverButton);
+			this.Text = root.Q<Label>(this._textLabel);
+			this.Continue = root.Q<Button>(this._continueButton);
+			this.OutLevel = root.Q<Button>(this._outLevelButton);
+			this.GameOver = root.Q<Button>(this._gameOverButton);
 		}
 	};
 };
