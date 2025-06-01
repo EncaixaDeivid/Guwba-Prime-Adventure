fd2c1118c05e849b96b0c659991ff3bd593045ac
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-01 18:25:42 -0300

    Update Touch Activator.cs

--- Items/Event Items/Scripts/Touch Activator.cs
+++ Items/Event Items/Scripts/Touch Activator.cs
@@ -4,30 +4,32 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 	[DisallowMultipleComponent]
 	internal sealed class TouchActivator : Activator
 	{
-		[SerializeField] private GameObject _objectActivator;
+		[SerializeField] private GameObject[] _objectsActivators;
 		[SerializeField] private bool _enterCollision;
 		[SerializeField] private bool _exitCollision;
 		[SerializeField] private bool _enterTrigger;
 		[SerializeField] private bool _exitTrigger;
 		[SerializeField] private bool _destroyObject;
-		private void Activate(bool activationKey, bool objectKey)
+		private void Activate(bool activationKey, GameObject objectKey)
 		{
 			if (activationKey)
-				if (this._objectActivator)
+				if (this._objectsActivators != null)
 				{
-					if (objectKey)
-					{
-						this.Activation();
-						if (this._destroyObject)
-							Destroy(this._objectActivator);
-					}
+					foreach (GameObject activator in this._objectsActivators)
+						if (activator != null && activator == objectKey)
+						{
+							this.Activation();
+							if (this._destroyObject)
+								Destroy(activator);
+							return;
+						}
 				}
 				else
 					this.Activation();
 		}
-		private void OnCollisionEnter2D(Collision2D other) => this.Activate(this._enterCollision, this._objectActivator == other.gameObject);
-		private void OnTriggerEnter2D(Collider2D other) => this.Activate(this._enterTrigger, this._objectActivator == other.gameObject);
-		private void OnCollisionExit2D(Collision2D other) => this.Activate(this._exitCollision, this._objectActivator == other.gameObject);
-		private void OnTriggerExit2D(Collider2D other) => this.Activate(this._exitTrigger, this._objectActivator == other.gameObject);
+		private void OnCollisionEnter2D(Collision2D other) => this.Activate(this._enterCollision, other.gameObject);
+		private void OnTriggerEnter2D(Collider2D other) => this.Activate(this._enterTrigger, other.gameObject);
+		private void OnCollisionExit2D(Collision2D other) => this.Activate(this._exitCollision, other.gameObject);
+		private void OnTriggerExit2D(Collider2D other) => this.Activate(this._exitTrigger, other.gameObject);
 	};
 };
