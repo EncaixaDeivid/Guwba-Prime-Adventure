452824a98d9dc285f02dece7accbcd9d7b4d73a5
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-01 20:14:38 -0300

    Update Hit Activator.cs

--- Items/Event Items/Scripts/Hit Activator.cs
+++ Items/Event Items/Scripts/Hit Activator.cs
@@ -4,7 +4,7 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 	[DisallowMultipleComponent]
 	internal sealed class HitActivator : Activator, IDamageable
 	{
-		[SerializeField] private ushort _biggerDamage;
+		[SerializeField, Tooltip("The amount of damage that this object have to receive real damage.")] private ushort _biggerDamage;
 		public ushort Health => 0;
 		public bool Damage(ushort damage)
 		{
