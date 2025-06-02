17499a8a49fb99b0e34d2ea5024c47423891e8e0
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-02 09:56:18 -0300

    Update Summon Object.cs

--- Enemys/Scripts/Summon Object.cs
+++ Enemys/Scripts/Summon Object.cs
@@ -4,19 +4,19 @@ namespace GuwbaPrimeAdventure.Enemy
 	[CreateAssetMenu(fileName = "Summon", menuName = "Scriptable Objects/Summon", order = 1)]
 	public sealed class SummonObject : ScriptableObject
 	{
-		[SerializeField] private EnemyController _enemySummon;
-		[SerializeField] private Projectile _projectileSummon;
-		[SerializeField] private Vector2[] _summonPoints;
-		[SerializeField] private ushort _summonTime;
-		[SerializeField] private ushort _quantityToSummon;
-		[SerializeField] private float _timeToStop;
-		[SerializeField] private bool _stopToSummon;
-		[SerializeField] private bool _paralyzeToSummon;
-		[SerializeField] private bool _stopTimedSummon;
-		[SerializeField] private bool _self;
-		[SerializeField] private bool _combine;
-		[SerializeField] private bool _sequential;
-		[SerializeField] private bool _random;
+		[SerializeField, Tooltip("The enemy that will be instantiate.")] private EnemyController _enemySummon;
+		[SerializeField, Tooltip("The projectile that will be instantiate.")] private Projectile _projectileSummon;
+		[SerializeField, Tooltip("The points that the instance can be instantiate.")] private Vector2[] _summonPoints;
+		[SerializeField, Tooltip("The amount of time to execute the instance.")] private ushort _summonTime;
+		[SerializeField, Tooltip("The amount of instance to be instantiate.")] private ushort _quantityToSummon;
+		[SerializeField, Tooltip("The amount of time to stop the instantiator.")] private float _timeToStop;
+		[SerializeField, Tooltip("If the instantiator will stop during the summon.")] private bool _stopToSummon;
+		[SerializeField, Tooltip("If the instantiator will paralyze during the summon.")] private bool _paralyzeToSummon;
+		[SerializeField, Tooltip("If the timed summon can be stopped.")] private bool _stopTimedSummon;
+		[SerializeField, Tooltip("If the instanciation will be in the same point as the summoner.")] private bool _self;
+		[SerializeField, Tooltip("If the instanciation will combine the point of the summoner and the instantiation.")] private bool _combine;
+		[SerializeField, Tooltip("If the instantiation will be for every point for the amount of quantity.")] private bool _sequential;
+		[SerializeField, Tooltip("If the instantiation will be randomized at one of the points.")] private bool _random;
 		public GameObject Summon
 		{
 			get => this._enemySummon ? this._enemySummon.gameObject : this._projectileSummon ? this._projectileSummon.gameObject : null;
