597dc3802df09a0df6bc7b9f261583152afb01b0
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-03 19:11:49 -0300

    Update Flying Enemy.cs

--- Enemys/Scripts/Flying Enemy.cs
+++ Enemys/Scripts/Flying Enemy.cs
@@ -126,7 +126,7 @@ namespace GuwbaPrimeAdventure.Enemy
 			if (additionalData as GameObject != this.gameObject)
 				return;
 			if (data.ConnectionState == ConnectionState.State && data.ToggleValue.HasValue)
-				this._stopMovement = data.ToggleValue.Value;
+				this._stopMovement = !data.ToggleValue.Value;
 		}
 	};
 };
