2255805d6d7d29d5d3dd3dd1653ec97669a2c23d
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-03 18:46:27 -0300

    Update Weak Point Prop.cs

--- Enemys/Bosses/Scripts/Weak Point Prop.cs
+++ Enemys/Bosses/Scripts/Weak Point Prop.cs
@@ -6,12 +6,19 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 	[DisallowMultipleComponent]
 	internal sealed class WeakPointProp : BossProp, IDamageable
 	{
+		private readonly Sender _sender = Sender.Create();
 		private bool _blockDamage = false;
 		[Header("Weak Point")]
 		[SerializeField, Tooltip("The vitality of this prop.")] private short _vitality;
 		[SerializeField, Tooltip("The amount of damage that this object have to receive real damage.")] private ushort _biggerDamage;
 		[SerializeField, Tooltip("The amount of time to wait after damaging the prop again.")] private float _timeToDamage;
 		public ushort Health => (ushort)this._vitality;
+		private new void Awake()
+		{
+			base.Awake();
+			this._sender.SetToWhereConnection(PathConnection.Boss).SetConnectionState(ConnectionState.Action);
+			this._sender.SetAdditionalData(BossType.All);
+		}
 		public bool Damage(ushort damage)
 		{
 			if (this._blockDamage)
@@ -27,11 +34,9 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 				}
 				this._vitality -= (short)damage;
 				if (this._indexReact)
-					Sender.Create().SetToWhereConnection(PathConnection.Boss).SetConnectionState(ConnectionState.Action)
-						.SetBossType(BossType.All).SetIndex(this._indexEvent).Send();
+					this._sender.SetIndex(this._indexEvent).Send();
 				else
-					Sender.Create().SetToWhereConnection(PathConnection.Boss).SetConnectionState(ConnectionState.Action)
-						.SetBossType(BossType.All).Send();
+					this._sender.Send();
 				if (this._vitality <= 0)
 				{
 					this._useDestructuion = true;
