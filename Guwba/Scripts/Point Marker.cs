c2244c7e0a55699da9cf5a49277443c5ddb2999d
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-04 13:23:47 -0300

    Update Point Marker.cs

--- Guwba/Scripts/Point Marker.cs
+++ Guwba/Scripts/Point Marker.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using GuwbaPrimeAdventure.Data;
+using GuwbaPrimeAdventure.Connection;
 namespace GuwbaPrimeAdventure.Guwba
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(BoxCollider2D))]
-	internal sealed class PointMarker : StateController
+	internal sealed class PointMarker : StateController, IConnector
 	{
 		private static Vector2 _checkpointIndex = new();
 		private static string _lastScene = "";
@@ -11,6 +12,23 @@ namespace GuwbaPrimeAdventure.Guwba
 		private static bool _checkScene = true;
 		[SerializeField, Tooltip("The name of the hubby world scene.")] private string _levelSelectorScene;
 		[SerializeField, Tooltip("Which point is checked when scene is the level selector.")] private ushort _selfIndex;
+		public PathConnection PathConnection => PathConnection.Character;
+		private new void Awake()
+		{
+			base.Awake();
+			Sender.Include(this);
+		}
+		private new void OnDestroy()
+		{
+			base.OnDestroy();
+			_useCheckpoint = true;
+			if (_checkScene)
+			{
+				_checkScene = false;
+				_lastScene = this.gameObject.scene.name;
+			}
+			Sender.Exclude(this);
+		}
 		private void Start()
 		{
 			SaveController.Load(out SaveFile saveFile);
@@ -24,20 +42,18 @@ namespace GuwbaPrimeAdventure.Guwba
 			}
 			_checkScene = true;
 		}
-		private new void OnDestroy()
-		{
-			base.OnDestroy();
-			_useCheckpoint = true;
-			if (_checkScene)
-			{
-				_checkScene = false;
-				_lastScene = this.gameObject.scene.name;
-			}
-		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
 			if (GuwbaAstral<CommandGuwba>.EqualObject(other.gameObject))
 				_checkpointIndex = this.transform.position;
+		}
+		public void Receive(DataConnection data, object additionalData)
+		{
+			if (data.ConnectionState == ConnectionState.Enable && data.ToggleValue.HasValue && data.ToggleValue.Value)
+			{
+				this.Start();
+				_useCheckpoint = true;
+			}
 		}
 	};
 };
