885b3664a7330958f77580b1238f2308d1103435
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-04 18:24:19 -0300

    Update Command Guwba.cs

--- Guwba/Scripts/Command Guwba.cs
+++ Guwba/Scripts/Command Guwba.cs
@@ -306,8 +306,7 @@ namespace GuwbaPrimeAdventure.Guwba
 			float yPoint = this.transform.position.y - this._collider.bounds.extents.y - this._groundChecker / 2f;
 			Vector2 pointGround = new(this.transform.position.x, yPoint);
 			Vector2 sizeGround = new(this._collider.size.x - .025f, this._groundChecker);
-			float rotation = this.transform.rotation.z * Mathf.Rad2Deg;
-			this._isOnGround = Physics2D.OverlapBox(pointGround, sizeGround, rotation, this._groundLayerMask);
+			this._isOnGround = Physics2D.OverlapBox(pointGround, sizeGround, this.transform.rotation.eulerAngles.z, this._groundLayerMask);
 		}
 		private void OnTrigger(GameObject collisionObject)
 		{
