0e8a14015de59826e07b7a2301218207985d6ff2
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-04 13:27:38 -0300

    Update Point Marker.cs

--- Guwba/Scripts/Point Marker.cs
+++ Guwba/Scripts/Point Marker.cs
@@ -7,9 +7,6 @@ namespace GuwbaPrimeAdventure.Guwba
 	internal sealed class PointMarker : StateController, IConnector
 	{
 		private static Vector2 _checkpointIndex = new();
-		private static string _lastScene = "";
-		private static bool _useCheckpoint = true;
-		private static bool _checkScene = true;
 		[SerializeField, Tooltip("The name of the hubby world scene.")] private string _levelSelectorScene;
 		[SerializeField, Tooltip("Which point is checked when scene is the level selector.")] private ushort _selfIndex;
 		public PathConnection PathConnection => PathConnection.Character;
@@ -21,12 +18,6 @@ namespace GuwbaPrimeAdventure.Guwba
 		private new void OnDestroy()
 		{
 			base.OnDestroy();
-			_useCheckpoint = true;
-			if (_checkScene)
-			{
-				_checkScene = false;
-				_lastScene = this.gameObject.scene.name;
-			}
 			Sender.Exclude(this);
 		}
 		private void Start()
@@ -35,12 +26,6 @@ namespace GuwbaPrimeAdventure.Guwba
 			if (this.gameObject.scene.name == this._levelSelectorScene && saveFile.lastLevelEntered != "")
 				if (ushort.Parse($"{saveFile.lastLevelEntered[^1]}") == this._selfIndex)
 					GuwbaAstral<CommandGuwba>.Position = this.transform.position;
-			if (_lastScene == this.gameObject.scene.name && _useCheckpoint)
-			{
-				_useCheckpoint = false;
-				GuwbaAstral<CommandGuwba>.Position = _checkpointIndex;
-			}
-			_checkScene = true;
 		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
@@ -50,10 +35,7 @@ namespace GuwbaPrimeAdventure.Guwba
 		public void Receive(DataConnection data, object additionalData)
 		{
 			if (data.ConnectionState == ConnectionState.Enable && data.ToggleValue.HasValue && data.ToggleValue.Value)
-			{
-				this.Start();
-				_useCheckpoint = true;
-			}
+				GuwbaAstral<CommandGuwba>.Position = _checkpointIndex;
 		}
 	};
 };
