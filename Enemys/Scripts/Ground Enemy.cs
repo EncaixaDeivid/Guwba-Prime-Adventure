9508d68e66e3df75c56ac54fddab6dc9579eb9b9
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-07 12:27:29 -0300

    Update Ground Enemy.cs

--- Enemys/Scripts/Ground Enemy.cs
+++ Enemys/Scripts/Ground Enemy.cs
@@ -6,14 +6,21 @@ namespace GuwbaPrimeAdventure.Enemy
 	internal sealed class GroundEnemy : EnemyController, IConnector
 	{
 		private readonly Sender _sender = Sender.Create();
-		private bool _rotate = true;
+		private bool _rotate = false;
+		private bool _runTowards = false;
+		private ushort _runnedTimes = 0;
+		private float _timeRun = 0f;
 		[Header("Ground Enemy")]
-		[SerializeField, Tooltip("If this enemy will increase its speed when look to a target.")] private bool _useFaceLookVerifier;
-		[SerializeField, Tooltip("Will become invencible while pursuing a target. Requires: Defender Enemy")] private bool _invenciblePursue;
 		[SerializeField, Tooltip("If this enemy will crawl on the walls.")] private bool _useCrawlMovement;
+		[SerializeField, Tooltip("If this enemy will do some action when look to a target.")] private bool _useFaceLook;
+		[SerializeField, Tooltip("If this enemy will run away from the target.")] private bool _runFromTarget;
+		[SerializeField, Tooltip("If this enemy will run away from the target.")] private bool _runTowardsAfter;
+		[SerializeField, Tooltip("Will become invencible while pursuing a target.\nRequires: Defender Enemy")] private bool _invenciblePursue;
 		[SerializeField, Tooltip("If the target is anything.")] private bool _targetEveryone;
 		[SerializeField, Tooltip("The amount of speed to increase.")] private ushort _increasedSpeed;
 		[SerializeField, Tooltip("The distance of the face look.")] private ushort _faceLookDistance;
+		[SerializeField, Tooltip("The amount of times this enemy have to run away from the target.")] private ushort _timesToRun;
+		[SerializeField, Tooltip("The amount of time this enemy will run away from or pursue the target.")] private float _runOfTime;
 		[SerializeField, Tooltip("The distance to check for the block perception.")] private float _blockDistance;
 		[SerializeField, Tooltip("The gravity applied when crawling.")] private float _crawlGravity;
 		[SerializeField, Tooltip("The distance of the ray when crawling.")] private float _crawlRayDistance;
@@ -21,6 +28,7 @@ namespace GuwbaPrimeAdventure.Enemy
 		private new void Awake()
 		{
 			base.Awake();
+			this._timeRun = this._timesToRun;
 			this._sender.SetToWhereConnection(PathConnection.Enemy).SetConnectionState(ConnectionState.Action);
 			this._sender.SetAdditionalData(this.gameObject);
 			Sender.Include(this);
@@ -35,7 +43,23 @@ namespace GuwbaPrimeAdventure.Enemy
 			if (this._stopMovement || this.Paralyzed)
 				return;
 			bool faceLook = false;
-			if (this._useFaceLookVerifier)
+			if (this._timeRun > 0f)
+			{
+				this._timeRun -= Time.fixedDeltaTime;
+				faceLook = true;
+			}
+			if (this._runFromTarget && this._timeRun <= 0f)
+			{
+				if (this._runTowardsAfter && this._runnedTimes >= this._timesToRun)
+				{
+					this._runnedTimes = 0;
+					this._runTowards = !this._runTowards;
+				}
+				else if (this._runTowardsAfter)
+					this._runnedTimes++;
+				faceLook = false;
+			}
+			if (this._useFaceLook)
 			{
 				Vector2 rayDirection = this._useCrawlMovement ? this.transform.right * this._movementSide : Vector2.right * this._movementSide;
 				float rayDistance = this._faceLookDistance;
@@ -51,7 +75,14 @@ namespace GuwbaPrimeAdventure.Enemy
 			float speedIncreased = this._movementSpeed + this._increasedSpeed;
 			this._spriteRenderer.flipX = this._movementSide < 0f;
 			Vector2 size = new(this._collider.bounds.size.x + this._blockDistance, this._collider.bounds.size.y - this._blockDistance);
-			if (Physics2D.OverlapBox(this.transform.position, size, this.transform.eulerAngles.z, this._groundLayer))
+			bool blockPerception = Physics2D.OverlapBox(this.transform.position, size, this.transform.eulerAngles.z, this._groundLayer);
+			if (this._runFromTarget && this._timeRun <= 0f && faceLook)
+			{
+				this._timeRun = this._runOfTime;
+				if (!this._runTowards)
+					this._movementSide *= -1;
+			}
+			if (blockPerception)
 				this._movementSide *= -1;
 			if (this._useCrawlMovement)
 			{
