361e9ece4c5ae308089bb350c1aef9e015e5e513
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-09 01:04:15 -0300

    Update Jump Point.cs

--- Enemys/Bosses/Scripts/Jump Point.cs
+++ Enemys/Bosses/Scripts/Jump Point.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using GuwbaPrimeAdventure.Guwba;
 namespace GuwbaPrimeAdventure.Enemy.Boss
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
@@ -7,6 +8,7 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 	{
 		private UnityAction<ushort> _getTouch;
 		private ushort _touchIndex;
+		[SerializeField, Tooltip("If this point will trigger with other object.")] private bool _hasTarget;
 		internal void GetTouch(ushort touchIndex, UnityAction<ushort> getTouch)
 		{
 			this._getTouch = getTouch;
@@ -14,8 +16,14 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
+			if (this._hasTarget)
+			{
+				if (GuwbaAstral<VisualGuwba>.EqualObject(other.gameObject))
+					this._getTouch.Invoke(this._touchIndex);
+				return;
+			}
 			if (other.TryGetComponent<JumperBoss>(out _))
-				this._getTouch(this._touchIndex);
+				this._getTouch.Invoke(this._touchIndex);
 		}
 	};
 };
