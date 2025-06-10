d1b69bf361ae24d119baa3a76772145044843d44
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-10 19:14:06 -0300

    Update Scene Object.cs

--- Off Enviroment/Scripts/Scene Object.cs
+++ Off Enviroment/Scripts/Scene Object.cs
@@ -5,11 +5,13 @@ namespace GuwbaPrimeAdventure.OffEnviroment
 	[CreateAssetMenu(fileName = "Scene", menuName = "Scriptable Objects/Scene", order = 2)]
 	internal sealed class SceneObject : ScriptableObject
 	{
+		[Header("Scene Component Collection")]
 		[SerializeField, Tooltip("The collection of objects that carry the background settings.")] private BackgroundImage[] _backgroundImages;
 		internal BackgroundImage[] BackgroundImages => this._backgroundImages;
 		[Serializable]
 		internal struct BackgroundImage
 		{
+			[Header("Scene Component")]
 			[SerializeField, Tooltip("The main imgae that is placed in the hud.")] private Texture2D _image;
 			[SerializeField, Tooltip("If the dialog will go turned off during the scene.")] private bool _offDialog;
 			[SerializeField, Tooltip("The amount of time that the dialog will be turned off.")] private float _timeToDesapear;
