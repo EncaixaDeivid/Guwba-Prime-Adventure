4854ec25fbc94173f2d3c1d5b5ec3150fc02aaac
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-10 19:14:24 -0300

    Update Story Scene Hud.cs

--- Off Enviroment/Scripts/Story Scene Hud.cs
+++ Off Enviroment/Scripts/Story Scene Hud.cs
@@ -6,6 +6,7 @@ namespace GuwbaPrimeAdventure.OffEnviroment
 	internal sealed class StorySceneHud : MonoBehaviour
 	{
 		private static StorySceneHud _instance;
+		[Header("Element")]
 		[SerializeField, Tooltip("User interface element.")] private string _sceneImageVisual;
 		internal VisualElement SceneImage { get; private set; }
 		private void Awake()
