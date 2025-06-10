317e1b06193602e965e3940a793d57c3f3019c37
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-10 19:23:14 -0300

    Update Death Screen Hud.cs

--- System/Huds/Scripts/Death Screen Hud.cs
+++ System/Huds/Scripts/Death Screen Hud.cs
@@ -6,7 +6,7 @@ namespace GuwbaPrimeAdventure.Hud
 	internal sealed class DeathScreenHud : MonoBehaviour
 	{
 		private static DeathScreenHud _instance;
-		[Header("Element")]
+		[Header("Elements")]
 		[SerializeField, Tooltip("User interface element.")] private string _textLabel;
 		[SerializeField, Tooltip("User interface element.")] private string _continueButton;
 		[SerializeField, Tooltip("User interface element.")] private string _outLevelButton;
