8a673ea2089d171828e24a6572e8d4fb2717e459
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-10 19:13:36 -0300

    Update Dialog Object.cs

--- Off Enviroment/Scripts/Dialog Object.cs
+++ Off Enviroment/Scripts/Dialog Object.cs
@@ -5,12 +5,14 @@ namespace GuwbaPrimeAdventure.OffEnviroment
 	[CreateAssetMenu(fileName = "Dialog", menuName = "Scriptable Objects/Dialog", order = 0)]
 	internal class DialogObject : ScriptableObject
     {
+		[Header("Dialog Collection")]
 		[SerializeField, Tooltip("The collection of the object that contais the entire dialog.")] private Dialog[] _dialog;
 		internal Dialog[] Dialogs => this._dialog;
 	};
 	[Serializable]
 	internal struct Dialog
 	{
+		[Header("Dialog Component")]
 		[SerializeField, Tooltip("The collection of the object that contais the speach.")] private Speach[] _speach;
 		[SerializeField, Tooltip("The scene to trancision to after the speach.")] private string _sceneToTransition;
 		[SerializeField, Tooltip("The animation to play after the speach.")] private string _animation;
@@ -32,6 +34,7 @@ namespace GuwbaPrimeAdventure.OffEnviroment
 		[Serializable]
 		internal struct Speach
 		{
+			[Header("Speach Component")]
 			[SerializeField, Tooltip("The image icon of the character that is speaking.")] private Sprite _model;
 			[SerializeField, Tooltip("The name of the character that is speaking.")] private string _characterName;
 			[SerializeField, TextArea(1, 12), Tooltip("The speach of the character that is speaking.")] private string _speachText;
