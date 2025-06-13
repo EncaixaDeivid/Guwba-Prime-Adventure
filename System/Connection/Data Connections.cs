d0379bc3b8b3c3a1f83c2a2b0f5021972ff9ff26
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-13 20:28:28 -0300

    Update Data Connections.cs

--- System/Connection/Data Connections.cs
+++ System/Connection/Data Connections.cs
@@ -1,15 +1,27 @@
 using Unity.Jobs;
 namespace GuwbaPrimeAdventure.Connection
 {
+	internal struct SenderPoperties
+	{
+		internal IConnector ConnectionToIgnore { get; private set; }
+		internal PathConnection ToWhereConnection { get; private set; }
+		internal object AdditionalData { get; private set; }
+		internal SenderPoperties(IConnector connectionToIgnore, PathConnection toWhereConnection, object additionalData)
+		{
+			this.ConnectionToIgnore = connectionToIgnore;
+			this.ToWhereConnection = toWhereConnection;
+			this.AdditionalData = additionalData;
+		}
+	};
 	public struct DataConnection : IJob
 	{
-		private readonly Sender _sender;
+		private readonly SenderPoperties _senderPoperties;
 		public StateForm StateForm { get; private set; }
 		public bool? ToggleValue { get; private set; }
 		public uint? IndexValue { get; private set; }
-		internal DataConnection(Sender sender, StateForm stateForm, bool? toggleValue, uint? indexValue)
+		internal DataConnection(SenderPoperties senderPoperties, StateForm stateForm, bool? toggleValue, uint? indexValue)
 		{
-			this._sender = sender;
+			this._senderPoperties = senderPoperties;
 			this.StateForm = stateForm;
 			this.ToggleValue = toggleValue;
 			this.IndexValue = indexValue;
@@ -18,9 +30,9 @@ namespace GuwbaPrimeAdventure.Connection
 		{
 			foreach (IConnector connector in Sender.Connectors)
 			{
-				if (connector == this._sender.ConnectionToIgnore || connector.PathConnection != this._sender.ToWhereConnection)
+				if (connector == this._senderPoperties.ConnectionToIgnore || connector.PathConnection != this._senderPoperties.ToWhereConnection)
 					continue;
-				connector.Receive(this, this._sender.AdditionalData);
+				connector.Receive(this, this._senderPoperties.AdditionalData);
 			}
 		}
 	};
