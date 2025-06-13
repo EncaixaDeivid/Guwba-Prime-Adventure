d51adfca26aac3640c63abc2681754c7ebcc169a
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-13 18:54:32 -0300

    Update Data Connections.cs

--- System/Connection/Data Connections.cs
+++ System/Connection/Data Connections.cs
@@ -1,15 +1,27 @@
+using Unity.Jobs;
 namespace GuwbaPrimeAdventure.Connection
 {
-	public struct DataConnection
+	public struct DataConnection : IJob
 	{
-		public ConnectionState ConnectionState { get; private set; }
+		private readonly Sender _sender;
+		public StateForm ConnectionState { get; private set; }
 		public bool? ToggleValue { get; private set; }
 		public uint? IndexValue { get; private set; }
-		internal DataConnection(ConnectionState connectionState, bool? toggleValue, uint? indexValue)
+		internal DataConnection(Sender sender, StateForm connectionState, bool? toggleValue, uint? indexValue)
 		{
+			this._sender = sender;
 			this.ConnectionState = connectionState;
 			this.ToggleValue = toggleValue;
 			this.IndexValue = indexValue;
+		}
+		public readonly void Execute()
+		{
+			foreach (IConnector connector in Sender.Connectors)
+			{
+				if (connector == this._sender.ConnectionToIgnore || connector.PathConnection != this._sender.ToWhereConnection)
+					continue;
+				connector.Receive(this, this._sender.AdditionalData);
+			}
 		}
 	};
 };
