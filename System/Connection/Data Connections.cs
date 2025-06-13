c923233bdd82b1be839672754bc064f550cff4ff
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-13 18:58:16 -0300

    Update Data Connections.cs

--- System/Connection/Data Connections.cs
+++ System/Connection/Data Connections.cs
@@ -4,13 +4,13 @@ namespace GuwbaPrimeAdventure.Connection
 	public struct DataConnection : IJob
 	{
 		private readonly Sender _sender;
-		public StateForm ConnectionState { get; private set; }
+		public StateForm StateForm { get; private set; }
 		public bool? ToggleValue { get; private set; }
 		public uint? IndexValue { get; private set; }
-		internal DataConnection(Sender sender, StateForm connectionState, bool? toggleValue, uint? indexValue)
+		internal DataConnection(Sender sender, StateForm stateForm, bool? toggleValue, uint? indexValue)
 		{
 			this._sender = sender;
-			this.ConnectionState = connectionState;
+			this.StateForm = stateForm;
 			this.ToggleValue = toggleValue;
 			this.IndexValue = indexValue;
 		}
