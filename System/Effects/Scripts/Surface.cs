5b998653f13a48dbe0a4c7cef1106c5d2950a2c6
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-16 22:45:06 -0300

    Delete System/Effects directory

--- System/Effects/Scripts/Surface.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-namespace GuwbaPrimeAdventure.Effects
-{
-	public sealed class Surface : StateController
-	{
-		[SerializeField] private bool _isScene;
-		public bool IsScene => this._isScene;
-	};
-};
