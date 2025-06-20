bb51392785542825881b14fb3305f4344ab61605
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-20 03:14:55 -0300

    Update Point Marker.cs

--- Guwba/Point Marker.cs
+++ Guwba/Point Marker.cs
@@ -6,7 +6,9 @@ namespace GuwbaPrimeAdventure.Guwba
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(BoxCollider2D))]
 	internal sealed class PointMarker : StateController, IConnector
 	{
+		private readonly Sender _sender = Sender.Create();
 		private static Vector2 _checkpointIndex = new();
+		private bool _isChecked = false;
 		[Header("Hubby World Interaction")]
 		[SerializeField, Tooltip("The name of the hubby world scene.")] private string _levelSelectorScene;
 		[SerializeField, Tooltip("Which point is checked when scene is the level selector.")] private ushort _selfIndex;
@@ -14,6 +16,7 @@ namespace GuwbaPrimeAdventure.Guwba
 		private new void Awake()
 		{
 			base.Awake();
+			this._sender.SetToWhereConnection(PathConnection.Character).SetStateForm(StateForm.Action).SetToggle(true);
 			Sender.Include(this);
 		}
 		private new void OnDestroy()
@@ -30,13 +33,19 @@ namespace GuwbaPrimeAdventure.Guwba
 		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
-			if (GuwbaAstral<CommandGuwba>.EqualObject(other.gameObject))
+			if (!this._isChecked && GuwbaAstral<CommandGuwba>.EqualObject(other.gameObject))
+			{
+				this._isChecked = true;
 				_checkpointIndex = this.transform.position;
+			}
 		}
 		public void Receive(DataConnection data, object additionalData)
 		{
 			if (data.StateForm == StateForm.Enable && data.ToggleValue.HasValue && data.ToggleValue.Value)
+			{
 				GuwbaAstral<CommandGuwba>.Position = _checkpointIndex;
+				this._sender.SetAdditionalData(_checkpointIndex);
+			}
 		}
 	};
 };
