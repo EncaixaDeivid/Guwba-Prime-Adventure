a303d36d32124ed5211e46a5c8ffd6d946597902
Deivid S. Santos <129303913+DeividSSantos@users.noreply.github.com>
2025-06-20 11:58:53 -0300

    Update Sender.cs

--- System/Connection/Sender.cs
+++ System/Connection/Sender.cs
@@ -9,14 +9,14 @@ namespace GuwbaPrimeAdventure.Connection
 			this._stateForm = StateForm.None;
 			this._additionalData = null;
 			this._toggleValue = null;
-			this._indexValue = null;
+			this._numberValue = null;
 		}
 		private static readonly List<IConnector> _connectors = new();
 		private PathConnection _toWhereConnection;
 		private StateForm _stateForm;
 		private object _additionalData;
 		private bool? _toggleValue;
-		private uint? _indexValue;
+		private uint? _numberValue;
 		public static void Include(IConnector connector)
 		{
 			if (!_connectors.Contains(connector))
@@ -51,12 +51,12 @@ namespace GuwbaPrimeAdventure.Connection
 		public Sender SetIndex(int value)
 		{
 			uint indexValue = (uint)(value < 0f ? -value : value);
-			this._indexValue = indexValue;
+			this._numberValue = indexValue;
 			return this;
 		}
 		public void Send()
 		{
-			DataConnection dataConnection = new(this._stateForm, this._toggleValue, this._indexValue);
+			DataConnection dataConnection = new(this._stateForm, this._toggleValue, this._numberValue);
 			foreach (IConnector connector in _connectors)
 				if (connector.PathConnection == this._toWhereConnection)
 					connector.Receive(dataConnection, this._additionalData);
