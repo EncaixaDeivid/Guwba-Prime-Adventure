c2108ed5c0897fbbec952fa02cc8ea8f76affbfb
AsWasWell <129303913+AsWasWell@users.noreply.github.com>
2025-06-24 18:23:48 -0300

    Update Command Guwba.cs

--- Guwba/Command Guwba.cs
+++ Guwba/Command Guwba.cs
@@ -32,7 +32,7 @@ namespace GuwbaPrimeAdventure.Guwba
 		[Header("Scene Interaction")]
 		[SerializeField, Tooltip("The camera that is attached to Guwba.")] private Camera _mainCamera;
 		[SerializeField, Tooltip("The layer mask that Guwba identifies the ground.")] private LayerMask _groundLayerMask;
-		[SerializeField, Tooltip("The layer mask that Guwba identifies a interactive object.")] private LayerMask _interactionLayerMask;
+		[SerializeField, Tooltip("The layer mask that Guwba identifies a interactive object.")] private LayerMask _InteractionLayerMask;
 		[SerializeField, Tooltip("Size of the collider in dash slide.")] private Vector2 _dashSlideSize;
 		[SerializeField, Tooltip("Size of the collider in death.")] private Vector2 _deadSize;
 		[Header("Animation")]
@@ -41,7 +41,7 @@ namespace GuwbaPrimeAdventure.Guwba
 		[SerializeField, Tooltip("Animation parameter.")] private string _walk;
 		[SerializeField, Tooltip("Animation parameter.")] private string _walkSpeed;
 		[SerializeField, Tooltip("Animation parameter.")] private string _dashSlide;
-		[SerializeField, Tooltip("Animation parameter.")] private string _jump;
+		[SerializeField, Tooltip("Animation parameter.")] private string _Jump;
 		[SerializeField, Tooltip("Animation parameter.")] private string _fall;
 		[SerializeField, Tooltip("Animation parameter.")] private string _attack;
 		[SerializeField, Tooltip("Animation parameter.")] private string _hold;
@@ -54,17 +54,17 @@ namespace GuwbaPrimeAdventure.Guwba
 		[SerializeField, Tooltip("Lowing the offset of the grab.")] private float _lowHoldOffset;
 		[Header("Movement")]
 		[SerializeField, Tooltip("The amount of speed that Guwba moves yourself.")] private float _movementSpeed;
-		[SerializeField, Tooltip("The amount of acceleration Guwba will apply to the movement.")] private float _acceleration;
-		[SerializeField, Tooltip("The amount of decceleration Guwba will apply to the movement.")] private float _decceleration;
-		[SerializeField, Tooltip("The amount of power the velocity Guwba will apply to the movement.")] private float _velocityPower;
-		[SerializeField, Tooltip("The amount of friction Guwba will apply to the end of movement.")] private float _frictionAmount;
+		[SerializeField, Tooltip("The amount of acceleration Guwba will apply to the Movement.")] private float _acceleration;
+		[SerializeField, Tooltip("The amount of decceleration Guwba will apply to the Movement.")] private float _decceleration;
+		[SerializeField, Tooltip("The amount of power the velocity Guwba will apply to the Movement.")] private float _velocityPower;
+		[SerializeField, Tooltip("The amount of friction Guwba will apply to the end of Movement.")] private float _frictionAmount;
 		[SerializeField, Tooltip("The amount of speed in both dashes.")] private float _dashSpeed;
 		[SerializeField, Tooltip("The amount of distance Guwba will go in both dashes.")] private float _dashDistance;
 		[SerializeField, Tooltip("If Guwba will look firstly to the left.")] private bool _turnLeft;
 		[Header("Jump")]
-		[SerializeField, Tooltip("The amount of strenght that Guwba can jump.")] private float _jumpStrenght;
-		[SerializeField, Tooltip("The amount of time that Guwba can jump before thouching ground.")] private float _jumpBufferTime;
-		[SerializeField, Tooltip("The amount of time that Guwba can jump when get out of the ground.")] private float _jumpCoyoteTime;
+		[SerializeField, Tooltip("The amount of strenght that Guwba can Jump.")] private float _jumpStrenght;
+		[SerializeField, Tooltip("The amount of time that Guwba can Jump before thouching ground.")] private float _jumpBufferTime;
+		[SerializeField, Tooltip("The amount of time that Guwba can Jump when get out of the ground.")] private float _jumpCoyoteTime;
 		[SerializeField, Tooltip("The amount of gravity to increase the fall.")] private float _fallGravityMultiply;
 		private new void Awake()
 		{
@@ -97,19 +97,19 @@ namespace GuwbaPrimeAdventure.Guwba
 			if (!_instance || _instance != this)
 				return;
 			this._inputController = new InputController();
-			this._inputController.commands.movement.performed += this.Movement;
-			this._inputController.commands.movement.canceled += this.Movement;
-			this._inputController.commands.jump.started += this.Jump;
-			this._inputController.commands.attackRotationConsole.performed += this.AttackRotationConsole;
-			this._inputController.commands.attackRotationKeyboard.performed += this.AttackRotationKeyboard;
-			this._inputController.commands.attackUse.started += this.AttackUse;
-			this._inputController.commands.interaction.started += this.Interaction;
-			this._inputController.commands.movement.Enable();
-			this._inputController.commands.jump.Enable();
-			this._inputController.commands.attackRotationConsole.Enable();
-			this._inputController.commands.attackRotationKeyboard.Enable();
-			this._inputController.commands.attackUse.Enable();
-			this._inputController.commands.interaction.Enable();
+			this._inputController.Commands.Movement.performed += this.Movement;
+			this._inputController.Commands.Movement.canceled += this.Movement;
+			this._inputController.Commands.Jump.started += this.Jump;
+			this._inputController.Commands.AttackRotationConsole.performed += this.AttackRotationConsole;
+			this._inputController.Commands.AttackRotationKeyboard.performed += this.AttackRotationKeyboard;
+			this._inputController.Commands.AttackUse.started += this.AttackUse;
+			this._inputController.Commands.Interaction.started += this.Interaction;
+			this._inputController.Commands.Movement.Enable();
+			this._inputController.Commands.Jump.Enable();
+			this._inputController.Commands.AttackRotationConsole.Enable();
+			this._inputController.Commands.AttackRotationKeyboard.Enable();
+			this._inputController.Commands.AttackUse.Enable();
+			this._inputController.Commands.Interaction.Enable();
 			this._animator.SetFloat(this._isOn, 1f);
 			this._animator.SetFloat(this._walkSpeed, this._dashValue ? -1f : 1f);
 			if (this._dashValue)
@@ -121,19 +121,19 @@ namespace GuwbaPrimeAdventure.Guwba
 		{
 			if (!_instance || _instance != this)
 				return;
-			this._inputController.commands.movement.performed -= this.Movement;
-			this._inputController.commands.movement.canceled -= this.Movement;
-			this._inputController.commands.jump.started -= this.Jump;
-			this._inputController.commands.attackRotationConsole.performed -= this.AttackRotationConsole;
-			this._inputController.commands.attackRotationKeyboard.performed -= this.AttackRotationKeyboard;
-			this._inputController.commands.attackUse.started -= this.AttackUse;
-			this._inputController.commands.interaction.started -= this.Interaction;
-			this._inputController.commands.movement.Disable();
-			this._inputController.commands.jump.Disable();
-			this._inputController.commands.attackRotationConsole.Disable();
-			this._inputController.commands.attackRotationKeyboard.Disable();
-			this._inputController.commands.attackUse.Disable();
-			this._inputController.commands.interaction.Disable();
+			this._inputController.Commands.Movement.performed -= this.Movement;
+			this._inputController.Commands.Movement.canceled -= this.Movement;
+			this._inputController.Commands.Jump.started -= this.Jump;
+			this._inputController.Commands.AttackRotationConsole.performed -= this.AttackRotationConsole;
+			this._inputController.Commands.AttackRotationKeyboard.performed -= this.AttackRotationKeyboard;
+			this._inputController.Commands.AttackUse.started -= this.AttackUse;
+			this._inputController.Commands.Interaction.started -= this.Interaction;
+			this._inputController.Commands.Movement.Disable();
+			this._inputController.Commands.Jump.Disable();
+			this._inputController.Commands.AttackRotationConsole.Disable();
+			this._inputController.Commands.AttackRotationKeyboard.Disable();
+			this._inputController.Commands.AttackUse.Disable();
+			this._inputController.Commands.Interaction.Disable();
 			this._inputController.Dispose();
 			this._animator.SetFloat(this._isOn, 0f);
 			this._animator.SetFloat(this._walkSpeed, 0f);
@@ -165,21 +165,21 @@ namespace GuwbaPrimeAdventure.Guwba
 				this._sender.SetStateForm(StateForm.Disable).SetToggle(true).Send();
 			}
 		};
-		private Action<InputAction.CallbackContext> Movement => movementAction =>
+		private Action<InputAction.CallbackContext> Movement => MovementAction =>
 		{
-			Vector2 movementValue = movementAction.ReadValue<Vector2>();
-			this._movementAction = movementValue.x;
-			if (movementValue.x > 0f)
+			Vector2 MovementValue = MovementAction.ReadValue<Vector2>();
+			this._movementAction = MovementValue.x;
+			if (MovementValue.x > 0f)
 				this._movementAction = 1f;
-			else if (movementValue.x < 0f)
+			else if (MovementValue.x < 0f)
 				this._movementAction = -1f;
-			if (!this._dashValue && this._movementAction != 0f && Mathf.Abs(movementValue.y) > 0.5f && this._isOnGround && !_grabObject)
+			if (!this._dashValue && this._movementAction != 0f && Mathf.Abs(MovementValue.y) > 0.5f && this._isOnGround && !_grabObject)
 			{
 				this.StartCoroutine(Dash());
 				IEnumerator Dash()
 				{
 					float dashDirection = 1f;
-					if (movementValue.y < 0f)
+					if (MovementValue.y < 0f)
 						dashDirection = -1f;
 					this._dashMovementValue = this._movementAction;
 					float dashLocation = this.transform.position.x;
@@ -228,7 +228,7 @@ namespace GuwbaPrimeAdventure.Guwba
 				}
 			}
 		};
-		private Action<InputAction.CallbackContext> Jump => jumpAction =>
+		private Action<InputAction.CallbackContext> Jump => JumpAction =>
 		{
 			if (_grabObject && !this._isOnGround)
 			{
@@ -279,7 +279,7 @@ namespace GuwbaPrimeAdventure.Guwba
 				GuwbaAstral<AttackGuwba>._actualState.Invoke(true);
 			}
 		};
-		private Action<InputAction.CallbackContext> Interaction => interactionAction =>
+		private Action<InputAction.CallbackContext> Interaction => InteractionAction =>
 		{
 			Vector2 point = this.transform.position;
 			if (_grabObject && !_grabObject.IsDamageable)
@@ -293,7 +293,7 @@ namespace GuwbaPrimeAdventure.Guwba
 			else if (this._isOnGround && this._movementAction == 0f && !this._animator.GetBool(this._attack))
 			{
 				float angle = this.transform.rotation.eulerAngles.z;
-				foreach (Collider2D collider in Physics2D.OverlapBoxAll(point, this._collider.size, angle, this._interactionLayerMask))
+				foreach (Collider2D collider in Physics2D.OverlapBoxAll(point, this._collider.size, angle, this._InteractionLayerMask))
 					if (collider.TryGetComponent<IInteractable>(out var interactable))
 					{
 						interactable.Interaction();
@@ -303,12 +303,12 @@ namespace GuwbaPrimeAdventure.Guwba
 		};
 		private void FixedUpdate()
 		{
-			float movementValue = this._movementAction != 0f ? this._movementAction > 0f ? 1f : -1f : 0f;
+			float MovementValue = this._movementAction != 0f ? this._movementAction > 0f ? 1f : -1f : 0f;
 			float rootHeight = this._collider.size.y / this._collider.size.y;
 			bool downStairs = false;
 			if (!this._isOnGround && this._downStairs && this._movementAction != 0f && this._lastJumpTime <= 0f)
 			{
-				float xOrigin = this.transform.position.x - ((this._collider.bounds.extents.x - .025f) * movementValue);
+				float xOrigin = this.transform.position.x - ((this._collider.bounds.extents.x - .025f) * MovementValue);
 				Vector2 downRayOrigin = new(xOrigin, this.transform.position.y - this._collider.bounds.extents.y);
 				float distance = rootHeight + this._groundChecker;
 				RaycastHit2D downRay = Physics2D.Raycast(downRayOrigin, -this.transform.up, distance, this._groundLayerMask);
@@ -321,7 +321,7 @@ namespace GuwbaPrimeAdventure.Guwba
 				{
 					this._animator.SetBool(this._idle, this._movementAction == 0f);
 					this._animator.SetBool(this._walk, this._movementAction != 0f);
-					this._animator.SetBool(this._jump, false);
+					this._animator.SetBool(this._Jump, false);
 					this._animator.SetBool(this._fall, false);
 					this._lastGroundedTime = this._jumpCoyoteTime;
 					this._downStairs = true;
@@ -331,7 +331,7 @@ namespace GuwbaPrimeAdventure.Guwba
 				{
 					this._animator.SetBool(this._idle, false);
 					this._animator.SetBool(this._walk, false);
-					this._animator.SetBool(this._jump, this._rigidbody.linearVelocityY > 0f);
+					this._animator.SetBool(this._Jump, this._rigidbody.linearVelocityY > 0f);
 					this._animator.SetBool(this._fall, this._rigidbody.linearVelocityY < 0f);
 					float fallGravity = this._fallGravityMultiply * this._gravityScale;
 					this._rigidbody.gravityScale = this._animator.GetBool(this._fall) ? fallGravity : this._gravityScale;
@@ -349,8 +349,8 @@ namespace GuwbaPrimeAdventure.Guwba
 				float targetSpeed = this._movementSpeed * this._movementAction;
 				float speedDiferrence = targetSpeed - this._rigidbody.linearVelocityX;
 				float accelerationRate = Mathf.Abs(targetSpeed) > 0f ? this._acceleration : this._decceleration;
-				float movement = Mathf.Pow(Mathf.Abs(speedDiferrence) * accelerationRate, this._velocityPower) * Mathf.Sign(speedDiferrence);
-				this._rigidbody.AddForceX(movement);
+				float Movement = Mathf.Pow(Mathf.Abs(speedDiferrence) * accelerationRate, this._velocityPower) * Mathf.Sign(speedDiferrence);
+				this._rigidbody.AddForceX(Movement);
 			}
 			if (this._isOnGround && Mathf.Abs(this._movementAction) <= 0f && !this._dashValue)
 			{
@@ -361,7 +361,7 @@ namespace GuwbaPrimeAdventure.Guwba
 			if (this._movementAction != 0f)
 			{
 				this._spriteRenderer.flipX = this._movementAction < 0f;
-				float xPosition = this.transform.position.x + (this._collider.bounds.extents.x + this._wallChecker / 2f) * movementValue;
+				float xPosition = this.transform.position.x + (this._collider.bounds.extents.x + this._wallChecker / 2f) * MovementValue;
 				Vector2 topPosition = new(xPosition, this.transform.position.y + rootHeight * .5f);
 				Vector2 bottomPosition = new(xPosition, this.transform.position.y - rootHeight * this._bottomCheckerOffset);
 				Vector2 topSize = new(this._wallChecker, rootHeight * this._topWallChecker - .1f);
@@ -372,12 +372,12 @@ namespace GuwbaPrimeAdventure.Guwba
 				{
 					float topCorner = this.transform.position.y + this._collider.bounds.extents.y;
 					float bottomCorner = this.transform.position.y - this._collider.bounds.extents.y;
-					Vector2 lineStart = new(xPosition + this._wallChecker / 2f * movementValue, topCorner);
-					Vector2 lineEnd = new(xPosition + this._wallChecker / 2f * movementValue, bottomCorner);
+					Vector2 lineStart = new(xPosition + this._wallChecker / 2f * MovementValue, topCorner);
+					Vector2 lineEnd = new(xPosition + this._wallChecker / 2f * MovementValue, bottomCorner);
 					RaycastHit2D lineWallStep = Physics2D.Linecast(lineStart, lineEnd, this._groundLayerMask);
 					if (lineWallStep && lineWallStep.collider == bottomCollider)
 					{
-						float xDistance = this.transform.position.x + this._wallChecker * movementValue;
+						float xDistance = this.transform.position.x + this._wallChecker * MovementValue;
 						float yDistance = this.transform.position.y + (lineWallStep.point.y - bottomCorner);
 						this.transform.position = new Vector2(xDistance, yDistance);
 						this._rigidbody.linearVelocityX = this._movementSpeed * this._movementAction;
