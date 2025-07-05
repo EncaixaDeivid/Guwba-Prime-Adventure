49c960204588914669d4a36131c1ff27208a92ed
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-07-05 20:46:55 -0300

    Update Mobile Place.cs

--- Items/Event Items/Mobile Place.cs
+++ Items/Event Items/Mobile Place.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Collections;
 using System.Linq;
+using GuwbaPrimeAdventure.Connection;
+using GuwbaPrimeAdventure.Guwba;
 namespace GuwbaPrimeAdventure.Item.EventItem
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Tilemap), typeof(TilemapRenderer))]
@@ -10,6 +12,7 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 	internal sealed class MobilePlace : StateController, Receptor.IReceptor
 	{
 		private Coroutine _movementCoroutine;
+		private readonly Sender _sender = Sender.Create();
 		private bool _touchActivate = false;
 		private ushort _actualPoint = 0;
 		[Header("Mobile Place")]
@@ -28,39 +31,35 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 		private new void Awake()
 		{
 			base.Awake();
+			this._sender.SetToWhereConnection(PathConnection.Guwba);
+			this._sender.SetStateForm(StateForm.Action);
+			this._sender.SetAdditionalData(this.transform);
 			this._touchActivate = this._touchActivation;
 			if (!this._isReceptor && !this._touchActivation)
 				this.StartCoroutine(this.Movement());
 		}
 		private IEnumerator Movement()
 		{
-			do
+			IEnumerator PassTrails(bool reverseTrail)
 			{
+				Vector2[] trail = reverseTrail ? this._trail.Reverse().ToArray() : this._trail;
 				yield return new WaitTime(this, this._waitStartTime);
-				foreach (Vector2 point in this._trail)
+				foreach (Vector2 point in trail)
 				{
 					yield return new WaitUntil(() =>
 					{
-						this.transform.position = Vector2.MoveTowards(this.transform.position, point, this._movementSpeed * Time.deltaTime);
+						this.transform.position = Vector2.MoveTowards(this.transform.position, point, this._movementSpeed * Time.fixedDeltaTime);
 						return (Vector2)this.transform.position == point && this.enabled;
 					});
 					yield return new WaitTime(this, this._waitWayTime);
 				}
 				yield return new WaitTime(this, this._waitEndTime);
+			}
+			do
+			{
+				yield return PassTrails(false);
 				if (this._returnWay)
-				{
-					foreach (Vector2 point in this._trail.Reverse())
-					{
-						yield return new WaitUntil(() =>
-						{
-							float maxDistanceDelta = this._speedReturn * Time.deltaTime;
-							this.transform.position = Vector2.MoveTowards(this.transform.position, point, maxDistanceDelta);
-							return (Vector2)this.transform.position == point && this.enabled;
-						});
-						yield return new WaitTime(this, this._waitWayTime);
-					}
-					yield return new WaitTime(this, this._waitEndTime);
-				}
+					yield return PassTrails(true);
 			}
 			while (this._executeAlways);
 			this._movementCoroutine = null;
@@ -72,7 +71,7 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 			yield return new WaitTime(this, this._waitStartTime);
 			yield return new WaitUntil(() =>
 			{
-				this.transform.position = Vector2.MoveTowards(this.transform.position, point, this._movementSpeed * Time.deltaTime);
+				this.transform.position = Vector2.MoveTowards(this.transform.position, point, this._movementSpeed * Time.fixedDeltaTime);
 				return (Vector2)this.transform.position == point && this.enabled;
 			});
 		}
@@ -88,9 +87,13 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 					this.StopCoroutine(this._movementCoroutine);
 			}
 		}
-
 		private void OnTriggerEnter2D(Collider2D other)
 		{
+			if (GuwbaAstral<CommandGuwba>.EqualObject(other.gameObject))
+			{
+				this._sender.SetToggle(true);
+				this._sender.Send();
+			}
 			if (this._touchActivation && this._touchActivate)
 			{
 				this._touchActivate = false;
@@ -101,6 +104,11 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 		}
 		private void OnTriggerExit2D(Collider2D other)
 		{
+			if (GuwbaAstral<CommandGuwba>.EqualObject(other.gameObject))
+			{
+				this._sender.SetToggle(false);
+				this._sender.Send();
+			}
 			if (this._stopOutTouch && !this._execution1X1)
 				this.StopCoroutine(this.Movement());
 		}
