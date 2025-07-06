a1d2888e9d301f74c445b21df6fc18bbdcabe716
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-07-06 00:17:55 -0300

    Update Mobile Place.cs

--- Items/Event Items/Mobile Place.cs
+++ Items/Event Items/Mobile Place.cs
@@ -7,33 +7,40 @@ using GuwbaPrimeAdventure.Guwba;
 namespace GuwbaPrimeAdventure.Item.EventItem
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Tilemap), typeof(TilemapRenderer))]
-	[RequireComponent(typeof(TilemapCollider2D), typeof(Rigidbody2D), typeof(CompositeCollider2D))]
-	[RequireComponent(typeof(BoxCollider2D), typeof(Receptor))]
+	[RequireComponent(typeof(TilemapCollider2D), typeof(Rigidbody2D), typeof(CompositeCollider2D)), RequireComponent(typeof(Receptor))]
 	internal sealed class MobilePlace : StateController, Receptor.IReceptor
 	{
 		private Coroutine _movementCoroutine;
 		private readonly Sender _sender = Sender.Create();
+		private Vector2 _startPosition = new();
 		private bool _touchActivate = false;
 		private ushort _actualPoint = 0;
 		[Header("Mobile Place")]
 		[SerializeField, Tooltip("The points that this object have to make the trail.")] private Vector2[] _trail;
-		[SerializeField, Tooltip("The speed that this object moves.")] private ushort _movementSpeed;
-		[SerializeField, Tooltip("The speed that this object moves to make the same way back.")] private ushort _speedReturn;
-		[SerializeField, Tooltip("The amount of time to wait after the activation.")] private ushort _waitStartTime;
-		[SerializeField, Tooltip("The amount of time to wait after every point of the trail.")] private ushort _waitWayTime;
-		[SerializeField, Tooltip("The amount of time to wait in the end of the trail.")] private ushort _waitEndTime;
+		[SerializeField, Tooltip("The offset of the plataform object checker.")] private Vector2 _checkerOffset;
+		[SerializeField, Tooltip("The size of the plataform object checker.")] private Vector2 _checkerSize;
+		[SerializeField, Tooltip("The size of the plataform object checker.")] private LayerMask _checkerLayerMask;
+		[SerializeField, Tooltip("The distance of the checker will go.")] private float _distanceChecker;
+		[SerializeField, Tooltip("The speed that this object moves.")] private float _movementSpeed;
+		[SerializeField, Tooltip("The speed that this object moves to make the same way back.")] private float _speedReturn;
+		[SerializeField, Tooltip("The amount of time to wait after the activation.")] private float _waitStartTime;
+		[SerializeField, Tooltip("The amount of time to wait after every point of the trail.")] private float _waitWayTime;
+		[SerializeField, Tooltip("The amount of time to wait in the end of the trail.")] private float _waitEndTime;
 		[SerializeField, Tooltip("If the object will return the way it makes.")] private bool _returnWay;
 		[SerializeField, Tooltip("If it will make the trail one time or always.")] private bool _executeAlways;
 		[SerializeField, Tooltip("If this object will receive a signal.")] private bool _isReceptor;
 		[SerializeField, Tooltip("If it will go to one point on each activation.")] private bool _execution1X1;
 		[SerializeField, Tooltip("If it activates at the touch with other object.")] private bool _touchActivation;
 		[SerializeField, Tooltip("If it will stop when other object step out of it.")] private bool _stopOutTouch;
+		[SerializeField, Tooltip("If it will make the trail one time or always on touch.")] private bool _executeAlwaysTouch;
+		[SerializeField, Tooltip("If it will teleport back when hit the destiny.")] private bool _teleportBack;
 		private new void Awake()
 		{
 			base.Awake();
 			this._sender.SetToWhereConnection(PathConnection.Guwba);
-			this._sender.SetStateForm(StateForm.Action);
+			this._sender.SetStateForm(StateForm.Enable);
 			this._sender.SetAdditionalData(this.transform);
+			this._startPosition = this.transform.position;
 			this._touchActivate = this._touchActivation;
 			if (!this._isReceptor && !this._touchActivation)
 				this.StartCoroutine(this.Movement());
@@ -62,6 +69,9 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 					yield return PassTrails(true);
 			}
 			while (this._executeAlways);
+			this._touchActivate = this._executeAlwaysTouch;
+			if (this._teleportBack)
+				this.transform.position = this._startPosition;
 			this._movementCoroutine = null;
 		}
 		private IEnumerator Movement1X1()
@@ -87,9 +97,13 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 					this.StopCoroutine(this._movementCoroutine);
 			}
 		}
-		private void OnTriggerEnter2D(Collider2D other)
+		private void OnCollision(GameObject gameObject)
 		{
-			if (GuwbaAstral<CommandGuwba>.EqualObject(other.gameObject))
+			Vector2 point = (Vector2)this.transform.position + this._checkerOffset;
+			bool isTouching = Physics2D.OverlapBoxAll(point, this._checkerSize, 0f, this._checkerLayerMask)?.Length > 0f;
+			if (!isTouching)
+				return;
+			if (GuwbaAstral<CommandGuwba>.EqualObject(gameObject))
 			{
 				this._sender.SetToggle(true);
 				this._sender.Send();
@@ -99,12 +113,15 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 				this._touchActivate = false;
 				if (this._execution1X1)
 					this.StartCoroutine(this.Movement1X1());
-				this.StartCoroutine(this.Movement());
+				else
+					this.StartCoroutine(this.Movement());
 			}
 		}
-		private void OnTriggerExit2D(Collider2D other)
+		private void OnCollisionEnter2D(Collision2D collision) => this.OnCollision(collision.gameObject);
+		private void OnCollisionStay2D(Collision2D collision) => this.OnCollision(collision.gameObject);
+		private void OnCollisionExit2D(Collision2D collision)
 		{
-			if (GuwbaAstral<CommandGuwba>.EqualObject(other.gameObject))
+			if (GuwbaAstral<CommandGuwba>.EqualObject(collision.gameObject))
 			{
 				this._sender.SetToggle(false);
 				this._sender.Send();
