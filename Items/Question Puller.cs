e7bef4960bb52a91d5b47252ec8d5e8e41a06513
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-07-08 06:23:41 -0300

    Update Question Puller.cs

--- Items/Question Puller.cs
+++ Items/Question Puller.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using GuwbaPrimeAdventure.Connection;
 using GuwbaPrimeAdventure.Guwba;
 namespace GuwbaPrimeAdventure.Item
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(SpriteRenderer), typeof(CircleCollider2D))]
-	[RequireComponent(typeof(IInteractable))]
-	internal sealed class QuestionPuller : StateController
+	internal sealed class QuestionPuller : StateController, IConnector
 	{
 		private SpriteRenderer _spriteRenderer;
+		public PathConnection PathConnection => PathConnection.Item;
 		private new void Awake()
 		{
 			base.Awake();
 			(this._spriteRenderer = this.GetComponent<SpriteRenderer>()).enabled = false;
+			Sender.Include(this);
+		}
+		private new void OnDestroy()
+		{
+			base.OnDestroy();
+			Sender.Exclude(this);
 		}
 		private void OnTriggerEnter2D(Collider2D collision)
 		{
@@ -21,6 +28,12 @@ namespace GuwbaPrimeAdventure.Item
 		{
 			if (GuwbaAstral<CommandGuwba>.EqualObject(collision.gameObject))
 				this._spriteRenderer.enabled = false;
+		}
+		public void Receive(DataConnection data, object additionalData)
+		{
+			Transform parentTransform = (Transform)additionalData;
+			if (parentTransform && parentTransform == this.transform.parent && data.StateForm == StateForm.Action && data.ToggleValue.HasValue)
+				this._spriteRenderer.enabled = data.ToggleValue.Value;
 		}
 	};
 };
