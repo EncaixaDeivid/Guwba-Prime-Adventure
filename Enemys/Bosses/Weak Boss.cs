192740234818678acce36f1fe11fedc05d1aa363
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-07-07 22:57:33 -0300

    Update Weak Boss.cs

--- Enemys/Bosses/Weak Boss.cs
+++ Enemys/Bosses/Weak Boss.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections;
 using GuwbaPrimeAdventure.Data;
 using GuwbaPrimeAdventure.Connection;
-using GuwbaPrimeAdventure.Hud;
 namespace GuwbaPrimeAdventure.Enemy.Boss
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(SpriteRenderer), typeof(Animator), typeof(Transitioner))]
