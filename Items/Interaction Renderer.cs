adfd0a94294bede6f207c389ded19f97c4320009
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-07-09 19:40:43 -0300

    Update Interaction Renderer.cs

--- Items/Interaction Renderer.cs
+++ Items/Interaction Renderer.cs
@@ -1,31 +1,57 @@
 using UnityEngine;
+using GuwbaPrimeAdventure.Connection;
 using GuwbaPrimeAdventure.Guwba;
 namespace GuwbaPrimeAdventure.Item
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(SpriteRenderer), typeof(CircleCollider2D))]
 	[RequireComponent(typeof(IInteractable))]
-	internal sealed class InteractionRenderer : StateController, IImageComponents
+	internal sealed class InteractionRenderer : StateController, IImageComponents, IConnector
 	{
 		private IImagePool _imagePool;
-		[Header("Props")]
+		private bool _isActive = true;
+		private bool _isOnCollision = false;
+		[Header("Image Components")]
 		[SerializeField, Tooltip("The image it will show.")] private Sprite _image;
 		[SerializeField, Tooltip("The offset of the image.")] private Vector2 _imageOffset;
 		public Sprite Image => this._image;
 		public Vector2 ImageOffset => this._imageOffset;
+		public PathConnection PathConnection => PathConnection.Item;
 		private new void Awake()
 		{
 			base.Awake();
 			this._imagePool = EffectsController.CreateImageRenderer(this);
+			Sender.Include(this);
+		}
+		private new void OnDestroy()
+		{
+			base.OnDestroy();
+			Sender.Exclude(this);
 		}
 		private void OnTriggerEnter2D(Collider2D collision)
 		{
-			if (GuwbaAstral<CommandGuwba>.EqualObject(collision.gameObject))
+			if ((this._isOnCollision = GuwbaAstral<CommandGuwba>.EqualObject(collision.gameObject)) && this._isActive)
 				this._imagePool.Pull();
 		}
 		private void OnTriggerExit2D(Collider2D collision)
 		{
-			if (GuwbaAstral<CommandGuwba>.EqualObject(collision.gameObject))
+			if (!(this._isOnCollision = !GuwbaAstral<CommandGuwba>.EqualObject(collision.gameObject)))
 				this._imagePool.Push();
+		}
+		public void Receive(DataConnection data, object additionalData)
+		{
+			GameObject gameObject = additionalData as GameObject;
+			if (gameObject == this.gameObject && data.StateForm == StateForm.Action && data.ToggleValue.HasValue)
+				if (data.ToggleValue.Value)
+				{
+					this._isActive = true;
+					if (this._isOnCollision)
+						this._imagePool.Pull();
+				}
+				else
+				{
+					this._isActive = false;
+					this._imagePool.Push();
+				}
 		}
 	};
 };
