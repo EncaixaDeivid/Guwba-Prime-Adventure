c0a04627b971c5c13390d8a3c73e10f167eae85e
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-07-09 19:21:30 -0300

    Update Image Renderer.cs

--- System/Image Renderer.cs
+++ System/Image Renderer.cs
@@ -9,14 +9,4 @@ namespace GuwbaPrimeAdventure
 		public void Pull() => this._spriteRenderer.enabled = true;
 		public void Push() => this._spriteRenderer.enabled = false;
 	};
-	public interface IImageComponents
-	{
-		public Sprite Image { get; }
-		public Vector2 ImageOffset { get; }
-	};
-	public interface IImagePool
-	{
-		public void Pull();
-		public void Push();
-	};
 };
