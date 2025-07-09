325657382c38eacb8988be6ea389631c92607307
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-07-09 19:37:58 -0300

    Update Interactive Activator.cs

--- Items/Event Items/Interactive Activator.cs
+++ Items/Event Items/Interactive Activator.cs
@@ -1,13 +1,25 @@
 using UnityEngine;
+using GuwbaPrimeAdventure.Connection;
 namespace GuwbaPrimeAdventure.Item.EventItem
 {
 	[DisallowMultipleComponent]
 	internal sealed class InteractiveActivator : Activator, IInteractable
 	{
+		private bool _used = false;
 		public void Interaction()
 		{
 			if (this.Usable)
 				this.Activation();
+			if (this.Usable && !this._used)
+			{
+				this._used = true;
+				Sender sender = Sender.Create();
+				sender.SetToWhereConnection(PathConnection.Item);
+				sender.SetStateForm(StateForm.Action);
+				sender.SetToggle(false);
+				sender.SetAdditionalData(this.gameObject);
+				sender.Send();
+			}
 		}
 	};
 };
