a8c37ea64e9ed7a2674f372795ec6fa53bd570dc
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-07-30 23:50:33 -0300

    Update and rename Command Guwba.cs to Guwba Default Transform.cs

---
+++ Guwba/Guwba Default Transform.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+namespace GuwbaPrimeAdventure.Guwba
+{
+	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Animator), typeof(SortingGroup))]
+	[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D), typeof(CircleCollider2D))]
+	public sealed class GuwbaDefaultTransform : StateController
+	{
+		private static GuwbaDefaultTransform _instance;
+		public static Vector2 Position { get => _instance.transform.position; set => _instance.transform.position = value; }
+		private new void Awake()
+		{
+			base.Awake();
+			if (_instance)
+			{
+				Destroy(this.gameObject, 0.001f);
+				return;
+			}
+			_instance = this;
+		}
+		public static bool EqualObject(params GameObject[] othersObjects)
+		{
+			foreach (GameObject other in othersObjects)
+				if (other == _instance.gameObject)
+					return true;
+			return false;
+		}
+		public static bool EqualObject(params RaycastHit2D[] othersObjects)
+		{
+			foreach (RaycastHit2D other in othersObjects)
+				if (other.collider.gameObject == _instance.gameObject)
+					return true;
+			return false;
+		}
+	};
+};
