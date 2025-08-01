d7fab36368cf7c767d0f258bff99a2af54574f19
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-07-31 21:11:23 -0300

    Update Guwba Damager Attack.cs

--- Guwba/Guwba Damager Attack.cs
+++ Guwba/Guwba Damager Attack.cs
@@ -6,7 +6,10 @@ namespace GuwbaPrimeAdventure.Guwba
 	internal sealed class GuwbaDamagerAttack : StateController
 	{
 		private SpriteRenderer _spriteRenderer;
-		internal UnityAction<IDamageable> Attack { get; set; }
+		[Header("Stat")]
+		[SerializeField, Tooltip("The amount of damage that the attack of Guwba hits.")] private ushort _attackDamage;
+		internal UnityAction<IDamageable, ushort> Attack { get; set; }
+		internal ushort AttackDamage => this._attackDamage;
 		internal float Alpha { get => this._spriteRenderer.color.a; set => this._spriteRenderer.color = new(1f, 1f, 1f, value); }
 		private new void Awake()
 		{
@@ -16,7 +19,7 @@ namespace GuwbaPrimeAdventure.Guwba
 		private void OnTriggerEnter2D(Collider2D other)
 		{
 			if (other.TryGetComponent<IDamageable>(out var damageable))
-				this.Attack.Invoke(damageable);
+				this.Attack.Invoke(damageable, this._attackDamage);
 		}
 	};
 };
