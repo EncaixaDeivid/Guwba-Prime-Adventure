c0245deb35ef3f9b96bb6fd582c93b1d0fb942d2
WasWeividDSS <129303913+WasWeividDSS@users.noreply.github.com>
2025-08-02 21:13:33 -0300

    Update Jump Point.cs

--- Enemys/Bosses/Jump Point.cs
+++ Enemys/Bosses/Jump Point.cs
@@ -19,7 +19,7 @@ namespace GuwbaPrimeAdventure.Enemy.Boss
 		{
 			if (this._hasTarget)
 			{
-				if (GuwbaAstral<VisualGuwba>.EqualObject(other.gameObject))
+				if (GuwbaBaseTransform.EqualObject(other.gameObject))
 					this._getTouch.Invoke(this._touchIndex);
 				return;
 			}
