cbb258a7a97291b71021054f4853bb10aa908b9b
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-08-10 10:57:54 -0300

    Update Camera Occluder.cs

--- System/Camera Occluder.cs
+++ System/Camera Occluder.cs
@@ -7,7 +7,7 @@ namespace GuwbaPrimeAdventure
 	internal sealed class CameraOccluder : StateController
 	{
 		private static CameraOccluder _instance;
-		private Camera _cameraGuwba;
+		private Camera _mainCamera;
 		private BoxCollider2D _cameraCollider;
 		private new void Awake()
 		{
@@ -18,10 +18,10 @@ namespace GuwbaPrimeAdventure
 			}
 			_instance = this;
 			base.Awake();
-			this._cameraGuwba = this.GetComponent<Camera>();
+			this._mainCamera = this.GetComponent<Camera>();
 			this._cameraCollider = this.GetComponent<BoxCollider2D>();
-			float sizeY = this._cameraGuwba.orthographicSize * 2f;
-			float sizeX = sizeY * this._cameraGuwba.aspect;
+			float sizeY = this._mainCamera.orthographicSize * 2f;
+			float sizeX = sizeY * this._mainCamera.aspect;
 			this._cameraCollider.size = new Vector2(sizeX, sizeY);
 			foreach (Collider2D objects in Physics2D.OverlapBoxAll(this.transform.position, this._cameraCollider.size, 0f))
 				this.SetOtherChildren(objects.gameObject, true);
