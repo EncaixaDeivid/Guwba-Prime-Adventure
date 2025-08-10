8f328739949fbb1a778cba1aed669fda13cb8a2d
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-08-10 12:30:03 -0300

    Update Death Screen Hud.cs

--- System/Huds/Death Screen Hud.cs
+++ System/Huds/Death Screen Hud.cs
@@ -8,10 +8,12 @@ namespace GuwbaPrimeAdventure.Hud
 		private static DeathScreenHud _instance;
 		[Header("Elements")]
 		[SerializeField, Tooltip("User interface element.")] private string _textLabel;
+		[SerializeField, Tooltip("User interface element.")] private string _curtainVisual;
 		[SerializeField, Tooltip("User interface element.")] private string _continueButton;
 		[SerializeField, Tooltip("User interface element.")] private string _outLevelButton;
 		[SerializeField, Tooltip("User interface element.")] private string _gameOverButton;
 		internal Label Text { get; private set; }
+		internal VisualElement Curtain { get; private set; }
 		internal Button Continue { get; private set; }
 		internal Button OutLevel { get; private set; }
 		internal Button GameOver { get; private set; }
@@ -25,6 +27,7 @@ namespace GuwbaPrimeAdventure.Hud
 			_instance = this;
 			VisualElement root = this.GetComponent<UIDocument>().rootVisualElement;
 			this.Text = root.Q<Label>(this._textLabel);
+			this.Curtain = root.Q<VisualElement>(this._curtainVisual);
 			this.Continue = root.Q<Button>(this._continueButton);
 			this.OutLevel = root.Q<Button>(this._outLevelButton);
 			this.GameOver = root.Q<Button>(this._gameOverButton);
