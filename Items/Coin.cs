9c145d8da5ebcd818a8bbe15baedeeacd4b222e8
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-08-24 18:36:43 -0300

    Update Coin.cs

--- Items/Coin.cs
+++ Items/Coin.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
 using GuwbaPrimeAdventure.Data;
+using GuwbaPrimeAdventure.Connection;
 namespace GuwbaPrimeAdventure.Item
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(SpriteRenderer), typeof(Animator))]
 	[RequireComponent(typeof(CircleCollider2D))]
-	internal sealed class Coin : StateController, ICollectable
+	internal sealed class Coin : StateController, ICollectable, IConnector
 	{
+		private SpriteRenderer _spriteRenderer;
 		private Animator _animator;
+		private CircleCollider2D _collider;
 		[Header("Condition")]
 		[SerializeField, Tooltip("If this object will be saved as already existent object.")] private bool _saveOnSpecifics;
+		public PathConnection PathConnection => PathConnection.Item;
 		private new void Awake()
 		{
 			base.Awake();
+			this._spriteRenderer = this.GetComponent<SpriteRenderer>();
 			this._animator = this.GetComponent<Animator>();
+			this._collider = this.GetComponent<CircleCollider2D>();
 		}
 		private void OnEnable() => this._animator.enabled = true;
 		private void OnDisable() => this._animator.enabled = false;
@@ -31,7 +37,16 @@ namespace GuwbaPrimeAdventure.Item
 			if (this._saveOnSpecifics && !saveFile.generalObjects.Contains(this.gameObject.name))
 				saveFile.generalObjects.Add(this.gameObject.name);
 			SaveController.WriteSave(saveFile);
-			Destroy(this.gameObject);
+			this._spriteRenderer.enabled = false;
+			this._collider.enabled = false;
+		}
+		public void Receive(DataConnection data, object additionalData)
+		{
+			if (data.StateForm == StateForm.Enable && data.ToggleValue.HasValue && data.ToggleValue.Value)
+			{
+				this._spriteRenderer.enabled = true;
+				this._collider.enabled = true;
+			}
 		}
 	};
 };
