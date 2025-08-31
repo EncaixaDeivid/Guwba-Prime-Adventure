a5a133a5faa5bcf5f08b0b95b9d17ef779bedee1
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-08-31 16:03:08 -0300

    Update Death Enemy.cs

--- Enemys/Death Enemy.cs
+++ Enemys/Death Enemy.cs
@@ -1,25 +1,45 @@
 using UnityEngine;
+using System.Collections;
+using GuwbaPrimeAdventure.Connection;
 using GuwbaPrimeAdventure.Guwba;
 namespace GuwbaPrimeAdventure.Enemy
 {
-	internal sealed class DeathEnemy : EnemyController
+	[DisallowMultipleComponent]
+	internal sealed class DeathEnemy : EnemyController, IDestructible
 	{
 		[Header("Death Enemy")]
 		[SerializeField, Tooltip("The enemy that this enemy will spawn in death.")] private EnemyController _childEnemy;
 		[SerializeField, Tooltip("The projectile that this enemy will spawn in death.")] private Projectile _childProjectile;
 		[SerializeField, Tooltip("If this enemy will die on touch.")] private bool _onTouch;
-		private void OnTriggerEnter2D(Collider2D other)
+		[SerializeField, Tooltip("If this enemy receives no type of damage.")] private float _deathTime;
+		private new void Awake()
 		{
-			if (this._onTouch && CentralizableGuwba.EqualObject(other.gameObject))
-				Destroy(this.gameObject);
+			base.Awake();
+			this._sender.SetStateForm(StateForm.State);
+			this._sender.SetToggle(false);
 		}
-		private new void OnDestroy()
+		private IEnumerator Death()
 		{
-			base.OnDestroy();
+			this._sender.Send();
+			yield return new WaitTime(this, this._deathTime);
 			if (this._childEnemy)
 				Instantiate(this._childEnemy, this.transform.position, Quaternion.identity);
 			if (this._childProjectile)
 				Instantiate(this._childProjectile, this.transform.position, Quaternion.identity);
+		}
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (this._onTouch && CentralizableGuwba.EqualObject(other.gameObject))
+				this.StartCoroutine(Death());
+		}
+		public new bool Damage(ushort damage)
+		{
+			if (this.Health - (short)damage <= 0f)
+			{
+				this.StartCoroutine(Death());
+				return true;
+			}
+			return base.Damage(damage);
 		}
 	};
 };
