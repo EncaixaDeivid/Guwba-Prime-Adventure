b0e56a2a0153b86ebc7b8ffa97a8751b3cc8047c
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-09-07 14:40:02 -0300

    Update Behavior.cs

--- System/Behavior.cs
+++ System/Behavior.cs
@@ -1,4 +1,3 @@
-using UnityEngine;
 namespace GuwbaPrimeAdventure
 {
 	public interface IDestructible
@@ -14,15 +13,5 @@ namespace GuwbaPrimeAdventure
 	public interface ICollectable
 	{
 		public void Collect();
-	};
-	public interface IImageComponents
-	{
-		public Sprite Image { get; }
-		public Vector2 ImageOffset { get; }
-	};
-	public interface IImagePool
-	{
-		public void Pull();
-		public void Push();
 	};
 };
