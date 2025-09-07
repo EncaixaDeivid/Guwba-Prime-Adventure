1fda1048c765a17ca1a60419f3f0faf9103f1e03
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-09-06 21:07:14 -0300

    Update Interactive Activator.cs

--- Items/Event Items/Interactive Activator.cs
+++ Items/Event Items/Interactive Activator.cs
@@ -5,20 +5,20 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 	[DisallowMultipleComponent]
 	internal sealed class InteractiveActivator : Activator, IInteractable
 	{
-		private bool _used = false;
 		public void Interaction()
 		{
 			if (this.Usable)
-				this.Activation();
-			if (this.Usable && !this._used)
 			{
-				this._used = true;
-				Sender sender = Sender.Create();
-				sender.SetToWhereConnection(PathConnection.Item);
-				sender.SetStateForm(StateForm.Action);
-				sender.SetToggle(false);
-				sender.SetAdditionalData(this.gameObject);
-				sender.Send();
+				this.Activation();
+				if (!this.Usable)
+				{
+					Sender sender = Sender.Create();
+					sender.SetToWhereConnection(PathConnection.Item);
+					sender.SetStateForm(StateForm.Action);
+					sender.SetToggle(false);
+					sender.SetAdditionalData(this.gameObject);
+					sender.Send();
+				}
 			}
 		}
 	};
