73e67fd90c04b157b683cae9d15846a48de03fbe
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-09-15 23:26:31 -0300

    Add files via upload

---
+++ Enemies/Bosses/Teleport Point.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Events;
+using GuwbaPrimeAdventure.Guwba;
+namespace GuwbaPrimeAdventure.Enemy.Boss
+{
+	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
+	internal sealed class TeleportPoint : MonoBehaviour
+	{
+		private UnityAction _getTouch;
+		[Header("Interactions")]
+		[SerializeField, Tooltip("If this point will destroy itself after use.")] private bool _destroyAfter;
+		[SerializeField, Tooltip("If this point will trigger with other object.")] private bool _hasTarget;
+		internal void GetTouch(UnityAction getTouch) => this._getTouch = getTouch;
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (this._hasTarget)
+			{
+				if (CentralizableGuwba.EqualObject(other.gameObject))
+					this._getTouch.Invoke();
+				return;
+			}
+			if (other.TryGetComponent<SummonerBoss>(out _))
+				this._getTouch.Invoke();
+			if (this._destroyAfter)
+				Destroy(this.gameObject);
+		}
+	};
+};
