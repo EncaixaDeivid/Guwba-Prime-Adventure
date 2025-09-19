56f59ffc8337fbc76f5960d9347c7028cf1b9a71
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-09-19 19:12:34 -0300

    Update Sender.cs

--- System/Connection/Sender.cs
+++ System/Connection/Sender.cs
@@ -5,14 +5,12 @@ namespace GuwbaPrimeAdventure.Connection
 	{
 		private Sender()
 		{
-			this._toWhereConnection = PathConnection.None;
 			this._stateForm = StateForm.None;
 			this._additionalData = null;
 			this._toggleValue = null;
 			this._numberValue = null;
 		}
 		private static readonly List<IConnector> _connectors = new();
-		private PathConnection _toWhereConnection;
 		private StateForm _stateForm;
 		private object _additionalData;
 		private bool? _toggleValue;
@@ -29,7 +27,6 @@ namespace GuwbaPrimeAdventure.Connection
 		}
 		public static Sender Create() => new();
 		public void SetAdditionalData(object additionalData) => this._additionalData = additionalData;
-		public void SetToWhereConnection(PathConnection toWhereConnection) => this._toWhereConnection = toWhereConnection;
 		public void SetStateForm(StateForm stateForm) => this._stateForm = stateForm;
 		public void SetToggle(bool value) => this._toggleValue = value;
 		public void SetNumber(int value)
@@ -37,11 +34,11 @@ namespace GuwbaPrimeAdventure.Connection
 			uint indexValue = (uint)(value < 0f ? -value : value);
 			this._numberValue = indexValue;
 		}
-		public void Send()
+		public void Send(PathConnection path)
 		{
 			DataConnection dataConnection = new(this._stateForm, this._toggleValue, this._numberValue);
 			foreach (IConnector connector in _connectors)
-				if (connector.PathConnection == this._toWhereConnection)
+				if (connector.PathConnection == path)
 					connector.Receive(dataConnection, this._additionalData);
 		}
 	};
