c908392a3ec262be780de904aa7e0cc00da18d9a
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-09-29 20:35:29 -0300

    Update Settings Controller.cs

--- System/Data/Settings Controller.cs
+++ System/Data/Settings Controller.cs
@@ -4,15 +4,18 @@ namespace GuwbaPrimeAdventure.Data
 {
 	public struct Settings
 	{
+		public Resolution resolution;
+		public FullScreenMode fullScreenMode;
 		public bool fullScreen;
+		public bool dialogToggle;
+		public float dialogSpeed;
+		public float screenBrightness;
 		public bool generalVolumeToggle;
 		public ushort generalVolume;
 		public bool effectsVolumeToggle;
 		public ushort effectsVolume;
 		public bool musicVolumeToggle;
 		public ushort musicVolume;
-		public bool dialogToggle;
-		public float dialogSpeed;
 	};
 	public static class SettingsController
 	{
@@ -28,15 +31,18 @@ namespace GuwbaPrimeAdventure.Data
 		{
 			settings = new Settings()
 			{
+				resolution = Screen.currentResolution,
+				fullScreenMode = FullScreenMode.MaximizedWindow,
 				fullScreen = true,
+				dialogToggle = true,
+				dialogSpeed = .05f,
+				screenBrightness = 1f,
 				generalVolumeToggle = true,
 				generalVolume = 100,
 				effectsVolumeToggle = true,
 				effectsVolume = 100,
 				musicVolumeToggle = true,
-				musicVolume = 100,
-				dialogToggle = true,
-				dialogSpeed = .05f
+				musicVolume = 100
 			};
 			if (File.Exists(SettingsPath))
 				settings = ArchiveEncoder.ReadData<Settings>(SettingsPath);
@@ -45,15 +51,18 @@ namespace GuwbaPrimeAdventure.Data
 		public static void SaveSettings() =>
 			ArchiveEncoder.WriteData(new Settings()
 			{
+				resolution = _settings.resolution,
+				fullScreenMode = _settings.fullScreenMode,
 				fullScreen = _settings.fullScreen,
+				dialogToggle = _settings.dialogToggle,
+				dialogSpeed = _settings.dialogSpeed,
+				screenBrightness = _settings.screenBrightness,
 				generalVolumeToggle = _settings.generalVolumeToggle,
 				generalVolume = _settings.generalVolume,
 				effectsVolumeToggle = _settings.effectsVolumeToggle,
 				effectsVolume = _settings.effectsVolume,
 				musicVolumeToggle = _settings.musicVolumeToggle,
-				musicVolume = _settings.musicVolume,
-				dialogToggle = _settings.dialogToggle,
-				dialogSpeed = _settings.dialogSpeed
+				musicVolume = _settings.musicVolume
 			}, SettingsPath);
 	};
 };
