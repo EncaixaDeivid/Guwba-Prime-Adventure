7f76ec58a0a3e608112d93250647693d2af8efe3
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-09-30 16:44:39 -0300

    Update Effects Controller.cs

--- System/Effects Controller.cs
+++ System/Effects Controller.cs
@@ -1,25 +1,29 @@
 using UnityEngine;
 using UnityEngine.U2D;
+using Unity.Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 namespace GuwbaPrimeAdventure
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Light2DBase))]
+	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Light2DBase), typeof(CinemachineImpulseSource))]
 	public sealed class EffectsController : StateController
 	{
 		private static EffectsController _instance;
 		private List<Light2DBase> _lightsStack;
+		private CinemachineImpulseSource _impulseSource;
 		private bool _canHitStop = true;
+		[SerializeField, Tooltip("The impulse listener attached at camera.")] private CinemachineImpulseListener _impulseListener;
 		private new void Awake()
 		{
+			base.Awake();
 			if (_instance)
 			{
 				Destroy(this.gameObject, 0.001f);
 				return;
 			}
 			_instance = this;
-			base.Awake();
 			this._lightsStack = new List<Light2DBase>() { this.GetComponent<Light2DBase>() };
+			this._impulseSource = this.GetComponent<CinemachineImpulseSource>();
 		}
 		private void PrvateHitStop(float stopTime, float slowTime)
 		{
@@ -52,8 +56,25 @@ namespace GuwbaPrimeAdventure
 			this._lightsStack.Remove(globalLight);
 			this._lightsStack[^1].enabled = true;
 		}
+		private void PrivateShakeScreen(ScreenShakeProfile screenShake)
+		{
+			this._impulseListener.ReactionSettings.Duration = screenShake.Duration;
+			this._impulseListener.ReactionSettings.AmplitudeGain = screenShake.AmplitudeGain;
+			this._impulseListener.ReactionSettings.FrequencyGain = screenShake.FrequencyGain;
+			this._impulseSource.ImpulseDefinition.CustomImpulseShape = screenShake.CustomImpulse;
+			this._impulseSource.DefaultVelocity = screenShake.Velocity;
+			this._impulseSource.ImpulseDefinition.ImpulseType = screenShake.ImpulseType;
+			this._impulseSource.ImpulseDefinition.ImpulseShape = screenShake.ImpulseShape;
+			this._impulseSource.ImpulseDefinition.PropagationSpeed = screenShake.PropagationSpeed;
+			this._impulseSource.ImpulseDefinition.DissipationMode = screenShake.DissipationMode;
+			this._impulseSource.ImpulseDefinition.DissipationDistance = screenShake.DissipationDistance;
+			this._impulseSource.ImpulseDefinition.DissipationRate = screenShake.DissipationRate;
+			this._impulseSource.ImpulseDefinition.ImpulseDuration = screenShake.Time;
+			this._impulseSource.GenerateImpulse(screenShake.Force);
+		}
 		public static void HitStop(float stopTime, float slowTime) => _instance.PrvateHitStop(stopTime, slowTime);
 		public static void OnGlobalLight(Light2DBase globalLight) => _instance.PrivateOnGlobalLight(globalLight);
 		public static void OffGlobalLight(Light2DBase globalLight) => _instance.PrivateOffGlobalLight(globalLight);
+		public static void ShakeScreen(ScreenShakeProfile screenShake) => _instance.PrivateShakeScreen(screenShake);
 	};
 };
