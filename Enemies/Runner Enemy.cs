105b7b49fa30447698084082fc0d5d11ebd7b940
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-02 20:08:41 -0300

    Update and rename Ground Enemy.cs to Runner Enemy.cs

--- Enemies/Runner Enemy.cs
+++ Enemies/Runner Enemy.cs
@@ -1,25 +1,32 @@
 using UnityEngine;
 using System;
 using GuwbaPrimeAdventure.Connection;
+using GuwbaPrimeAdventure.Guwba;
 namespace GuwbaPrimeAdventure.Enemy
 {
-	internal sealed class GroundEnemy : MovingEnemy
+	internal sealed class RunnerEnemy : MovingEnemy
 	{
 		private bool _runTowards = false;
-		private short _movementSide = 1;
+		private bool _stopVelocity = false;
 		private ushort _runnedTimes = 0;
 		private float _timeRun = 0f;
 		private float _dashedTime = 0f;
-		[Header("Ground Enemy")]
+		private float _dashTime = 0f;
+		[Header("Runner Enemy")]
+		[SerializeField, Tooltip("In the react to damage it already have a target.")] private Vector2 _otherTarget;
 		[SerializeField, Tooltip("The distance to check for the block perception.")] private float _blockDistance;
 		[SerializeField, Tooltip("If this enemy will do some action when look to a target.")] private bool _useFaceLook;
+		[SerializeField, Tooltip("If the dash is timed to start when the boss is instantiate.")] private bool _timedDash;
 		[SerializeField, Tooltip("If the target is anything.")] private bool _targetEveryone;
 		[SerializeField, Tooltip("If this enemy will run away from the target.")] private bool _runFromTarget;
 		[SerializeField, Tooltip("If this enemy will run toward the target after the run.")] private bool _runTowardsAfter;
 		[SerializeField, Tooltip("The distance of the detection of target.")] private ushort _faceLookDistance;
+		[SerializeField, Tooltip("The amount of time to wait the timed dash to go.")] private float _timeToDash;
 		[SerializeField, Tooltip("The amount of times this enemy have to run away from the target.")] private ushort _timesToRun;
 		[SerializeField, Tooltip("The amount of time this enemy will run away from or pursue the target.")] private float _runOfTime;
 		[SerializeField, Tooltip("The amount of time this enemy will be dashing upon the target.")] private float _timeDashing;
+		[SerializeField, Tooltip("If the boss can jump while dashing.\nRequires: Jumper Enemy")] private bool _jumpDash;
+		[SerializeField, Tooltip("If the boss will target other object when react to damage.")] private bool _useOtherTarget;
 		private new void Awake()
 		{
 			base.Awake();
@@ -36,13 +43,22 @@ namespace GuwbaPrimeAdventure.Enemy
 				if (this._stoppedTime >= this._stopTime)
 				{
 					this._stoppedTime = 0f;
-					this._stopWorking = false;
+					this._stopVelocity = this._stopWorking = false;
 					this._isDashing = true;
 				}
 				return;
 			}
 			if (this._stopWorking)
 				return;
+			if (this._timedDash && !this._isDashing)
+			{
+				this._dashTime += Time.deltaTime;
+				if (this._dashTime >= this._timeToDash)
+				{
+					this._dashTime = 0f;
+					this._isDashing = true;
+				}
+			}
 			if (this._runFromTarget)
 			{
 				if (this._timeRun > 0f)
@@ -65,11 +81,13 @@ namespace GuwbaPrimeAdventure.Enemy
 		}
 		private void FixedUpdate()
 		{
-			if (this._stopWorking || this.IsStunned)
+			if (this._stopVelocity)
 			{
+				this._stopVelocity = false;
 				this._rigidybody.linearVelocity = Vector2.zero;
-				return;
 			}
+			if (this._stopWorking || this.IsStunned)
+				return;
 			Vector2 right = this.transform.right * this._movementSide;
 			if (this._isDashing)
 			{
@@ -78,6 +96,8 @@ namespace GuwbaPrimeAdventure.Enemy
 				{
 					this._dashedTime = 0f;
 					this._isDashing = false;
+					this._sender.SetToggle(true);
+					this._sender.Send(PathConnection.Enemy);
 				}
 			}
 			else
@@ -103,33 +123,59 @@ namespace GuwbaPrimeAdventure.Enemy
 				if (this._runTowards)
 					this._runTowards = false;
 				else
-					this.transform.localScale = new Vector3()
-					{
-						x = (this._movementSide *= -1) < 0f ? -MathF.Abs(this.transform.localScale.x) : MathF.Abs(this.transform.localScale.x),
-						y = this.transform.localScale.y,
-						z = this.transform.localScale.z
-					};
+					this._movementSide *= -1;
 			}
 			float xAxis = this.transform.position.x + this._collider.bounds.extents.x * right.x;
 			float yAxis = this.transform.position.y + this._collider.bounds.extents.y * -this.transform.up.y;
 			if (!Physics2D.Raycast(new Vector2(xAxis, yAxis), -this.transform.up, this._blockDistance, this._groundLayer) || blockPerception)
-				this.transform.localScale = new Vector3()
-				{
-					x = (this._movementSide *= -1) < 0f ? -MathF.Abs(this.transform.localScale.x) : MathF.Abs(this.transform.localScale.x),
-					y = this.transform.localScale.y,
-					z = this.transform.localScale.z
-				};
+				this._movementSide *= -1;
 			if (this._detected && !this._isDashing)
 				if (this._detectionStop)
 				{
-					this._stopWorking = true;
+					this._stopVelocity = this._stopWorking = true;
+					this._sender.SetToggle(this._jumpDash);
+					this._sender.Send(PathConnection.Enemy);
 					if (this._stopToShoot)
 						this._sender.Send(PathConnection.Enemy);
 					return;
 				}
 				else if (this._shootDetection)
 					this._sender.Send(PathConnection.Enemy);
+			this.transform.localScale = new Vector3()
+			{
+				x = this._movementSide < 0f ? -MathF.Abs(this.transform.localScale.x) : MathF.Abs(this.transform.localScale.x),
+				y = this.transform.localScale.y,
+				z = this.transform.localScale.z
+			};
 			this._rigidybody.linearVelocity = this._detected ? right * this._dashSpeed : right * this._movementSpeed;
+		}
+		public new void Receive(DataConnection data, object additionalData)
+		{
+			base.Receive(data, additionalData);
+			EnemyController[] enemies = (EnemyController[])additionalData;
+			if (enemies != null)
+				foreach (EnemyController enemy in enemies)
+					if (enemy != this)
+						return;
+			if (data.StateForm == StateForm.State && data.ToggleValue.HasValue)
+				this._stopVelocity = !data.ToggleValue.Value;
+			else if (data.StateForm == StateForm.Action && this._reactToDamage)
+			{
+				Vector2 targetPosition;
+				if (this._useOtherTarget)
+					targetPosition = this._otherTarget;
+				else
+					targetPosition = CentralizableGuwba.Position;
+				this._movementSide = (short)(targetPosition.x < this.transform.position.x ? -1f : 1f);
+				if (this._detectionStop)
+				{
+					this._stopVelocity = this._stopWorking = true;
+					this._sender.SetToggle(this._jumpDash);
+					this._sender.Send(PathConnection.Enemy);
+					if (this._stopToShoot)
+						this._sender.Send(PathConnection.Enemy);
+				}
+			}
 		}
 	};
 };
