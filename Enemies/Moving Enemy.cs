cd0fc3eceb9bd13882ac49d8fa9ab3a08b9d33c8
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-02 20:08:18 -0300

    Update Moving Enemy.cs

--- Enemies/Moving Enemy.cs
+++ Enemies/Moving Enemy.cs
@@ -3,14 +3,15 @@ using System;
 using GuwbaPrimeAdventure.Connection;
 namespace GuwbaPrimeAdventure.Enemy
 {
-	[DisallowMultipleComponent]
 	internal abstract class MovingEnemy : EnemyController, IConnector
 	{
 		private Vector2 _guardVelocity = new();
 		protected bool _detected = false;
 		protected bool _isDashing = false;
 		protected float _stoppedTime = 0f;
+		protected short _movementSide = 1;
 		[Header("Moving Enemy")]
+		[SerializeField, Tooltip("Size of collider for checking the ground below the feet.")] private float _groundChecker;
 		[SerializeField, Tooltip("The speed of the enemy to moves.")] protected ushort _movementSpeed;
 		[SerializeField, Tooltip("The amount of speed of the dash.")] protected ushort _dashSpeed;
 		[SerializeField, Tooltip("If this enemy will moves firstly to the left.")] private bool _invertMovementSide;
@@ -34,19 +35,27 @@ namespace GuwbaPrimeAdventure.Enemy
 			base.OnEnable();
 			this._rigidybody.linearVelocity = this._guardVelocity;
 		}
-		private new void OnDisable()
+		private void OnDisable()
 		{
-			base.OnDisable();
 			this._guardVelocity = this._rigidybody.linearVelocity;
 			this._rigidybody.linearVelocity = Vector2.zero;
+		}
+		protected bool SurfacePerception()
+		{
+			float yPoint = this.transform.position.y - this._collider.bounds.extents.y + this._collider.offset.y - this._groundChecker / 2f;
+			Vector2 origin = new(this.transform.position.x, yPoint);
+			Vector2 size = new(this._collider.bounds.size.x - this._groundChecker, this._groundChecker);
+			float angle = this.transform.rotation.z * Mathf.Rad2Deg;
+			return Physics2D.BoxCast(origin, size, angle, -this.transform.up, this._groundChecker, this._groundLayer);
 		}
 		public new void Receive(DataConnection data, object additionalData)
 		{
 			base.Receive(data, additionalData);
-			if (additionalData as GameObject != this.gameObject)
-				return;
-			if (data.StateForm == StateForm.State && data.ToggleValue.HasValue)
-				this._stopWorking = !data.ToggleValue.Value;
+			EnemyController[] enemies = (EnemyController[])additionalData;
+			if (enemies != null)
+				foreach (EnemyController enemy in enemies)
+					if (enemy == this && data.StateForm == StateForm.State && data.ToggleValue.HasValue)
+						this._stopWorking = !data.ToggleValue.Value;
 		}
 	};
 };
