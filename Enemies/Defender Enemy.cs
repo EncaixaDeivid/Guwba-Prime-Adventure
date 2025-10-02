ceb1c76f5788011fe1f17a593bd93c4f1c17b715
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-02 20:12:30 -0300

    Update Defender Enemy.cs

--- Enemies/Defender Enemy.cs
+++ Enemies/Defender Enemy.cs
@@ -72,8 +72,11 @@ namespace GuwbaPrimeAdventure.Enemy
 		public new void Receive(DataConnection data, object additionalData)
 		{
 			base.Receive(data, additionalData);
-			if (additionalData as GameObject != this.gameObject)
-				return;
+			EnemyController[] enemies = (EnemyController[])additionalData;
+			if (enemies != null)
+				foreach (EnemyController enemy in enemies)
+					if (enemy != this)
+						return;
 			if (data.StateForm == StateForm.Action && data.ToggleValue.HasValue)
 				if (this._useAlternatedTime && data.ToggleValue.Value)
 					this._invencible = true;
