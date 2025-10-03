b84fba8260def7d9328c077d82feee495b357e47
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-03 19:24:40 -0300

    Update Enemy Controller.cs

--- Enemies/Enemy Controller.cs
+++ Enemies/Enemy Controller.cs
@@ -6,55 +6,38 @@ namespace GuwbaPrimeAdventure.Enemy
 	[RequireComponent(typeof(Transform), typeof(Rigidbody2D)), RequireComponent(typeof(Collider2D))]
 	internal abstract class EnemyController : StateController, IConnector, IDestructible
     {
-		protected SpriteRenderer _spriteRenderer;
 		protected Rigidbody2D _rigidybody;
 		protected Collider2D _collider;
 		protected readonly Sender _sender = Sender.Create();
 		private float _guardGravityScale = 0f;
 		private float _stunTimer = 0f;
+		private short _vitality;
 		private short _armorResistance = 0;
 		protected bool _stopWorking = false;
 		[Header("Enemy Controller")]
-		[SerializeField, Tooltip("The bosses to send messages.")] private EnemyController[] _enemiesToSend;
-		[SerializeField, Tooltip("The layer mask to identify the ground.")] protected LayerMask _groundLayer;
-		[SerializeField, Tooltip("The layer mask to identify the target of the attacks.")] protected LayerMask _targetLayerMask;
-		[SerializeField, Tooltip("The vitality of the enemy.")] private short _vitality;
-		[SerializeField, Tooltip("The amount of stun that this enemy can resists.")] private ushort _hitResistance;
-		[SerializeField, Tooltip("The amount of damage that the enemy hit.")] private ushort _damage;
-		[SerializeField, Tooltip("If this enemy receives no type of damage.")] private bool _noDamage;
-		[SerializeField, Tooltip("If this enemy will fade away over time.")] private bool _fadeOverTime;
-		[SerializeField, Tooltip("The amount of time this enemy will fade away.")] private float _timeToFadeAway;
-		[SerializeField, Tooltip("The amount of time this enemy will stun.")] private float _stunTime;
-		[SerializeField, Tooltip("The amount of time this enemy will be stunned when armor be broken.")] private float _stunnedTime;
-		[SerializeField, Tooltip("The amount of time to stop the game when hit is given.")] private float _hitStopTime;
-		[SerializeField, Tooltip("The amount of time to slow the game when hit is given.")] private float _hitSlowTime;
-		[SerializeField, Tooltip("If this boss will react to any damage taken.")] protected bool _reactToDamage;
-		[SerializeField, Tooltip("If this boss has a index atribute to use.")] private bool _hasIndex;
-		[SerializeField, Tooltip("The index to a event to a boss make.")] private ushort _indexEvent;
-		[SerializeField, Tooltip("If this boss will start a trancision.")] private bool _isTransitioner;
-		[SerializeField, Tooltip("If this boss have any dialog to start after his death.")] private bool _haveDialog;
-		[SerializeField, Tooltip("If this object will be saved as already existent object.")] private bool _saveOnSpecifics;
+		[SerializeField, Tooltip("The control statitics of this enemy.")] private EnemyStatistics _control;
+		[SerializeField, Tooltip("The enemies to send messages.")] private EnemyController[] _enemiesToSend;
 		protected bool IsStunned { get; private set; }
 		public PathConnection PathConnection => PathConnection.Enemy;
 		public short Health => this._vitality;
 		protected new void Awake()
 		{
 			base.Awake();
-			this._spriteRenderer = this.GetComponent<SpriteRenderer>();
 			this._rigidybody = this.GetComponent<Rigidbody2D>();
 			this._collider = this.GetComponent<Collider2D>();
 			this._sender.SetAdditionalData(this._enemiesToSend);
 			this._guardGravityScale = this._rigidybody.gravityScale;
-			this._armorResistance = (short)this._hitResistance;
-			if (this._fadeOverTime)
-				Destroy(this.gameObject, this._timeToFadeAway);
+			this._vitality = (short)this._control.Vitality;
+			this._armorResistance = (short)this._control.HitResistance;
+			if (this._control.FadeOverTime)
+				Destroy(this.gameObject, this._control.TimeToFadeAway);
 			Sender.Include(this);
 		}
 		private new void OnDestroy()
 		{
 			base.OnDestroy();
 			SaveController.Load(out SaveFile saveFile);
-			if (this._saveOnSpecifics && !saveFile.generalObjects.Contains(this.gameObject.name))
+			if (this._control.SaveOnSpecifics&& !saveFile.generalObjects.Contains(this.gameObject.name))
 			{
 				saveFile.generalObjects.Add(this.gameObject.name);
 				SaveController.WriteSave(saveFile);
@@ -73,23 +56,23 @@ namespace GuwbaPrimeAdventure.Enemy
 		}
 		private void OnTrigger(GameObject collisionObject)
 		{
-			if (collisionObject.TryGetComponent<IDestructible>(out var destructible) && destructible.Hurt(this._damage))
+			if (collisionObject.TryGetComponent<IDestructible>(out var destructible) && destructible.Hurt(this._control.Damage))
 			{
-				destructible.Stun(this._damage, this._stunTime);
-				EffectsController.HitStop(this._hitStopTime, this._hitSlowTime);
+				destructible.Stun(this._control.Damage, this._control.StunTime);
+				EffectsController.HitStop(this._control.Physics.HitStopTime, this._control.Physics.HitSlowTime);
 			}
 		}
 		private void OnTriggerEnter2D(Collider2D other) => this.OnTrigger(other.gameObject);
 		private void OnTriggerStay2D(Collider2D other) => this.OnTrigger(other.gameObject);
 		public bool Hurt(ushort damage)
 		{
-			if (this._noDamage || damage <= 0)
+			if (this._control.NoDamage|| damage <= 0)
 				return false;
-			if (this._reactToDamage)
-				if (this._hasIndex)
+			if (this._control.ReactToDamage)
+				if (this._control.HasIndex)
 				{
 					this._sender.SetStateForm(StateForm.Action);
-					this._sender.SetNumber(this._indexEvent);
+					this._sender.SetNumber(this._control.IndexEvent);
 					this._sender.Send(PathConnection.Enemy);
 				}
 				else
@@ -109,8 +92,8 @@ namespace GuwbaPrimeAdventure.Enemy
 			this._stunTimer = stunTime;
 			if ((this._armorResistance -= (short)stunStength) <= 0f)
 			{
-				this._stunTimer = this._stunnedTime;
-				this._armorResistance = (short)this._hitResistance;
+				this._stunTimer = this._control.StunnedTime;
+				this._armorResistance = (short)this._control.HitResistance;
 			}
 		}
 		public void Receive(DataConnection data, object additionalData)
