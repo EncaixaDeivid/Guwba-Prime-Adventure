b797e56649356be7bd7f06526b87273e7f73b8cc
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-03 19:25:48 -0300

    Update Flying Enemy.cs

--- Enemies/Flying Enemy.cs
+++ Enemies/Flying Enemy.cs
@@ -12,31 +12,24 @@ namespace GuwbaPrimeAdventure.Enemy
 		private bool _returnDash = false;
 		private ushort _pointIndex = 0;
 		[Header("Flying Enemy")]
-		[SerializeField, Tooltip("The target this enemy have to pursue.")] private GameObject _target;
-		[SerializeField, Tooltip("How far this enemy detect any target.")] private float _radiusDetection;
-		[SerializeField, Tooltip("The distance to stay away from the target.")] private float _targetDistance;
-		[SerializeField, Tooltip("The time this enemy stay alive during the endless pursue.")] private float _fadeTime;
-		[SerializeField, Tooltip("If this enemy will pursue the target until fade.")] private bool _endlessPursue;
-		[Header("Trail Stats")]
+		[SerializeField, Tooltip("The flying statitics of this enemy.")] private FlyingStatistics _statistics;
 		[SerializeField, Tooltip("The points that this enemy have to make.")] private Vector2[] _trail;
-		[SerializeField, Tooltip("The amount of speed that this enemy moves to go back to the original point.")] private float _returnSpeed;
-		[SerializeField, Tooltip("If this enemy will repeat the same way it makes before.")] private bool _repeatWay;
 		private new void Awake()
 		{
 			base.Awake();
 			this._pointOrigin = this.transform.position;
-			if (this._endlessPursue)
-				Destroy(this.gameObject, this._fadeTime);
+			if (this._statistics.EndlessPursue)
+				Destroy(this.gameObject, this._statistics.FadeTime);
 		}
 		private new void Update()
 		{
 			base.Update();
 			if (this.IsStunned)
 				return;
-			if (this._detectionStop && this._stopWorking)
+			if (this._statistics.DetectionStop && this._stopWorking)
 			{
 				this._stoppedTime += Time.deltaTime;
-				if (this._stoppedTime >= this._stopTime)
+				if (this._stoppedTime >= this._statistics.StopTime)
 				{
 					this._stoppedTime = 0f;
 					this._stopWorking = false;
@@ -47,28 +40,28 @@ namespace GuwbaPrimeAdventure.Enemy
 		private void Chase()
 		{
 			float maxDistanceDelta;
-			if (!this._isDashing && Vector2.Distance(this.transform.position, this._targetPoint) <= this._targetDistance)
+			if (!this._isDashing && Vector2.Distance(this.transform.position, this._targetPoint) <= this._statistics.TargetDistance)
 			{
 				if (this._returnDash)
 				{
-					maxDistanceDelta = Time.fixedDeltaTime * this._dashSpeed;
+					maxDistanceDelta = Time.fixedDeltaTime * this._statistics.DashSpeed;
 					this.transform.position = Vector2.MoveTowards(this.transform.position, this._pointOrigin, maxDistanceDelta);
-					this._returnDash = Vector2.Distance(this.transform.position, this._targetPoint) <= this._targetDistance;
+					this._returnDash = Vector2.Distance(this.transform.position, this._targetPoint) <= this._statistics.TargetDistance;
 					return;
 				}
-				else if (this._detectionStop)
+				else if (this._statistics.DetectionStop)
 				{
 					this._stopWorking = true;
-					if (this._stopToShoot)
+					if (this._statistics.StopToShoot)
 						this._sender.Send(PathConnection.Enemy);
 					return;
 				}
-				else if (this._shootDetection)
+				else if (this._statistics.ShootDetection)
 					this._sender.Send(PathConnection.Enemy);
 			}
 			else if (!this._isDashing && this._returnDash)
 				this._returnDash = false;
-			maxDistanceDelta = this._isDashing ? Time.fixedDeltaTime * this._dashSpeed : Time.fixedDeltaTime * this._movementSpeed;
+			maxDistanceDelta = Time.fixedDeltaTime * (this._isDashing ? this._statistics.DashSpeed : this._statistics.MovementSpeed);
 			this.transform.position = Vector2.MoveTowards(this.transform.position, this._targetPoint, maxDistanceDelta);
 			if (this._isDashing && Vector2.Distance(this.transform.position, this._targetPoint) <= 0f)
 				this._isDashing = !(this._returnDash = true);
@@ -86,13 +79,13 @@ namespace GuwbaPrimeAdventure.Enemy
 					y = this.transform.localScale.y,
 					z = this.transform.localScale.z
 				};
-				float maxDistanceDelta = this._returnSpeed * Time.fixedDeltaTime;
+				float maxDistanceDelta = this._statistics.ReturnSpeed * Time.fixedDeltaTime;
 				this.transform.position = Vector2.MoveTowards(this.transform.position, this._pointOrigin, maxDistanceDelta);
 			}
 			else
 			{
 				Vector2 target = this._trail[this._pointIndex];
-				if (this._repeatWay)
+				if (this._statistics.RepeatWay)
 				{
 					if ((ushort)Vector2.Distance(this.transform.localPosition, target) <= 0f)
 						this._pointIndex = (ushort)(this._pointIndex < this._trail.Length - 1f ? this._pointIndex + 1f : 0f);
@@ -109,7 +102,7 @@ namespace GuwbaPrimeAdventure.Enemy
 						this._pointIndex -= 1;
 					this._normal = this._pointIndex == 0f;
 				}
-				float maxDistanceDelta = Time.fixedDeltaTime * this._movementSpeed;
+				float maxDistanceDelta = Time.fixedDeltaTime * this._statistics.MovementSpeed;
 				bool valid = target.x < this.transform.localPosition.x;
 				this.transform.localScale = new Vector3()
 				{
@@ -125,30 +118,32 @@ namespace GuwbaPrimeAdventure.Enemy
 		{
 			if (this._stopWorking || this.IsStunned)
 				return;
-			if (this._target)
+			if (this._statistics.Target)
 			{
-				this._targetPoint = this._target.transform.position;
-				float maxDistanceDelta = Time.fixedDeltaTime * this._movementSpeed;
+				this._targetPoint = this._statistics.Target.transform.position;
+				float maxDistanceDelta = Time.fixedDeltaTime * this._statistics.MovementSpeed;
 				this.transform.position = Vector2.MoveTowards(this.transform.position, this._targetPoint, maxDistanceDelta);
 				return;
 			}
-			if (this._endlessPursue)
+			if (this._statistics.EndlessPursue)
 			{
-				float maxDistanceDelta = Time.fixedDeltaTime * this._movementSpeed;
+				float maxDistanceDelta = Time.fixedDeltaTime * this._statistics.MovementSpeed;
 				this.transform.position = Vector2.MoveTowards(this.transform.position, CentralizableGuwba.Position, maxDistanceDelta);
 				return;
 			}
+			LayerMask groundLayer = this._statistics.Physics.GroundLayer;
+			LayerMask targetLayer = this._statistics.Physics.TargetLayer;
 			if (!this._isDashing)
 				this._detected = false;
-			if (this._targetLayerMask > -1f && !this._isDashing)
-				foreach (Collider2D collider in Physics2D.OverlapCircleAll(this._pointOrigin, this._radiusDetection, this._targetLayerMask))
+			if (targetLayer > -1f && !this._isDashing)
+				foreach (Collider2D collider in Physics2D.OverlapCircleAll(this._pointOrigin, this._statistics.RadiusDetection, targetLayer))
 					if (CentralizableGuwba.EqualObject(collider.gameObject))
 					{
 						this._targetPoint = collider.transform.position;
 						Vector2 origin = this.transform.position;
 						Vector2 size = this._collider.bounds.size;
 						float targetDistance = Vector2.Distance(this.transform.position, this._targetPoint);
-						this._detected = !Physics2D.BoxCast(origin, size, 0f, this._targetPoint, targetDistance, this._groundLayer);
+						this._detected = !Physics2D.BoxCast(origin, size, 0f, this._targetPoint, targetDistance, groundLayer);
 						if (this._detected)
 						{
 							bool valid = collider.transform.position.x < this.transform.position.x;
