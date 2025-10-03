1a0f7f66aef53b5373673d28c0180d59ba8a61a3
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-03 19:24:59 -0300

    Update Moving Enemy.cs

--- Enemies/Moving Enemy.cs
+++ Enemies/Moving Enemy.cs
@@ -11,14 +11,8 @@ namespace GuwbaPrimeAdventure.Enemy
 		protected float _stoppedTime = 0f;
 		protected short _movementSide = 1;
 		[Header("Moving Enemy")]
-		[SerializeField, Tooltip("Size of collider for checking the ground below the feet.")] private float _groundChecker;
-		[SerializeField, Tooltip("The speed of the enemy to moves.")] protected ushort _movementSpeed;
-		[SerializeField, Tooltip("The amount of speed of the dash.")] protected ushort _dashSpeed;
+		[SerializeField, Tooltip("The moving statitics of this enemy.")] private MovingStatistics _moving;
 		[SerializeField, Tooltip("If this enemy will moves firstly to the left.")] private bool _invertMovementSide;
-		[SerializeField, Tooltip("If this enemy will stop on detection of the target.")] protected bool _detectionStop;
-		[SerializeField, Tooltip("If this enemy will shoot a projectile on detection.\nRequires: Shooter Enemy")] protected bool _shootDetection;
-		[SerializeField, Tooltip("If this enemy will stop to shoot a projectile.\nRequires: Shooter Enemy")] protected bool _stopToShoot;
-		[SerializeField, Tooltip("The amount of time this enemy will stop on detection.")] protected float _stopTime;
 		protected new void Awake()
 		{
 			base.Awake();
@@ -42,11 +36,12 @@ namespace GuwbaPrimeAdventure.Enemy
 		}
 		protected bool SurfacePerception()
 		{
-			float yPoint = this.transform.position.y - this._collider.bounds.extents.y + this._collider.offset.y - this._groundChecker / 2f;
+			float groundChecker = this._moving.Physics.GroundChecker;
+			float yPoint = this.transform.position.y - this._collider.bounds.extents.y + this._collider.offset.y - groundChecker / 2f;
 			Vector2 origin = new(this.transform.position.x, yPoint);
-			Vector2 size = new(this._collider.bounds.size.x - this._groundChecker, this._groundChecker);
-			float angle = this.transform.rotation.z * Mathf.Rad2Deg;
-			return Physics2D.BoxCast(origin, size, angle, -this.transform.up, this._groundChecker, this._groundLayer);
+			Vector2 size = new(this._collider.bounds.size.x - groundChecker, groundChecker);
+			float angle = this.transform.eulerAngles.z;
+			return Physics2D.BoxCast(origin, size, angle, -this.transform.up, groundChecker, this._moving.Physics.GroundLayer);
 		}
 		public new void Receive(DataConnection data, object additionalData)
 		{
