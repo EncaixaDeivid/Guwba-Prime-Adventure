04abddc31eab63878b0bb391b373bde0784d85d3
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-04 20:13:17 -0300

    Update Summon Object.cs

--- Enemies/Summon Object.cs
+++ Enemies/Summon Object.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 namespace GuwbaPrimeAdventure.Enemy
 {
-	[CreateAssetMenu(fileName = "Summon", menuName = "Scriptable Objects/Summon", order = 0)]
+	[CreateAssetMenu(fileName = "Enemy Summon", menuName = "Enemy Statistics/Summon", order = 7)]
 	public sealed class SummonObject : ScriptableObject
 	{
-		[Header("Components Stats")]
+		[Header("Components Statistics")]
 		[SerializeField, Tooltip("The enemy that will be instantiate.")] private GameObject[] _summons;
 		[SerializeField, Tooltip("The points that the instance can be instantiate.")] private Vector2[] _summonPoints;
 		[SerializeField, Tooltip("The amount of time to execute the instance.")] private ushort _summonTime;
@@ -16,7 +16,6 @@ namespace GuwbaPrimeAdventure.Enemy
 		[SerializeField, Tooltip("If the timed summon can be stopped.")] private bool _stopTimedSummon;
 		[SerializeField, Tooltip("If the timed summon will stop permanently.")] private bool _stopPermanently;
 		[SerializeField, Tooltip("If the instanciation will be in the same point as the summoner.")] private bool _self;
-		[SerializeField, Tooltip("If the instantiation will be for every point for the amount of quantity.")] private bool _sequential;
 		[SerializeField, Tooltip("If the instantiation will be randomized at one of the points.")] private bool _random;
 		public GameObject[] Summons => this._summons;
 		public Vector2[] SummonPoints => this._summonPoints;
@@ -29,7 +28,6 @@ namespace GuwbaPrimeAdventure.Enemy
 		public bool StopTimedSummon => this._stopTimedSummon;
 		public bool StopPermanently => this._stopPermanently;
 		public bool Self => this._self;
-		public bool Sequential => this._sequential;
 		public bool Random => this._random;
 	};
 };
