77253c1dfd1368b1edc71d06d345023e35efb796
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-04 20:10:32 -0300

    Update Flying Statistics.cs

--- Enemies/Utilities/Flying Statistics.cs
+++ Enemies/Utilities/Flying Statistics.cs
@@ -12,13 +12,11 @@ namespace GuwbaPrimeAdventure.Enemy
 		[SerializeField, Tooltip("If this enemy will pursue the target until fade.")] private bool _endlessPursue;
 		[Header("Trail")]
 		[SerializeField, Tooltip("The amount of speed that this enemy moves to go back to the original point.")] private float _returnSpeed;
-		[SerializeField, Tooltip("If this enemy will repeat the same way it makes before.")] private bool _repeatWay;
 		internal GameObject Target => this._target;
 		internal float RadiusDetection => this._radiusDetection;
 		internal float TargetDistance => this._targetDistance;
 		internal float FadeTime => this._fadeTime;
 		internal bool EndlessPursue => this._endlessPursue;
 		internal float ReturnSpeed => this._returnSpeed;
-		internal bool RepeatWay => this._repeatWay;
 	};
 };
