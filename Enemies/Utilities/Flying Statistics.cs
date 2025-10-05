cb5d3c91beb7f22ef9fa4b571a7be48e332aac35
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-05 12:39:19 -0300

    Update Flying Statistics.cs

--- Enemies/Utilities/Flying Statistics.cs
+++ Enemies/Utilities/Flying Statistics.cs
@@ -6,14 +6,12 @@ namespace GuwbaPrimeAdventure.Enemy
 	{
 		[Header("Flying Enemy")]
 		[SerializeField, Tooltip("The target this enemy have to pursue.")] private GameObject _target;
-		[SerializeField, Tooltip("How far this enemy detect any target.")] private float _radiusDetection;
 		[SerializeField, Tooltip("The distance to stay away from the target.")] private float _targetDistance;
 		[SerializeField, Tooltip("The time this enemy stay alive during the endless pursue.")] private float _fadeTime;
 		[SerializeField, Tooltip("If this enemy will pursue the target until fade.")] private bool _endlessPursue;
 		[Header("Trail")]
 		[SerializeField, Tooltip("The amount of speed that this enemy moves to go back to the original point.")] private float _returnSpeed;
 		internal GameObject Target => this._target;
-		internal float RadiusDetection => this._radiusDetection;
 		internal float TargetDistance => this._targetDistance;
 		internal float FadeTime => this._fadeTime;
 		internal bool EndlessPursue => this._endlessPursue;
