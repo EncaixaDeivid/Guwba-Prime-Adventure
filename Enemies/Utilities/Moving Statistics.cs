8ad6e1badf26246f428bc9f7e82f4f551d4bf4ce
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-05 12:38:52 -0300

    Update Moving Statistics.cs

--- Enemies/Utilities/Moving Statistics.cs
+++ Enemies/Utilities/Moving Statistics.cs
@@ -7,12 +7,16 @@ namespace GuwbaPrimeAdventure.Enemy
 		[SerializeField, Tooltip("The physics of the enemy.")] private EnemyPhysics _physics;
 		[SerializeField, Tooltip("The speed of the enemy to moves.")] private ushort _movementSpeed;
 		[SerializeField, Tooltip("The amount of speed of the dash.")] private ushort _dashSpeed;
+		[SerializeField, Tooltip("The distance of the detection of target.")] private ushort _lookDistance;
+		[SerializeField, Tooltip("If this enemy will do some action when look to a target.")] private bool _lookPerception;
 		[SerializeField, Tooltip("If this enemy will stop on detection of the target.")] private bool _detectionStop;
 		[SerializeField, Tooltip("If this enemy will react to any damage taken.")] private bool _reactToDamage;
 		[SerializeField, Tooltip("The amount of time this enemy will stop on detection.")] private float _stopTime;
 		internal EnemyPhysics Physics => this._physics;
 		internal ushort MovementSpeed => this._movementSpeed;
 		internal ushort DashSpeed => this._dashSpeed;
+		internal ushort LookDistance => this._lookDistance;
+		internal bool LookPerception => this._lookPerception;
 		internal bool DetectionStop => this._detectionStop;
 		internal bool ReactToDamage => this._reactToDamage;
 		internal float StopTime => this._stopTime;
