75e62f1e419cf3121476d0403f1a5832116d7fe7
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-06 15:40:05 -0300

    Update and rename Centralizable Guwba.cs to Guwba Centralizer.cs

--- Characters/Guwba Centralizer.cs
+++ Characters/Guwba Centralizer.cs
@@ -4,9 +4,9 @@ namespace GuwbaPrimeAdventure.Character
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Animator), typeof(SortingGroup))]
 	[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D), typeof(CircleCollider2D))]
-	public sealed class CentralizableGuwba : StateController
+	public sealed class GuwbaCentralizer : StateController
 	{
-		private static CentralizableGuwba _instance;
+		private static GuwbaCentralizer _instance;
 		public static Vector2 Position { get => _instance.transform.position; set => _instance.transform.position = value; }
 		private new void Awake()
 		{
