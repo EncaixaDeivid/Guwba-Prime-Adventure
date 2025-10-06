4f79a865a8e6895cf09db2e014e69eb5968de42c
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-06 00:14:25 -0300

    Update Jumper Enemy.cs

--- Enemies/Jumper Enemy.cs
+++ Enemies/Jumper Enemy.cs
@@ -8,16 +8,33 @@ namespace GuwbaPrimeAdventure.Enemy
 	internal sealed class JumperEnemy : MovingEnemy, IConnector
 	{
 		private Animator _animator;
+		private bool _isJumping;
 		private bool _stopJump = false;
 		[Header("Jumper Enemy")]
 		[SerializeField, Tooltip("The jumper statitics of this enemy.")] private JumperStatistics _statistics;
-		private void HighJump(Vector2 otherTarget, bool useTarget)
+		private void BasicJump(Vector2 target)
+		{
+			this.StartCoroutine(Jump());
+			IEnumerator Jump()
+			{
+				if (this._statistics.DetectionStop)
+				{
+					this._isJumping = true;
+					this._sender.SetToggle(false);
+					this._sender.Send(PathConnection.Enemy);
+					this._rigidybody.linearVelocityX = 0f;
+					yield return new WaitTime(this, this._statistics.StopTime);
+				}
+				Vector2 direction = (target + Vector2.up * this._statistics.JumpHeight - (Vector2)this.transform.position).normalized;
+				this._rigidybody.AddForce(this._rigidybody.mass * this._statistics.JumpStrenght * direction, ForceMode2D.Impulse);
+			}
+		}
+		private void FollowJump(Vector2 otherTarget, bool useTarget)
 		{
 			this.StartCoroutine(FollowTarget());
 			IEnumerator FollowTarget()
 			{
 				yield return new WaitUntil(() => !this.SurfacePerception() && this.isActiveAndEnabled);
-				this._sender.Send(PathConnection.Enemy);
 				this._rigidybody.linearVelocityX = 0f;
 				float randomDirection = 0f;
 				if (this._statistics.RandomFollow)
@@ -34,18 +51,15 @@ namespace GuwbaPrimeAdventure.Enemy
 						else if (randomDirection < 0f)
 							targetPosition = otherTarget.x;
 					}
-					float targetDirection = targetPosition - this.transform.position.x;
-					this._movementSide = (short)(targetDirection > 0f ? 1f : -1f);
+					this._movementSide = (short)(targetPosition - this.transform.position.x > 0f ? 1f : -1f);
 					this.transform.localScale = new Vector3()
 					{
 						x = this._movementSide < 0f ? -Mathf.Abs(this.transform.localScale.x) : Mathf.Abs(this.transform.localScale.x),
 						y = this.transform.localScale.y,
 						z = this.transform.localScale.z
 					};
-					if (this.enabled && Mathf.Abs(targetPosition - this.transform.position.x) > this._statistics.DistanceToTarget)
+					if (this.isActiveAndEnabled && Mathf.Abs(targetPosition - this.transform.position.x) > this._statistics.DistanceToTarget)
 						this._rigidybody.linearVelocityX = this._movementSide * this._statistics.MovementSpeed;
-					else
-						this._rigidybody.linearVelocityX = 0f;
 					yield return new WaitForFixedUpdate();
 					yield return new WaitUntil(() => this.isActiveAndEnabled);
 				}
@@ -57,7 +71,6 @@ namespace GuwbaPrimeAdventure.Enemy
 			base.Awake();
 			this._animator = this.GetComponent<Animator>();
 			this._sender.SetStateForm(StateForm.State);
-			this._sender.SetToggle(false);
 			for (ushort i = 0; i < this._statistics.JumpPointStructures.Length; i++)
 			{
 				JumpPoint jumpPoint = this._statistics.JumpPointStructures[i].JumpPointObject;
@@ -75,14 +88,17 @@ namespace GuwbaPrimeAdventure.Enemy
 						{
 							if (this._statistics.JumpPointStructures[index].JumpStats.StopMove)
 							{
+								this._isJumping = true;
+								this._sender.SetToggle(false);
 								this._sender.Send(PathConnection.Enemy);
 								this._rigidybody.linearVelocityX = 0f;
 							}
-							this._rigidybody.AddForceY(this._statistics.JumpPointStructures[index].JumpStats.Strength * this._rigidybody.mass);
-							if (this._statistics.JumpPointStructures[index].JumpStats.High)
+							float jumpStrenght = this._statistics.JumpPointStructures[index].JumpStats.Strength * this._rigidybody.mass;
+							this._rigidybody.AddForceY(jumpStrenght, ForceMode2D.Impulse);
+							if (this._statistics.JumpPointStructures[index].JumpStats.Follow)
 							{
 								bool useTarget = this._statistics.JumpPointStructures[index].JumpStats.UseTarget;
-								this.HighJump(this._statistics.JumpPointStructures[index].JumpStats.OtherTarget, useTarget);
+								this.FollowJump(this._statistics.JumpPointStructures[index].JumpStats.OtherTarget, useTarget);
 							}
 							short jumpCount = (short)this._statistics.JumpPointStructures[index].JumpCount;
 							this._statistics.JumpPointStructures[index].RemovalJumpCount = jumpCount;
@@ -110,12 +126,14 @@ namespace GuwbaPrimeAdventure.Enemy
 				yield return new WaitTime(this, stats.TimeToExecute);
 				if (stats.StopMove)
 				{
+					this._isJumping = true;
+					this._sender.SetToggle(false);
 					this._sender.Send(PathConnection.Enemy);
 					this._rigidybody.linearVelocityX = 0f;
 				}
-				this._rigidybody.AddForceY(stats.Strength * this._rigidybody.mass);
-				if (stats.High)
-					this.HighJump(stats.OtherTarget, stats.UseTarget);
+				this._rigidybody.AddForceY(stats.Strength * this._rigidybody.mass, ForceMode2D.Impulse);
+				if (stats.Follow)
+					this.FollowJump(stats.OtherTarget, stats.UseTarget);
 				if (!this._statistics.SequentialTimmedJumps)
 					this.StartCoroutine(TimedJump(stats));
 			}
@@ -129,6 +147,37 @@ namespace GuwbaPrimeAdventure.Enemy
 		{
 			this._animator.enabled = false;
 			this._rigidybody.gravityScale = 0f;
+		}
+		private void FixedUpdate()
+		{
+			if (this.IsStunned)
+				return;
+			if (this._isJumping && this.SurfacePerception())
+			{
+				this._isJumping = false;
+				this._sender.SetToggle(true);
+				this._sender.Send(PathConnection.Enemy);
+			}
+			Vector2 right = (this.transform.right * this.transform.localScale.x).normalized;
+			LayerMask targetLayer = this._statistics.Physics.TargetLayer;
+			float lookDistance = this._statistics.LookDistance;
+			if (this._statistics.LookPerception && this.SurfacePerception())
+				if (this._statistics.CircularDetection)
+				{
+					foreach (Collider2D collider in Physics2D.OverlapCircleAll(this.transform.position, lookDistance, targetLayer))
+						if (collider.TryGetComponent<IDestructible>(out _))
+						{
+							this.BasicJump(collider.transform.position);
+							break;
+						}
+				}
+				else
+					foreach (RaycastHit2D ray in Physics2D.RaycastAll(this.transform.position, right, lookDistance, targetLayer))
+						if (ray.collider.TryGetComponent<IDestructible>(out _))
+						{
+							this.BasicJump(ray.collider.transform.position);
+							break;
+						}
 		}
 		public new void Receive(DataConnection data, object additionalData)
 		{
@@ -144,12 +193,14 @@ namespace GuwbaPrimeAdventure.Enemy
 						{
 							if (this._statistics.StopMoveReact)
 							{
+								this._isJumping = true;
+								this._sender.SetToggle(false);
 								this._sender.Send(PathConnection.Enemy);
 								this._rigidybody.linearVelocityX = 0f;
 							}
-							this._rigidybody.AddForceY(this._statistics.StrenghtReact* this._rigidybody.mass);
-							if (this._statistics.HighReact)
-								this.HighJump(this._statistics.OtherTarget, this._statistics.UseTarget);
+							this._rigidybody.AddForceY(this._statistics.StrenghtReact* this._rigidybody.mass, ForceMode2D.Impulse);
+							if (this._statistics.FollowReact)
+								this.FollowJump(this._statistics.OtherTarget, this._statistics.UseTarget);
 						}
 						break;
 					}
