eaaf8d42cb7c58b1274051da20a996095466b354
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-10 16:19:33 -0300

    Update Behavior.cs

--- System/Behavior.cs
+++ System/Behavior.cs
@@ -1,5 +1,23 @@
+using UnityEngine;
+using System.IO;
+using System.Runtime.CompilerServices;
 namespace GuwbaPrimeAdventure
 {
+	public interface ILogger
+	{
+		public void LogInfo(string message,
+			[CallerMemberName] string member = "",
+			[CallerFilePath] string file = "",
+			[CallerLineNumber] int line = 0)
+		{
+			Debug.Log($"[{Path.GetFileName(file)} : {line} - {member}] {message}");
+		}
+	};
+	public sealed class Logger : ILogger
+	{
+		public static readonly ILogger Informer = new Logger();
+		private Logger() { }
+	};
 	public interface IDestructible
 	{
 		public short Health { get; }
