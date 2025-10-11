28f726c07dac1c0d408c740ea5a601cfb6d21d14
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-11 12:31:37 -0300

    Update Transitioner.cs

--- System/Transitioner.cs
+++ System/Transitioner.cs
@@ -10,6 +10,7 @@ namespace GuwbaPrimeAdventure
 		[Header("Scene Interaction")]
 		[SerializeField, Tooltip("The object that handles the hud of the trancision.")] private TransicionHud _transicionHud;
 		[SerializeField, Tooltip("The scene that will be trancisionate to.")] private SceneField _sceneTransicion;
+		[SerializeField, Tooltip("The scene of the menu.")] private SceneField _menuScene;
 		public void Transicion(SceneField scene = null)
 		{
 			this.StartCoroutine(SceneTransicion());
@@ -18,19 +19,31 @@ namespace GuwbaPrimeAdventure
 				SaveController.Load(out SaveFile saveFile);
 				StateController.SetState(false);
 				TransicionHud transicionHud = Instantiate(this._transicionHud);
-				DontDestroyOnLoad(transicionHud);
 				for (float i = 0f; transicionHud.RootVisualElement.style.opacity.value < 1f; i += 0.1f)
 				{
 					transicionHud.RootVisualElement.style.opacity = i;
 					yield return new WaitForEndOfFrame();
 				}
 				SceneField newScene = scene ?? this._sceneTransicion;
-				AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Single);
 				if (newScene != this.gameObject.scene.name)
 					for (ushort i = 0; i < saveFile.levelsCompleted.Length; i++)
 						if (newScene.SceneName.Contains($"{i}"))
 							saveFile.lastLevelEntered = newScene;
-				yield return new WaitUntil(() => asyncOperation.isDone);
+				AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Single);
+				if (newScene != this._menuScene)
+				{
+					DontDestroyOnLoad(transicionHud);
+					yield return new WaitUntil(() => asyncOperation.isDone);
+				}
+				else
+				{
+					this._transicionHud.LoadingBar.highValue = 100f;
+					while (!asyncOperation.isDone)
+					{
+						this._transicionHud.LoadingBar.value = asyncOperation.progress * 100f;
+						yield return new WaitForEndOfFrame();
+					}
+				}
 				asyncOperation.allowSceneActivation = true;
 			}
 		}
