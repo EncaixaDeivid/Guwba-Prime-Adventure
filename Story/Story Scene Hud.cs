52337d4920bd3a116c54f3e9abc91565f15f6e21
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-12 12:00:02 -0300

    Update Story Scene Hud.cs

--- Story/Story Scene Hud.cs
+++ Story/Story Scene Hud.cs
@@ -5,18 +5,18 @@ namespace GuwbaPrimeAdventure.Story
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(UIDocument))]
 	internal sealed class StorySceneHud : MonoBehaviour
 	{
-		private static bool _isExistent;
+		private static StorySceneHud _instance;
 		[Header("Elements")]
 		[SerializeField, Tooltip("User interface element.")] private string _sceneImageVisual;
 		internal VisualElement SceneImage { get; private set; }
 		private void Awake()
 		{
-			if (_isExistent)
+			if (_instance)
 			{
 				Destroy(this.gameObject, 0.001f);
 				return;
 			}
-			_isExistent = this;
+			_instance = this;
 			VisualElement root = this.GetComponent<UIDocument>().rootVisualElement;
 			this.SceneImage = root.Q<VisualElement>(this._sceneImageVisual);
 		}
