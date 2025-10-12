3e1c1906751011442118ff763cc0de3606090575
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-12 00:21:55 -0300

    Update Background Controller.cs

--- System/Background Controller.cs
+++ System/Background Controller.cs
@@ -6,7 +6,7 @@ namespace GuwbaPrimeAdventure
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Camera), typeof(SortingGroup))]
 	internal sealed class BackgroundController : StateController
 	{
-		private static BackgroundController _instance;
+		private static bool _isExistent;
 		private Transform[] _childrenTransforms;
 		private SpriteRenderer[] _childrenRenderers;
 		private Vector2 _startPosition = Vector2.zero;
@@ -21,12 +21,12 @@ namespace GuwbaPrimeAdventure
 		[SerializeField, Tooltip("The amount to slow vertically for each layer that is after the first.")] private float _slowVertical;
 		private new void Awake()
 		{
-			if (_instance)
+			if (_isExistent)
 			{
 				Destroy(this.gameObject);
 				return;
 			}
-			_instance = this;
+			_isExistent = this;
 			base.Awake();
 			this._childrenTransforms = new Transform[this._backgroundImages.Length];
 			this._childrenRenderers = new SpriteRenderer[this._backgroundImages.Length];
