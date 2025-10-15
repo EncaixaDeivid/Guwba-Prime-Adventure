b40ef25bb863f652deea10220dfd3ddc07d2494e
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 16:43:12 -0300

    Update Death Screen Hud.cs

--- Huds/Death Screen Hud.cs
+++ Huds/Death Screen Hud.cs
@@ -7,13 +7,13 @@ namespace GuwbaPrimeAdventure.Hud
 	{
 		private static DeathScreenHud _instance;
 		[Header("Elements")]
-		[SerializeField, Tooltip("User interface element.")] private string _textLabel;
 		[SerializeField, Tooltip("User interface element.")] private string _curtainVisual;
+		[SerializeField, Tooltip("User interface element.")] private string _textLabel;
 		[SerializeField, Tooltip("User interface element.")] private string _continueButton;
 		[SerializeField, Tooltip("User interface element.")] private string _outLevelButton;
 		[SerializeField, Tooltip("User interface element.")] private string _gameOverButton;
-		internal Label Text { get; private set; }
 		internal VisualElement Curtain { get; private set; }
+		internal Label Text { get; private set; }
 		internal Button Continue { get; private set; }
 		internal Button OutLevel { get; private set; }
 		internal Button GameOver { get; private set; }
@@ -21,16 +21,16 @@ namespace GuwbaPrimeAdventure.Hud
 		{
 			if (_instance)
 			{
-				Destroy(this.gameObject, 1e-3f);
+				Destroy(gameObject, 1e-3f);
 				return;
 			}
 			_instance = this;
-			VisualElement root = this.GetComponent<UIDocument>().rootVisualElement;
-			this.Text = root.Q<Label>(this._textLabel);
-			this.Curtain = root.Q<VisualElement>(this._curtainVisual);
-			this.Continue = root.Q<Button>(this._continueButton);
-			this.OutLevel = root.Q<Button>(this._outLevelButton);
-			this.GameOver = root.Q<Button>(this._gameOverButton);
+			VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+			Curtain = root.Q<VisualElement>(_curtainVisual);
+			Text = root.Q<Label>(_textLabel);
+			Continue = root.Q<Button>(_continueButton);
+			OutLevel = root.Q<Button>(_outLevelButton);
+			GameOver = root.Q<Button>(_gameOverButton);
 		}
 	};
 };
