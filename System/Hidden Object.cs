dc82d52b7d5fc70acf0364ffe16e9e1e16b131cc
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 16:33:32 -0300

    Update Hidden Object.cs

--- System/Hidden Object.cs
+++ System/Hidden Object.cs
@@ -11,23 +11,23 @@ namespace GuwbaPrimeAdventure
 		public PathConnection PathConnection => PathConnection.System;
 		private void Awake()
 		{
-			if (!this._initialActive)
-				for (ushort i = 0; i < this.transform.childCount; i++)
-					this.transform.GetChild(i).gameObject.SetActive(false);
-			this.GetComponent<BoxCollider2D>().enabled = !this._offCollision;
+			if (!_initialActive)
+				for (ushort i = 0; i < transform.childCount; i++)
+					transform.GetChild(i).gameObject.SetActive(false);
+			GetComponent<BoxCollider2D>().enabled = !_offCollision;
 			Sender.Include(this);
 		}
 		private void OnDestroy() => Sender.Exclude(this);
 		internal void Execution(bool activate)
 		{
-			for (ushort i = 0; i < this.transform.childCount; i++)
-				this.transform.GetChild(i).gameObject.SetActive(activate);
+			for (ushort i = 0; i < transform.childCount; i++)
+				transform.GetChild(i).gameObject.SetActive(activate);
 		}
 		public void Receive(DataConnection data, object additionalData)
 		{
 			if (this == additionalData as HiddenObject && data.StateForm == StateForm.State && data.ToggleValue.HasValue)
-					for (ushort i = 0; i < this.transform.childCount; i++)
-						this.transform.GetChild(i).gameObject.SetActive(data.ToggleValue.Value);
+					for (ushort i = 0; i < transform.childCount; i++)
+						transform.GetChild(i).gameObject.SetActive(data.ToggleValue.Value);
 		}
 	};
 };
