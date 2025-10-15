64967ebb8e0edaa677cf6f875a3f5b2e3464e40d
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 17:15:10 -0300

    Update Scene Object.cs

--- Story/Scene Object.cs
+++ Story/Scene Object.cs
@@ -1,42 +1,25 @@
 using UnityEngine;
-using UnityEditor;
 using System;
 namespace GuwbaPrimeAdventure.Story
 {
-	[CreateAssetMenu(fileName = "Dialog", menuName = "Story/Dialog", order = 0)]
-	internal class DialogObject : ScriptableObject
-    {
-		[Header("Dialog Components")]
-		[SerializeField, Tooltip("The scene to trancision to after the speach.")] private SceneAsset _sceneToTransition;
-		[SerializeField, Tooltip("The collection of the object that contais the speach.")] private Speach[] _speach;
-		[SerializeField, Tooltip("The animation to play after the speach.")] private string _animation;
-		[SerializeField, Tooltip("If the trancision have to be activated after the speach.")] private bool _activateTransition;
-		[SerializeField, Tooltip("If the interaction have to be inactivated after the speach.")] private bool _desactiveInteraction;
-		[SerializeField, Tooltip("If the animation will play.")] private bool _activateAnimation;
-		[SerializeField, Tooltip("If the object will destruct.")] private bool _activateDestroy;
-		[SerializeField, Tooltip("If this object will be saved as already existent object.")] private bool _saveOnSpecifics;
-		[SerializeField, Tooltip("The amount of time to wait to destroy after the speach.")] private float _timeToDestroy;
-		internal SceneAsset SceneToTransition => this._sceneToTransition;
-		internal Speach[] Speachs => this._speach;
-		internal string Animation => this._animation;
-		internal bool ActivateTransition => this._activateTransition;
-		internal bool DesactiveInteraction => this._desactiveInteraction;
-		internal bool ActivateAnimation => this._activateAnimation;
-		internal bool ActivateDestroy => this._activateDestroy;
-		internal bool SaveOnEspecific => this._saveOnSpecifics;
-		internal float TimeToDestroy => this._timeToDestroy;
+	[CreateAssetMenu(fileName = "Scene", menuName = "Story/Scene", order = 1)]
+	internal sealed class SceneObject : ScriptableObject
+	{
+		[Header("Scene Component Collection")]
+		[SerializeField, Tooltip("The collection of objects that carry the background settings.")] private SceneComponent[] _sceneComponents;
+		internal SceneComponent[] SceneComponents => _sceneComponents;
 		[Serializable]
-		internal struct Speach
+		internal struct SceneComponent
 		{
-			[Header("Speach Components")]
-			[SerializeField, Tooltip("The image icon of the character that is speaking.")] private Sprite _model;
-			[SerializeField, Tooltip("The name of the character that is speaking.")] private string _characterName;
-			[SerializeField, TextArea(1, 12), Tooltip("The speach of the character that is speaking.")] private string _speachText;
-			[SerializeField, Tooltip("If after the speach the next slide of story scene have to come.")] private bool _nextSlide;
-			internal readonly Sprite Model => this._model;
-			internal readonly string CharacterName => this._characterName;
-			internal readonly string SpeachText => this._speachText;
-			internal readonly bool NextSlide => this._nextSlide;
+			[Header("Components")]
+			[SerializeField, Tooltip("The main imgae that is placed in the hud.")] private Texture2D _image;
+			[SerializeField, Tooltip("If the dialog will go turned off during the scene.")] private bool _offDialog;
+			[SerializeField, Tooltip("If the dialog will jump to the next slide.")] private bool _jumpToNext;
+			[SerializeField, Tooltip("The amount of time that the dialog will be turned off.")] private float _timeToDesapear;
+			internal readonly Texture2D Image => _image;
+			internal readonly bool OffDialog => _offDialog;
+			internal readonly bool JumpToNext => _jumpToNext;
+			internal readonly float TimeToDesapear => _timeToDesapear;
 		};
 	};
 };
