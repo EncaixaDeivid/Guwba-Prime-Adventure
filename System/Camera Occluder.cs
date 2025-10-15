c3d10a1ba0ed0141f55fece63fff7a341a37916d
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 14:02:18 -0300

    Update Camera Occluder.cs

--- System/Camera Occluder.cs
+++ System/Camera Occluder.cs
@@ -19,7 +19,7 @@ namespace GuwbaPrimeAdventure
 			base.Awake();
 			if (_instance)
 			{
-				Destroy(this.gameObject, 0.001f);
+				Destroy(this.gameObject, 1e-3f);
 				return;
 			}
 			_instance = this;
