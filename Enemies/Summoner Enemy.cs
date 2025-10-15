7b3f8c30bddb1d6ccbdac5de190f9f57460cdbd0
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 15:37:48 -0300

    Update Summoner Enemy.cs

--- Enemies/Summoner Enemy.cs
+++ Enemies/Summoner Enemy.cs
@@ -13,68 +13,63 @@ namespace GuwbaPrimeAdventure.Enemy
 		private void Summon(SummonObject summon)
 		{
 			if (summon.StopToSummon)
-				this.StartCoroutine(StopToSummon());
+				StartCoroutine(StopToSummon());
 			IEnumerator StopToSummon()
 			{
-				this._sender.SetToggle(false);
-				this._sender.Send(PathConnection.Enemy);
+				_sender.SetToggle(false);
+				_sender.Send(PathConnection.Enemy);
 				if (summon.ParalyzeToSummon)
-					this._rigidybody.gravityScale = 0f;
+					_rigidybody.gravityScale = 0f;
 				yield return new WaitTime(this, summon.TimeToStop);
-				this._sender.SetToggle(true);
-				this._sender.Send(PathConnection.Enemy);
-				this._rigidybody.gravityScale = this._gravityScale;
+				_sender.SetToggle(true);
+				_sender.Send(PathConnection.Enemy);
+				_rigidybody.gravityScale = _gravityScale;
 			}
-			GameObject gameObject;
 			Vector2 position;
 			ushort summonIndex = 0;
 			for (ushort i = 0; i < summon.QuantityToSummon; i++)
 			{ 
 				if (summon.Self)
-					position = this.transform.position;
+					position = transform.position;
 				else if (summon.Random)
 					position = summon.SummonPoints[Random.Range(0, summon.SummonPoints.Length - 1)];
 				else
 					position = summon.SummonPoints[summonIndex];
-				gameObject = summon.Summons[summonIndex];
+				Instantiate(summon.Summons[summonIndex], position, summon.Summons[summonIndex].transform.rotation);
 				summonIndex = (ushort)(summonIndex >= summon.Summons.Length - 1f ? 0f : summonIndex + 1f);
-				Instantiate(gameObject, position, gameObject.transform.rotation);
 			}
 		}
 		private new void Awake()
 		{
 			base.Awake();
-			this._sender.SetStateForm(StateForm.State);
-			this._gravityScale = this._rigidybody.gravityScale;
-			foreach (SummonPointStructure summonStructure in this._statistics.SummonPointStructures)
+			_sender.SetStateForm(StateForm.State);
+			_gravityScale = _rigidybody.gravityScale;
+			foreach (SummonPointStructure summonStructure in _statistics.SummonPointStructures)
+				Instantiate(summonStructure.SummonPointObject, summonStructure.Point, Quaternion.identity).GetTouch(() => Summon(summonStructure.Summon));
+			if (_statistics.RandomTimedSummons)
 			{
-				SummonPoint summonPoint = summonStructure.SummonPointObject;
-				Instantiate(summonPoint, summonStructure.Point, Quaternion.identity).GetTouch(() => this.Summon(summonStructure.Summon));
-			}
-			if (this._statistics.RandomTimedSummons)
-			{
-				this.StartCoroutine(RandomTimedSummon());
+				StartCoroutine(RandomTimedSummon());
 				IEnumerator RandomTimedSummon()
 				{
-					ushort randomIndex = (ushort)Random.Range(0f, this._statistics.TimedSummons.Length);
-					yield return TimedSummon(this._statistics.TimedSummons[randomIndex]);
-					yield return new WaitTime(this, this._statistics.TimedSummons[randomIndex].PostSummonTime);
-					this.StartCoroutine(RandomTimedSummon());
+					ushort randomIndex = (ushort)Random.Range(0, _statistics.TimedSummons.Length - 1);
+					yield return TimedSummon(_statistics.TimedSummons[randomIndex]);
+					yield return new WaitTime(this, _statistics.TimedSummons[randomIndex].PostSummonTime);
+					StartCoroutine(RandomTimedSummon());
 				}
 			}
 			else
-				foreach (SummonObject summon in this._statistics.TimedSummons)
-					this.StartCoroutine(TimedSummon(summon));
+				foreach (SummonObject summon in _statistics.TimedSummons)
+					StartCoroutine(TimedSummon(summon));
 			IEnumerator TimedSummon(SummonObject summon)
 			{
 				yield return new WaitTime(this, summon.SummonTime);
-				yield return new WaitUntil(() => !summon.StopTimedSummon && !this._stopSummon && this.isActiveAndEnabled && !this.IsStunned);
-				if (!summon.StopTimedSummon && !summon.StopPermanently && !this._stopSummon)
+				yield return new WaitUntil(() => !summon.StopTimedSummon && !_stopSummon && isActiveAndEnabled && !IsStunned);
+				if (!summon.StopTimedSummon && !summon.StopPermanently && !_stopSummon)
 				{
-					this.Summon(summon);
+					Summon(summon);
 					yield return new WaitTime(this, summon.PostSummonTime);
-					if (!this._statistics.RandomTimedSummons)
-						this.StartCoroutine(TimedSummon(summon));
+					if (!_statistics.RandomTimedSummons)
+						StartCoroutine(TimedSummon(summon));
 				}
 			}
 			Sender.Include(this);
@@ -90,17 +85,16 @@ namespace GuwbaPrimeAdventure.Enemy
 				foreach (EnemyProvider enemy in (EnemyProvider[])additionalData)
 					if (enemy != this)
 						return;
-			bool numberValid = data.NumberValue.HasValue && data.NumberValue.Value < this._statistics.EventSummons.Length;
 			if (data.StateForm == StateForm.State && data.ToggleValue.HasValue)
-				this._stopSummon = !data.ToggleValue.Value;
-			else if (data.StateForm == StateForm.Action && this._statistics.ReactToDamage && this._statistics.EventSummons.Length > 0f)
-				if (this._statistics.RandomReactSummons)
+				_stopSummon = !data.ToggleValue.Value;
+			else if (data.StateForm == StateForm.Action && _statistics.ReactToDamage && _statistics.EventSummons.Length > 0f)
+				if (_statistics.RandomReactSummons)
 				{
-					ushort randomIndex = (ushort)Random.Range(0f, this._statistics.EventSummons.Length - 1f);
-					this.Summon(this._statistics.EventSummons[randomIndex]);
+					ushort randomIndex = (ushort)Random.Range(0f, _statistics.EventSummons.Length - 1f);
+					Summon(_statistics.EventSummons[randomIndex]);
 				}
-				else if (numberValid && data.NumberValue.Value >= 0)
-					this.Summon(this._statistics.EventSummons[data.NumberValue.Value]);
+				else if (data.NumberValue.HasValue && data.NumberValue.Value < _statistics.EventSummons.Length && data.NumberValue.Value >= 0)
+					Summon(_statistics.EventSummons[data.NumberValue.Value]);
 		}
 	};
 };
