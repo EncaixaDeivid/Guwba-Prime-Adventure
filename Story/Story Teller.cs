d36d3867de4cebcec42654b25e3e84cc41375400
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 17:16:25 -0300

    Update Story Teller.cs

--- Story/Story Teller.cs
+++ Story/Story Teller.cs
@@ -13,38 +13,38 @@ namespace GuwbaPrimeAdventure.Story
 		[SerializeField, Tooltip("The object that carry the scene settings.")] private SceneObject _sceneObject;
 		private void UpdateImage()
 		{
-			this._imageIndex = (ushort)(this._imageIndex < this._sceneObject.SceneComponents.Length - 1 ? this._imageIndex + 1 : 0);
-			Texture2D texture = this._sceneObject.SceneComponents[this._imageIndex].Image;
-			this._storySceneHud.SceneImage.style.backgroundImage = Background.FromTexture2D(texture);
+			_imageIndex = (ushort)(_imageIndex < _sceneObject.SceneComponents.Length - 1 ? _imageIndex + 1 : 0);
+			Texture2D texture = _sceneObject.SceneComponents[_imageIndex].Image;
+			_storySceneHud.SceneImage.style.backgroundImage = Background.FromTexture2D(texture);
 		}
 		private IEnumerator FadeImage(bool appear)
 		{
 			if (appear)
-				for (float i = 0f; this._storySceneHud.SceneImage.style.opacity.value < 1f; i += 0.1f)
-					yield return this._storySceneHud.SceneImage.style.opacity = i;
+				for (float i = 0f; _storySceneHud.SceneImage.style.opacity.value < 1f; i += 0.1f)
+					yield return _storySceneHud.SceneImage.style.opacity = i;
 			else
-				for (float i = 1f; this._storySceneHud.SceneImage.style.opacity.value > 0f; i -= 0.1f)
-					yield return this._storySceneHud.SceneImage.style.opacity = i;
+				for (float i = 1f; _storySceneHud.SceneImage.style.opacity.value > 0f; i -= 0.1f)
+					yield return _storySceneHud.SceneImage.style.opacity = i;
 		}
 		internal void ShowScene()
 		{
-			this._storySceneHud = Instantiate(this._storySceneHudObject, this.transform);
-			Texture2D texture = this._sceneObject.SceneComponents[this._imageIndex].Image;
-			this._storySceneHud.SceneImage.style.backgroundImage = Background.FromTexture2D(texture);
-			this.StartCoroutine(this.FadeImage(true));
+			_storySceneHud = Instantiate(_storySceneHudObject, transform);
+			Texture2D texture = _sceneObject.SceneComponents[_imageIndex].Image;
+			_storySceneHud.SceneImage.style.backgroundImage = Background.FromTexture2D(texture);
+			StartCoroutine(FadeImage(true));
 		}
 		internal IEnumerator NextSlide()
 		{
-			yield return this.FadeImage(false);
-			this.UpdateImage();
-			yield return this.FadeImage(true);
-			while (this._sceneObject.SceneComponents[this._imageIndex].OffDialog)
+			yield return FadeImage(false);
+			UpdateImage();
+			yield return FadeImage(true);
+			while (_sceneObject.SceneComponents[_imageIndex].OffDialog)
 			{
-				yield return new WaitForSeconds(this._sceneObject.SceneComponents[this._imageIndex].TimeToDesapear);
-				if (this._sceneObject.SceneComponents[this._imageIndex].JumpToNext)
+				yield return new WaitForSeconds(_sceneObject.SceneComponents[_imageIndex].TimeToDesapear);
+				if (_sceneObject.SceneComponents[_imageIndex].JumpToNext)
 					yield return NextSlide();
 			}
 		}
-		internal void CloseScene() => this.StartCoroutine(this.FadeImage(false));
+		internal void CloseScene() => StartCoroutine(FadeImage(false));
 	};
 };
