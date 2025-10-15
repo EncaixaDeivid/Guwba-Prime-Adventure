d7d7979f50c16d425a3555518e47547f6a2570f4
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 17:07:57 -0300

    Update Level Gate.cs

--- Items/Level Gate.cs
+++ Items/Level Gate.cs
@@ -23,51 +23,51 @@ namespace GuwbaPrimeAdventure.Item
 		[SerializeField, Tooltip("Where the this camera have to be in the hierarchy.")] private short _overlayPriority;
 		private void Awake()
 		{
-			this._gateCamera = this.GetComponentInChildren<CinemachineCamera>();
-			this._sender.SetStateForm(StateForm.Action);
-			this._sender.SetAdditionalData(this.gameObject);
-			this._levelGate = Instantiate(this._levelGateObject, this.transform);
-			this._levelGate.transform.localPosition = this._offsetPosition;
+			_gateCamera = GetComponentInChildren<CinemachineCamera>();
+			_sender.SetStateForm(StateForm.Action);
+			_sender.SetAdditionalData(gameObject);
+			_levelGate = Instantiate(_levelGateObject, transform);
+			_levelGate.transform.localPosition = _offsetPosition;
 			SaveController.Load(out SaveFile saveFile);
-			this._levelGate.Level.clicked += this.EnterLevel;
-			if (saveFile.levelsCompleted[ushort.Parse($"{this._levelScene.SceneName[^1]}") - 1])
-				this._levelGate.Boss.clicked += this.EnterBoss;
-			if (saveFile.deafetedBosses[ushort.Parse($"{this._levelScene.SceneName[^1]}") - 1])
-				this._levelGate.Scenes.clicked += this.ShowScenes;
-			this._levelGate.Level.SetEnabled(false);
-			this._levelGate.Boss.SetEnabled(false);
-			this._levelGate.Scenes.SetEnabled(false);
-			this._defaultPriority = (short)this._gateCamera.Priority.Value;
+			_levelGate.Level.clicked += EnterLevel;
+			if (saveFile.levelsCompleted[ushort.Parse($"{_levelScene.SceneName[^1]}") - 1])
+				_levelGate.Boss.clicked += EnterBoss;
+			if (saveFile.deafetedBosses[ushort.Parse($"{_levelScene.SceneName[^1]}") - 1])
+				_levelGate.Scenes.clicked += ShowScenes;
+			_levelGate.Level.SetEnabled(false);
+			_levelGate.Boss.SetEnabled(false);
+			_levelGate.Scenes.SetEnabled(false);
+			_defaultPriority = (short)_gateCamera.Priority.Value;
 		}
 		private void OnDestroy()
 		{
 			SaveController.Load(out SaveFile saveFile);
-			this._levelGate.Level.clicked -= this.EnterLevel;
-			if (saveFile.levelsCompleted[ushort.Parse($"{this._levelScene.SceneName[^1]}") - 1])
-				this._levelGate.Boss.clicked -= this.EnterBoss;
-			if (saveFile.deafetedBosses[ushort.Parse($"{this._levelScene.SceneName[^1]}") - 1])
-				this._levelGate.Scenes.clicked -= this.ShowScenes;
+			_levelGate.Level.clicked -= EnterLevel;
+			if (saveFile.levelsCompleted[ushort.Parse($"{_levelScene.SceneName[^1]}") - 1])
+				_levelGate.Boss.clicked -= EnterBoss;
+			if (saveFile.deafetedBosses[ushort.Parse($"{_levelScene.SceneName[^1]}") - 1])
+				_levelGate.Scenes.clicked -= ShowScenes;
 		}
-		private Action EnterLevel => () => this.GetComponent<Transitioner>().Transicion(this._levelScene);
-		private Action EnterBoss => () => this.GetComponent<Transitioner>().Transicion(this._bossScene);
-		private Action ShowScenes => () => this._sender.Send(PathConnection.Story);
+		private Action EnterLevel => () => GetComponent<Transitioner>().Transicion(_levelScene);
+		private Action EnterBoss => () => GetComponent<Transitioner>().Transicion(_bossScene);
+		private Action ShowScenes => () => _sender.Send(PathConnection.Story);
 		private void OnTriggerExit2D(Collider2D other)
 		{
-			if (!this._isOnInteraction || !GuwbaCentralizer.EqualObject(other.gameObject))
+			if (!_isOnInteraction || !GuwbaCentralizer.EqualObject(other.gameObject))
 				return;
-			this._isOnInteraction = false;
-			this._gateCamera.Priority.Value = this._defaultPriority;
-			this._levelGate.Level.SetEnabled(false);
-			this._levelGate.Boss.SetEnabled(false);
-			this._levelGate.Scenes.SetEnabled(false);
+			_isOnInteraction = false;
+			_gateCamera.Priority.Value = _defaultPriority;
+			_levelGate.Level.SetEnabled(false);
+			_levelGate.Boss.SetEnabled(false);
+			_levelGate.Scenes.SetEnabled(false);
 		}
 		public void Interaction()
 		{
-			this._isOnInteraction = true;
-			this._gateCamera.Priority.Value = this._overlayPriority;
-			this._levelGate.Level.SetEnabled(true);
-			this._levelGate.Boss.SetEnabled(true);
-			this._levelGate.Scenes.SetEnabled(true);
+			_isOnInteraction = true;
+			_gateCamera.Priority.Value = _overlayPriority;
+			_levelGate.Level.SetEnabled(true);
+			_levelGate.Boss.SetEnabled(true);
+			_levelGate.Scenes.SetEnabled(true);
 		}
 	};
 };
