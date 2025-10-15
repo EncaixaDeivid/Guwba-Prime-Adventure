aabb2e6b5369af6a124681c3e484895f9f015e1d
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 17:13:45 -0300

    Update Destructive Object.cs

--- Items/Event Items/Destructive Object.cs
+++ Items/Event Items/Destructive Object.cs
@@ -8,61 +8,61 @@ namespace GuwbaPrimeAdventure.Item.EventItem
 	{
 		private readonly Sender _sender = Sender.Create();
 		[Header("Destructive Object")]
-		[SerializeField, Tooltip("If there a object that will be instantiate after the destruction of this.")]
+		[SerializeField, Tooltip("If there a object that will be instantiate after the destruction of ")]
 		private HiddenObject _hiddenObject;
 		[SerializeField, Tooltip("The vitality of this object before it destruction.")] private short _vitality;
 		[SerializeField, Tooltip("The amount of damage that this object have to receive real damage.")] private short _biggerDamage;
 		[SerializeField, Tooltip("If this object will be destructed on collision with another object.")] private bool _destroyOnCollision;
 		[SerializeField, Tooltip("If this object will be saved as already existent object.")] private bool _saveOnSpecifics;
-		public short Health => this._vitality;
+		public short Health => _vitality;
 		private new void Awake()
 		{
 			base.Awake();
-			this._sender.SetStateForm(StateForm.State);
-			this._sender.SetAdditionalData(this._hiddenObject);
-			this._sender.SetToggle(true);
+			_sender.SetStateForm(StateForm.State);
+			_sender.SetAdditionalData(_hiddenObject);
+			_sender.SetToggle(true);
 			SaveController.Load(out SaveFile saveFile);
-			if (this._saveOnSpecifics && saveFile.generalObjects.Contains(this.gameObject.name))
-				Destroy(this.gameObject, 1e-3f);
+			if (_saveOnSpecifics && saveFile.generalObjects.Contains(gameObject.name))
+				Destroy(gameObject, 1e-3f);
 		}
 		public void Execute()
 		{
-			if (this._hiddenObject)
-				this._sender.Send(PathConnection.System);
-			this.SaveObject();
-			Destroy(this.gameObject);
+			if (_hiddenObject)
+				_sender.Send(PathConnection.System);
+			SaveObject();
+			Destroy(gameObject);
 		}
 		private void SaveObject()
 		{
 			SaveController.Load(out SaveFile saveFile);
-			if (this._saveOnSpecifics && !saveFile.generalObjects.Contains(this.gameObject.name))
+			if (_saveOnSpecifics && !saveFile.generalObjects.Contains(gameObject.name))
 			{
-				saveFile.generalObjects.Add(this.gameObject.name);
+				saveFile.generalObjects.Add(gameObject.name);
 				SaveController.WriteSave(saveFile);
 			}
 		}
 		private void DestroyOnCollision()
 		{
-			if (this._destroyOnCollision)
+			if (_destroyOnCollision)
 			{
-				if (this._hiddenObject)
-					this._sender.Send(PathConnection.System);
-				this.SaveObject();
-				Destroy(this.gameObject);
+				if (_hiddenObject)
+					_sender.Send(PathConnection.System);
+				SaveObject();
+				Destroy(gameObject);
 			}
 		}
-		private void OnCollisionEnter2D(Collision2D collision) => this.DestroyOnCollision();
-		private void OnTriggerEnter2D(Collider2D collision) => this.DestroyOnCollision();
+		private void OnCollisionEnter2D(Collision2D collision) => DestroyOnCollision();
+		private void OnTriggerEnter2D(Collider2D collision) => DestroyOnCollision();
 		public bool Hurt(ushort damage)
 		{
-			if (damage < this._biggerDamage || this._vitality <= 0f)
+			if (damage < _biggerDamage || _vitality <= 0f)
 				return false;
-			if ((this._vitality -= (short)damage) <= 0f)
+			if ((_vitality -= (short)damage) <= 0f)
 			{
-				if (this._hiddenObject)
-					this._sender.Send(PathConnection.System);
-				this.SaveObject();
-				Destroy(this.gameObject);
+				if (_hiddenObject)
+					_sender.Send(PathConnection.System);
+				SaveObject();
+				Destroy(gameObject);
 			}
 			return true;
 		}
