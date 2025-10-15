9aa03a68fdcfdf76105ccd273fb5c55859efef24
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 15:52:19 -0300

    Update Enemy Physics.cs

--- Enemies/Utilities/Enemy Physics.cs
+++ Enemies/Utilities/Enemy Physics.cs
@@ -10,10 +10,10 @@ namespace GuwbaPrimeAdventure.Enemy
 		[SerializeField, Tooltip("Size of collider for checking the ground below the feet.")] private float _groundChecker;
 		[SerializeField, Tooltip("The amount of time to stop the game when hit is given.")] private float _hitStopTime;
 		[SerializeField, Tooltip("The amount of time to slow the game when hit is given.")] private float _hitSlowTime;
-		internal LayerMask GroundLayer => this._groundLayer;
-		internal LayerMask TargetLayer => this._targetLayer;
-		internal float GroundChecker => this._groundChecker;
-		internal float HitStopTime => this._hitStopTime;
-		internal float HitSlowTime => this._hitSlowTime;
+		internal LayerMask GroundLayer => _groundLayer;
+		internal LayerMask TargetLayer => _targetLayer;
+		internal float GroundChecker => _groundChecker;
+		internal float HitStopTime => _hitStopTime;
+		internal float HitSlowTime => _hitSlowTime;
 	};
 };
