b577c4263afbc4926cb31c9d2ab69cb39502107e
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 15:53:34 -0300

    Update Guwba Damager.cs

--- Characters/Guwba Damager.cs
+++ Characters/Guwba Damager.cs
@@ -14,33 +14,33 @@ namespace GuwbaPrimeAdventure.Character
 		[SerializeField, Tooltip("If this Guwba's part will take damage.")] private bool _takeDamage;
 		[SerializeField, Tooltip("The amount of damage that the attack of Guwba hits.")] private ushort _attackDamage;
 		[SerializeField, Tooltip("The amount of time that this Guwba's attack stun does.")] private float _stunTime;
-		internal Predicate<ushort> DamagerHurt { get => null; set => this._damagerHurt = value; }
-		internal UnityAction<ushort, float> DamagerStun { get => null; set => this._damagerStun = value; }
-		internal UnityAction<GuwbaDamager, IDestructible> DamagerAttack { get => null; set => this._damagerAttack = value; }
-		internal ushort AttackDamage => this._attackDamage;
-		internal float StunTime => this._stunTime;
-		internal float Alpha { get => this._spriteRenderer.color.a; set => this._spriteRenderer.color = new(1f, 1f, 1f, value); }
+		internal Predicate<ushort> DamagerHurt { get => null; set => _damagerHurt = value; }
+		internal UnityAction<ushort, float> DamagerStun { get => null; set => _damagerStun = value; }
+		internal UnityAction<GuwbaDamager, IDestructible> DamagerAttack { get => null; set => _damagerAttack = value; }
+		internal ushort AttackDamage => _attackDamage;
+		internal float StunTime => _stunTime;
+		internal float Alpha { get => _spriteRenderer.color.a; set => _spriteRenderer.color = new(1f, 1f, 1f, value); }
 		public short Health => 0;
 		private new void Awake()
 		{
 			base.Awake();
-			this._spriteRenderer = this.GetComponent<SpriteRenderer>();
+			_spriteRenderer = GetComponent<SpriteRenderer>();
 		}
 		public bool Hurt(ushort damage)
 		{
-			if (this._takeDamage)
-				return this._damagerHurt.Invoke(damage);
+			if (_takeDamage)
+				return _damagerHurt.Invoke(damage);
 			return false;
 		}
 		public void Stun(ushort stunStength, float stunTime)
 		{
-			if (this._takeDamage)
-				this._damagerStun.Invoke(stunStength, stunTime);
+			if (_takeDamage)
+				_damagerStun.Invoke(stunStength, stunTime);
 		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
-			if (!this._takeDamage && other.TryGetComponent<IDestructible>(out var destructible))
-				this._damagerAttack.Invoke(this, destructible);
+			if (!_takeDamage && other.TryGetComponent<IDestructible>(out var destructible))
+				_damagerAttack.Invoke(this, destructible);
 		}
 	};
 };
