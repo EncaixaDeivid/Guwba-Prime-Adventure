e073e398ec803600e48308e010dbf3b361426aef
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-15 15:49:14 -0300

    Update Shooter Statistics.cs

--- Enemies/Utilities/Shooter Statistics.cs
+++ Enemies/Utilities/Shooter Statistics.cs
@@ -10,18 +10,13 @@ namespace GuwbaPrimeAdventure.Enemy
 		[SerializeField, Tooltip("The distance this enemy can detect the target.")] private float _perceptionDistance;
 		[SerializeField, Tooltip("The angle fo the direction of ray of the detection.")] private float _rayAngleDirection;
 		[SerializeField, Tooltip("The amount of time to wait to execute another shoot.")] private float _intervalToShoot;
-		[SerializeField, Tooltip("The amount of time to stop this enemy to move.\nRequires: Moving Enemy")]
-		private float _stopTime;
+		[SerializeField, Tooltip("The amount of time to stop this enemy to move.\nRequires: Moving Enemy")] private float _stopTime;
 		[SerializeField, Tooltip("If this enemy will turn the ray to the looking side.")] private bool _turnRay;
-		[SerializeField, Tooltip("If this enemy will become invencible while shooting.\nRequires: Defender Enemy")]
-		private bool _invencibleShoot;
+		[SerializeField, Tooltip("If this enemy will become invencible while shooting.\nRequires: Defender Enemy")]	private bool _invencibleShoot;
 		[SerializeField, Tooltip("If this enemy gets hurt it will shoot.")] private bool _shootDamaged;
-		[SerializeField, Tooltip("If this enemy will stop moving when shoot.\nRequires: Moving Enemy")]
-		private bool _stop;
-		[SerializeField, Tooltip("If this enemy will paralyze moving.\nRequires: Moving Enemy")]
-		private bool _paralyze;
-		[SerializeField, Tooltip("If this enemy will return the gravity after paralyze.\nRequires: Moving Enemy")]
-		private bool _returnParalyze;
+		[SerializeField, Tooltip("If this enemy will stop moving when shoot.\nRequires: Moving Enemy")]	private bool _stop;
+		[SerializeField, Tooltip("If this enemy will paralyze moving.\nRequires: Moving Enemy")] private bool _paralyze;
+		[SerializeField, Tooltip("If this enemy will return the gravity after paralyze.\nRequires: Moving Enemy")] private bool _returnParalyze;
 		[SerializeField, Tooltip("If the detection will be circular.")] private bool _circularDetection;
 		[SerializeField, Tooltip("Will shoot to infinity without any detection.")] private bool _shootInfinity;
 		[SerializeField, Tooltip("If this enemy won't interfere in the projectile.")] private bool _pureInstance;
