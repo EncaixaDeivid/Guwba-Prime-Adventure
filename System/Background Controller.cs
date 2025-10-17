21bc2ac5e3db98196714cdae8519804c304bbe14
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-17 15:00:37 -0300

    Update Background Controller.cs

--- System/Background Controller.cs
+++ System/Background Controller.cs
@@ -11,6 +11,7 @@ namespace GuwbaPrimeAdventure
 		private Transform[] _childrenTransforms;
 		private SpriteRenderer[] _childrenRenderers;
 		private Vector2 _startPosition = Vector2.zero;
+		private bool _isInTrancision = false;
 		[Header("Background Objects")]
 		[SerializeField, Tooltip("The object that handles the backgrounds.")] private Transform _backgroundTransform;
 		[SerializeField, Tooltip("The handler of the background.")] private SpriteAtlas _backgroundHandler;
@@ -34,22 +35,31 @@ namespace GuwbaPrimeAdventure
 		}
 		private IEnumerator Start()
 		{
+			_isInTrancision = true;
 			yield return new WaitWhile(() => SceneInitiator.IsInTrancision());
+			Sprite sprite;
+			Vector2 imageSize;
+			float centerX;
+			float centerY;
+			float right;
+			float left;
+			float top;
+			float bottom;
 			for (ushort ia = 0; ia < _backgroundImages.Length; ia++)
 			{
 				_childrenTransforms[ia] = Instantiate(_backgroundTransform, transform);
 				_childrenRenderers[ia] = _childrenTransforms[ia].GetComponent<SpriteRenderer>();
-				Sprite sprite = _backgroundHandler.GetSprite(_backgroundImages[ia]);
+				sprite = _backgroundHandler.GetSprite(_backgroundImages[ia]);
 				_childrenRenderers[ia].sprite = sprite;
 				_childrenRenderers[ia].sortingOrder = _backgroundImages.Length - 1 - ia;
 				_childrenTransforms[ia].GetComponent<SortingGroup>().sortingOrder = _childrenRenderers[ia].sortingOrder;
-				float centerX = _childrenTransforms[ia].position.x;
-				float centerY = _childrenTransforms[ia].position.y;
-				Vector2 imageSize = _childrenRenderers[ia].sprite.bounds.size;
-				float right = centerX + imageSize.x;
-				float left = centerX - imageSize.x;
-				float top = centerY + imageSize.y;
-				float bottom = centerY - imageSize.y;
+				centerX = _childrenTransforms[ia].position.x;
+				centerY = _childrenTransforms[ia].position.y;
+				imageSize = _childrenRenderers[ia].sprite.bounds.size;
+				right = centerX + imageSize.x;
+				left = centerX - imageSize.x;
+				top = centerY + imageSize.y;
+				bottom = centerY - imageSize.y;
 				_childrenTransforms[ia].GetChild(0).position = new Vector2(left, top);
 				_childrenTransforms[ia].GetChild(1).position = new Vector2(centerX, top);
 				_childrenTransforms[ia].GetChild(2).position = new Vector2(right, top);
@@ -61,26 +71,28 @@ namespace GuwbaPrimeAdventure
 				for (ushort ib = 0; ib < _childrenTransforms[ia].childCount; ib++)
 					_childrenTransforms[ia].GetChild(ib).GetComponent<SpriteRenderer>().sprite = sprite;
 			}
+			_isInTrancision = false;
 		}
 		private void FixedUpdate()
 		{
+			if (_isInTrancision)
+				return;
+			Vector2 imageSize;
+			float axisX;
+			float axisY;
 			for (ushort i = 0; i < _backgroundImages.Length; i++)
 			{
-				float axisX = 1f - (_horizontalBackgroundSpeed - (i * _slowHorizontal));
-				float axisY = 1f - (_verticalBackgroundSpeed - (i * _slowVertical));
-				float movementAxisX = transform.position.x * axisX;
-				float movementAxisY = transform.position.y * axisY;
-				_childrenTransforms[i].position = new Vector2(_startPosition.x + movementAxisX, _startPosition.y + movementAxisY);
-				Vector2 imageSize = _childrenRenderers[i].sprite.bounds.size;
-				float distanceAxisX = transform.position.x * (1f - axisX);
-				float distanceAxisY = transform.position.y * (1f - axisY);
-				if (distanceAxisX > _startPosition.x + imageSize.x)
+				axisX = 1f - (_horizontalBackgroundSpeed - (i * _slowHorizontal));
+				axisY = 1f - (_verticalBackgroundSpeed - (i * _slowVertical));
+				_childrenTransforms[i].position = new Vector2(_startPosition.x + transform.position.x * axisX, _startPosition.y + transform.position.y * axisY);
+				imageSize = _childrenRenderers[i].sprite.bounds.size;
+				if (transform.position.x * (1f - axisX) > _startPosition.x + imageSize.x)
 					_startPosition = new Vector2(_startPosition.x + imageSize.x, _startPosition.y);
-				else if (distanceAxisX < _startPosition.x - imageSize.x)
+				else if (transform.position.x * (1f - axisX) < _startPosition.x - imageSize.x)
 					_startPosition = new Vector2(_startPosition.x - imageSize.x, _startPosition.y);
-				if (distanceAxisY > _startPosition.y + imageSize.y)
+				if (transform.position.y * (1f - axisY) > _startPosition.y + imageSize.y)
 					_startPosition = new Vector2(_startPosition.x, _startPosition.y + imageSize.y);
-				else if (distanceAxisY < _startPosition.y - imageSize.y)
+				else if (transform.position.y * (1f - axisY) < _startPosition.y - imageSize.y)
 					_startPosition = new Vector2(_startPosition.x, _startPosition.y - imageSize.y);
 			}
 		}
