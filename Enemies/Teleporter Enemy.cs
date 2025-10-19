54ac275ef0532cea5834d6dd8f72758366b0e20d
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-10-19 19:21:32 -0300

    Update Teleporter Enemy.cs

--- Enemies/Teleporter Enemy.cs
+++ Enemies/Teleporter Enemy.cs
@@ -5,27 +5,24 @@ namespace GuwbaPrimeAdventure.Enemy
 	[DisallowMultipleComponent]
 	internal sealed class TeleporterEnemy : EnemyProvider
 	{
+		private ushort _teleportIndex = 0;
 		private bool _canTeleport = true;
 		[Header("Teleporter Enemy")]
 		[SerializeField, Tooltip("The teleporter statitics of this enemy.")] private TeleporterStatistics _statistics;
-		private new void Awake()
+		private IEnumerator Start()
 		{
-			base.Awake();
+			yield return new WaitWhile(() => SceneInitiator.IsInTrancision());
 			foreach (TeleportPointStructure teleportPointStructure in _statistics.TeleportPointStructures)
 			{
 				Instantiate(teleportPointStructure.TeleportPointObject, teleportPointStructure.InstancePoint, Quaternion.identity).GetTouch(() =>
 				{
 					if (_canTeleport)
 					{
-						Vector2 teleportPointToUse;
 						if (teleportPointStructure.RandomTeleports)
-						{
-							ushort index = (ushort)Random.Range(0, teleportPointStructure.TeleportPoints.Length - 1f);
-							teleportPointToUse = teleportPointStructure.TeleportPoints[index];
-						}
-						else
-							teleportPointToUse = teleportPointStructure.TeleportPoints[0];
-						transform.position = teleportPointToUse;
+							_teleportIndex = (ushort)Random.Range(0, teleportPointStructure.TeleportPoints.Length - 1);
+						transform.position = teleportPointStructure.TeleportPoints[_teleportIndex];
+						if (!teleportPointStructure.RandomTeleports)
+							_teleportIndex = (ushort)(_teleportIndex >= teleportPointStructure.TeleportPoints.Length - 1 ? _teleportIndex + 1 : 0);
 						StartCoroutine(UseTimer());
 					}
 					IEnumerator UseTimer()
