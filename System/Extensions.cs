ad93efbdf4178f881a7d212531d1917e2a4ac1eb
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-10 14:50:53 -0300

    Add files via upload

---
+++ System/Extensions.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+namespace GuwbaPrimeAdventure
+{
+	public static class Extensions
+	{
+		private static float _localScaleX;
+		public static void TurnScaleX(this Transform transform, float valueChanger)
+		{
+			_localScaleX = Mathf.Abs(transform.localScale.x) * valueChanger;
+			transform.localScale = new Vector3(_localScaleX, transform.localScale.y, transform.localScale.z);
+		}
+		public static void TurnScaleX(this Transform transform, bool conditionChanger)
+		{
+			_localScaleX = Mathf.Abs(transform.localScale.x) * (conditionChanger ? -1f : 1f);
+			transform.localScale = new Vector3(_localScaleX, transform.localScale.y, transform.localScale.z);
+		}
+	};
+};
