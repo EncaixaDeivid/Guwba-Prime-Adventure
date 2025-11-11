96523f54b5fce9fb40da87fddad18a27535fe52e
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-10 23:34:52 -0300

    Update Summon Object.cs

--- Enemies/Utilities/Summon Object.cs
+++ Enemies/Utilities/Summon Object.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 namespace GuwbaPrimeAdventure.Enemy
 {
 	[CreateAssetMenu(fileName = "Enemy Summon", menuName = "Enemy Statistics/Summon", order = 7)]
-	public sealed class SummonObject : ScriptableObject
+	internal sealed class SummonObject : ScriptableObject
 	{
 		[Header("Components Statistics")]
 		[SerializeField, Tooltip("The enemy that will be instantiate.")] private GameObject[] _summons;
@@ -11,23 +11,25 @@ namespace GuwbaPrimeAdventure.Enemy
 		[SerializeField, Tooltip("The amount of time to wait to execute the next instance.")] private ushort _postSummonTime;
 		[SerializeField, Tooltip("The amount of instance to be instantiate.")] private ushort _quantityToSummon;
 		[SerializeField, Tooltip("The amount of time to stop the instantiator.")] private float _timeToStop;
+		[SerializeField, Tooltip("If the points to summon are relative to it's parent.")] private bool _localPoints;
 		[SerializeField, Tooltip("If the instantiator will stop during the summon.")] private bool _stopToSummon;
 		[SerializeField, Tooltip("If the instantiator will paralyze during the summon.")] private bool _paralyzeToSummon;
 		[SerializeField, Tooltip("If the timed summon can be stopped.")] private bool _stopTimedSummon;
 		[SerializeField, Tooltip("If the timed summon will stop permanently.")] private bool _stopPermanently;
 		[SerializeField, Tooltip("If the instanciation will be in the same point as the summoner.")] private bool _self;
 		[SerializeField, Tooltip("If the instantiation will be randomized at one of the points.")] private bool _random;
-		public GameObject[] Summons => _summons;
-		public Vector2[] SummonPoints => _summonPoints;
-		public ushort SummonTime => _summonTime;
-		public ushort PostSummonTime => _postSummonTime;
-		public ushort QuantityToSummon => _quantityToSummon;
-		public float TimeToStop => _timeToStop;
-		public bool StopToSummon => _stopToSummon;
-		public bool ParalyzeToSummon => _paralyzeToSummon;
-		public bool StopTimedSummon => _stopTimedSummon;
-		public bool StopPermanently => _stopPermanently;
-		public bool Self => _self;
-		public bool Random => _random;
+		internal GameObject[] Summons => _summons;
+		internal Vector2[] SummonPoints => _summonPoints;
+		internal ushort SummonTime => _summonTime;
+		internal ushort PostSummonTime => _postSummonTime;
+		internal ushort QuantityToSummon => _quantityToSummon;
+		internal float TimeToStop => _timeToStop;
+		internal bool LocalPoints => _localPoints;
+		internal bool StopToSummon => _stopToSummon;
+		internal bool ParalyzeToSummon => _paralyzeToSummon;
+		internal bool StopTimedSummon => _stopTimedSummon;
+		internal bool StopPermanently => _stopPermanently;
+		internal bool Self => _self;
+		internal bool Random => _random;
 	};
 };
