90aa54829bb3467b223f277dad992b39b3fb6a01
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-12 17:34:12 -0300

    Update Death Statistics.cs

--- Enemies/Utilities/Death Statistics.cs
+++ Enemies/Utilities/Death Statistics.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-namespace GuwbaPrimeAdventure.Enemy
+namespace GwambaPrimeAdventure.Enemy
 {
 	[CreateAssetMenu(fileName = "Death Enemy", menuName = "Enemy Statistics/Death", order = 10)]
 	internal sealed class DeathStatistics : ScriptableObject
