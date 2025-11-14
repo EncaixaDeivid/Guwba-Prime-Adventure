b22eb1391f1bcf4dc4f1f8a82ae7fb1f3e0f0787
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-14 14:18:41 -0300

    Update Summoner Enemy.cs

--- Enemies/Summoner Enemy.cs
+++ Enemies/Summoner Enemy.cs
@@ -108,10 +108,11 @@ namespace GwambaPrimeAdventure.Enemy
 		}
 		public void Receive(DataConnection data, object additionalData)
 		{
-			if ((EnemyProvider[])additionalData != null)
-				foreach (EnemyProvider enemy in (EnemyProvider[])additionalData)
-					if (enemy != this)
-						return;
+			if (additionalData == null || additionalData is not EnemyProvider[] || (EnemyProvider[])additionalData == null || ((EnemyProvider[])additionalData).Length <= 0)
+				return;
+			foreach (EnemyProvider enemy in (EnemyProvider[])additionalData)
+				if (enemy != this)
+					return;
 			if (data.StateForm == StateForm.State && data.ToggleValue.HasValue)
 				_stopSummon = !data.ToggleValue.Value;
 			else if (data.StateForm == StateForm.Action && _statistics.HasEventSummon && _statistics.EventSummons.Length > 0f)
