206096eb9ea78b666802b0375ef48cc6191a1b55
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-14 19:29:31 -0300

    Update Summon Point.cs

--- Enemies/Utilities/Summon Point.cs
+++ Enemies/Utilities/Summon Point.cs
@@ -1,27 +1,35 @@
 using UnityEngine;
-using UnityEngine.Events;
 using GwambaPrimeAdventure.Character;
 namespace GwambaPrimeAdventure.Enemy
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
 	internal sealed class SummonPoint : StateController
 	{
-		private UnityAction _getTouch;
+		private ISummoner _summoner;
+		private ushort _summonIndex;
 		[Header("Interactions")]
 		[SerializeField, Tooltip("If this point will destroy itself after use.")] private bool _destroyAfter;
 		[SerializeField, Tooltip("If this point will trigger with other object.")] private bool _hasTarget;
-		internal void GetTouch(UnityAction getTouch) => _getTouch = getTouch;
+		internal void GetTouch(ISummoner summoner, ushort summonIndex)
+		{
+			_summoner = summoner;
+			_summonIndex = summonIndex;
+		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
 			if (_hasTarget)
 			{
 				if (GwambaStateMarker.EqualObject(other.gameObject))
-					_getTouch.Invoke();
+					_summoner.OnSummon(_summonIndex);
 			}
 			else if (other.TryGetComponent<SummonerEnemy>(out _))
-				_getTouch.Invoke();
+				_summoner.OnSummon(_summonIndex);
 			if (_destroyAfter)
 				Destroy(gameObject);
 		}
+	};
+	internal interface ISummoner
+	{
+		public void OnSummon(ushort summonIndex);
 	};
 };
