c557c3506fb824f7af6a8dba130a62dba6561d27
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-14 19:29:11 -0300

    Update Teleport Point.cs

--- Enemies/Utilities/Teleport Point.cs
+++ Enemies/Utilities/Teleport Point.cs
@@ -1,27 +1,35 @@
 using UnityEngine;
-using UnityEngine.Events;
 using GwambaPrimeAdventure.Character;
 namespace GwambaPrimeAdventure.Enemy
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
 	internal sealed class TeleportPoint : StateController
 	{
-		private UnityAction _getTouch;
+		private ITeleporter _teleporter;
+		private ushort _teleportIndex;
 		[Header("Interactions")]
 		[SerializeField, Tooltip("If this point will destroy itself after use.")] private bool _destroyAfter;
 		[SerializeField, Tooltip("If this point will trigger with other object.")] private bool _hasTarget;
-		internal void GetTouch(UnityAction getTouch) => _getTouch = getTouch;
+		internal void GetTouch(ITeleporter teleporter, ushort teleportIndex)
+		{
+			_teleporter = teleporter;
+			_teleportIndex = teleportIndex;
+		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
 			if (_hasTarget)
 			{
 				if (GwambaStateMarker.EqualObject(other.gameObject))
-					_getTouch.Invoke();
+					_teleporter.OnTeleport(_teleportIndex);
 			}
 			else if (other.TryGetComponent<TeleporterEnemy>(out _))
-				_getTouch.Invoke();
+				_teleporter.OnTeleport(_teleportIndex);
 			if (_destroyAfter)
 				Destroy(gameObject);
 		}
+	};
+	internal interface ITeleporter
+	{
+		public void OnTeleport(ushort teleportIndex);
 	};
 };
