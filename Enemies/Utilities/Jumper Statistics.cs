8325f09bf9a3ae8663a766545a09b9891fd1a2b4
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-14 14:46:53 -0300

    Update Jumper Statistics.cs

--- Enemies/Utilities/Jumper Statistics.cs
+++ Enemies/Utilities/Jumper Statistics.cs
@@ -21,7 +21,8 @@ namespace GwambaPrimeAdventure.Enemy
 		[SerializeField, Tooltip("The distance this enemy will be to the follow target.")] private float _distanceToTarget;
 		[Header("Reaction")]
 		[SerializeField, Tooltip("The strenght of the jump on a react of damage.")] private ushort _strenghtReact;
-		[SerializeField, Tooltip("If the react to damage jump is a high jump.")] private bool _followReact;
+		[SerializeField, Tooltip("If the react to damage jump will follow a target.")] private bool _followReact;
+		[SerializeField, Tooltip("If the react to damage jump will turn on the target.")] private bool _turnFollowReact;
 		[SerializeField, Tooltip("If it will stop moving on react to damage.")] private bool _stopMoveReact;
 		internal JumpPointStructure[] JumpPointStructures => _jumpPointStructures;
 		internal JumpStats[] TimedJumps => _timedJumps;
@@ -39,6 +40,7 @@ namespace GwambaPrimeAdventure.Enemy
 		internal float DistanceToTarget => _distanceToTarget;
 		internal ushort StrenghtReact => _strenghtReact;
 		internal bool FollowReact => _followReact;
+		internal bool TurnFollowReact => _turnFollowReact;
 		internal bool StopMoveReact => _stopMoveReact;
 	};
 	[System.Serializable]
@@ -61,12 +63,14 @@ namespace GwambaPrimeAdventure.Enemy
 		[SerializeField, Tooltip("The strenght of the jump.")] private ushort _strength;
 		[SerializeField, Tooltip("If in the high jumo it will stop moving.")] private bool _stopMove;
 		[SerializeField, Tooltip("If this is a follow jump.")] private bool _follow;
+		[SerializeField, Tooltip("If this enemy will turn on the follow.")] private bool _turnFollow;
 		[SerializeField, Tooltip("If for this jump it will use the other target.")] private bool _useTarget;
 		[SerializeField, Tooltip("The amount of time for this jump to execute.")] private float _timeToExecute;
 		internal readonly Vector2 OtherTarget => _otherTarget;
 		internal readonly ushort Strength => _strength;
 		internal readonly bool StopMove => _stopMove;
 		internal readonly bool Follow => _follow;
+		internal readonly bool TurnFollow => _turnFollow;
 		internal readonly bool UseTarget => _useTarget;
 		internal readonly float TimeToExecute => _timeToExecute;
 	};
