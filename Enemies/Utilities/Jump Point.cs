d046ea1172168732dacb7d324d713b80fceb9164
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-14 19:29:45 -0300

    Update Jump Point.cs

--- Enemies/Utilities/Jump Point.cs
+++ Enemies/Utilities/Jump Point.cs
@@ -1,19 +1,18 @@
 using UnityEngine;
-using UnityEngine.Events;
 using GwambaPrimeAdventure.Character;
 namespace GwambaPrimeAdventure.Enemy
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
 	internal sealed class JumpPoint : StateController
 	{
-		private UnityAction<ushort> _getTouch;
+		private IJumper _jumper;
 		private ushort _touchIndex;
 		[Header("Interactions")]
 		[SerializeField, Tooltip("If this point will destroy itself after use.")] private bool _destroyAfter;
 		[SerializeField, Tooltip("If this point will trigger with other object.")] private bool _hasTarget;
-		internal void GetTouch(ushort touchIndex, UnityAction<ushort> getTouch)
+		internal void GetTouch(IJumper jumperEnemy, ushort touchIndex)
 		{
-			_getTouch = getTouch;
+			_jumper = jumperEnemy;
 			_touchIndex = touchIndex;
 		}
 		private void OnTriggerEnter2D(Collider2D other)
@@ -21,12 +20,16 @@ namespace GwambaPrimeAdventure.Enemy
 			if (_hasTarget)
 			{
 				if (GwambaStateMarker.EqualObject(other.gameObject))
-					_getTouch.Invoke(_touchIndex);
+					_jumper.OnJump(_touchIndex);
 			}
-			else if (other.TryGetComponent<JumperEnemy>(out _))
-				_getTouch.Invoke(_touchIndex);
+			else if (other.TryGetComponent<IJumper>(out _))
+				_jumper.OnJump(_touchIndex);
 			if (_destroyAfter)
 				Destroy(gameObject);
 		}
+	};
+	internal interface IJumper
+	{
+		public void OnJump(ushort jumpIndex);
 	};
 };
