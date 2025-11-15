dfe38fca405c226398553b25a1a9207473bdbf0b
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-15 00:32:49 -0300

    Update Summoner Enemy.cs

--- Enemies/Summoner Enemy.cs
+++ Enemies/Summoner Enemy.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using GwambaPrimeAdventure.Connection;
+using GwambaPrimeAdventure.Enemy.Utility;
 namespace GwambaPrimeAdventure.Enemy
 {
 	[DisallowMultipleComponent]
