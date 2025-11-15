309b0015e6e97f610dfa02f23be495820b9de229
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-15 00:27:02 -0300

    Update Flying Statistics.cs

--- Enemies/Utilities/Flying Statistics.cs
+++ Enemies/Utilities/Flying Statistics.cs
@@ -1,23 +1,23 @@
 using UnityEngine;
-namespace GwambaPrimeAdventure.Enemy
+namespace GwambaPrimeAdventure.Enemy.Utility
 {
 	[CreateAssetMenu(fileName = "Flying Enemy", menuName = "Enemy Statistics/Flying", order = 4)]
-	internal sealed class FlyingStatistics : MovingStatistics
+	public sealed class FlyingStatistics : MovingStatistics
 	{
 		[Header("Flying Enemy")]
-		[SerializeField, Tooltip("The target this enemy have to pursue.")] private GameObject _target;
+		[SerializeField, Tooltip("The target this enemy have to pursue.")] private Transform _target;
 		[SerializeField, Tooltip("The amount of speed to this enemy rotate towards.")] private float _rotationSpeed;
 		[SerializeField, Tooltip("The distance to stay away from the target.")] private float _targetDistance;
 		[SerializeField, Tooltip("The multiplication factor of the detection.")] private float _detectionFactor;
 		[SerializeField, Tooltip("The amount of speed that this enemy moves to go back to the original point.")] private float _returnSpeed;
 		[SerializeField, Tooltip("The amount of time this enemy moves will be stopped after it attack.")] private float _afterTime;
 		[SerializeField, Tooltip("If this enemy will pursue the target until fade.")] private bool _endlessPursue;
-		internal GameObject Target => _target;
-		internal float RotationSpeed => _rotationSpeed;
-		internal float TargetDistance => _targetDistance;
-		internal float DetectionFactor => _detectionFactor;
-		internal float ReturnSpeed => _returnSpeed;
-		internal float AfterTime => _afterTime;
-		internal bool EndlessPursue => _endlessPursue;
+		public Transform Target => _target;
+		public float RotationSpeed => _rotationSpeed;
+		public float TargetDistance => _targetDistance;
+		public float DetectionFactor => _detectionFactor;
+		public float ReturnSpeed => _returnSpeed;
+		public float AfterTime => _afterTime;
+		public bool EndlessPursue => _endlessPursue;
 	};
 };
