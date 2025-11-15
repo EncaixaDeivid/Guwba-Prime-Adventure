1aff8bba42552885df99c362f36b5539a9554238
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-15 16:32:22 -0300

    Update Enemy Physics.cs

--- Enemies/Utilities/Enemy Physics.cs
+++ Enemies/Utilities/Enemy Physics.cs
@@ -7,12 +7,10 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 		[Header("Enemy Physics")]
 		[SerializeField, Tooltip("The layer mask to identify the ground.")] private LayerMask _groundLayer;
 		[SerializeField, Tooltip("The layer mask to identify the target of the attacks.")] private LayerMask _targetLayer;
-		[SerializeField, Tooltip("Size of collider for checking the ground below the feet.")] private float _groundChecker;
 		[SerializeField, Tooltip("The amount of time to stop the game when hit is given.")] private float _hitStopTime;
 		[SerializeField, Tooltip("The amount of time to slow the game when hit is given.")] private float _hitSlowTime;
 		public LayerMask GroundLayer => _groundLayer;
 		public LayerMask TargetLayer => _targetLayer;
-		public float GroundChecker => _groundChecker;
 		public float HitStopTime => _hitStopTime;
 		public float HitSlowTime => _hitSlowTime;
 	};
