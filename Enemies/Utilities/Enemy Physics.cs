d6fed2edae61dcab48a22c2e3fd25c6f8ff00434
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-15 00:26:18 -0300

    Update Enemy Physics.cs

--- Enemies/Utilities/Enemy Physics.cs
+++ Enemies/Utilities/Enemy Physics.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
-namespace GwambaPrimeAdventure.Enemy
+namespace GwambaPrimeAdventure.Enemy.Utility
 {
 	[CreateAssetMenu(fileName = "Enemy Physics", menuName = "Enemy Statistics/Physics", order = 0)]
-	internal sealed class EnemyPhysics : ScriptableObject
+	public sealed class EnemyPhysics : ScriptableObject
 	{
 		[Header("Enemy Physics")]
 		[SerializeField, Tooltip("The layer mask to identify the ground.")] private LayerMask _groundLayer;
@@ -10,10 +10,10 @@ namespace GwambaPrimeAdventure.Enemy
 		[SerializeField, Tooltip("Size of collider for checking the ground below the feet.")] private float _groundChecker;
 		[SerializeField, Tooltip("The amount of time to stop the game when hit is given.")] private float _hitStopTime;
 		[SerializeField, Tooltip("The amount of time to slow the game when hit is given.")] private float _hitSlowTime;
-		internal LayerMask GroundLayer => _groundLayer;
-		internal LayerMask TargetLayer => _targetLayer;
-		internal float GroundChecker => _groundChecker;
-		internal float HitStopTime => _hitStopTime;
-		internal float HitSlowTime => _hitSlowTime;
+		public LayerMask GroundLayer => _groundLayer;
+		public LayerMask TargetLayer => _targetLayer;
+		public float GroundChecker => _groundChecker;
+		public float HitStopTime => _hitStopTime;
+		public float HitSlowTime => _hitSlowTime;
 	};
 };
