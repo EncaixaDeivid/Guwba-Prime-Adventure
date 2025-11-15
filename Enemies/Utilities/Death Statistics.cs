c96fd50920cf4e39f12376125ef1901e9f9320e6
EncaixaDeivid <129303913+EncaixaDeivid@users.noreply.github.com>
2025-11-15 00:25:28 -0300

    Update Death Statistics.cs

--- Enemies/Utilities/Death Statistics.cs
+++ Enemies/Utilities/Death Statistics.cs
@@ -1,19 +1,17 @@
 using UnityEngine;
-namespace GwambaPrimeAdventure.Enemy
+namespace GwambaPrimeAdventure.Enemy.Utility
 {
 	[CreateAssetMenu(fileName = "Death Enemy", menuName = "Enemy Statistics/Death", order = 10)]
-	internal sealed class DeathStatistics : ScriptableObject
+	public sealed class DeathStatistics : ScriptableObject
 	{
 		[Header("Death Enemy")]
-		[SerializeField, Tooltip("The physics of the enemy.")] private EnemyPhysics _physics;
-		[SerializeField, Tooltip("The enemy that this enemy will spawn in death.")] private EnemyController _childEnemy;
-		[SerializeField, Tooltip("The projectile that this enemy will spawn in death.")] private EnemyProjectile _childProjectile;
+		[SerializeField, Tooltip("The enemy that this enemy will spawn in death.")] private Control _childEnemy;
+		[SerializeField, Tooltip("The projectile that this enemy will spawn in death.")] private Projectile _childProjectile;
 		[SerializeField, Tooltip("If this enemy will die on touch.")] private bool _onTouch;
 		[SerializeField, Tooltip("THe time to this enemy die.")] private float _timeToDie;
-		internal EnemyPhysics Physics => _physics;
-		internal EnemyController ChildEnemy => _childEnemy;
-		internal EnemyProjectile ChildProjectile => _childProjectile;
-		internal bool OnTouch => _onTouch;
-		internal float TimeToDie => _timeToDie;
+		public Control ChildEnemy => _childEnemy;
+		public Projectile ChildProjectile => _childProjectile;
+		public bool OnTouch => _onTouch;
+		public float TimeToDie => _timeToDie;
 	};
 };
