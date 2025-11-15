e61a3415aab7aae60900848337ca8ee365379138
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-15 16:55:55 -0300

    Update Gwamba State Marker.cs

--- Characters/Gwamba State Marker.cs
+++ Characters/Gwamba State Marker.cs
@@ -633,7 +633,7 @@ namespace GwambaPrimeAdventure.Character
 					{
 						_originCast = new Vector2(Local.x - (_collider.bounds.extents.x - (_jokerValue.x += WorldBuild.SNAPLENGTH / 2f)) * _movementAction, Local.y - _collider.bounds.extents.y);
 						_bottomCast = Physics2D.Raycast(_originCast, -transform.up, 1f + WorldBuild.SNAPLENGTH, _groundLayer);
-						_downStairs = Mathf.Round((transform.position.y - _collider.bounds.extents.y) * 10f) / 10f != Mathf.Round(_bottomCast.point.y * 10f) / 10f;
+						_downStairs = _bottomCast && Mathf.Round((transform.position.y - _collider.bounds.extents.y) * 10f) / 10f != Mathf.Round(_bottomCast.point.y * 10f) / 10f;
 					}
 					while (!_downStairs && _jokerValue.x < _collider.bounds.extents.x / 4f);
 					if (_downStairs)
