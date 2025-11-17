2a5cd2d81b2e03ff037b2438a77e6ee3d6e9fd2e
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-17 20:44:29 -0300

    Update Shooter Enemy.cs

--- Enemies/Shooter Enemy.cs
+++ Enemies/Shooter Enemy.cs
@@ -126,10 +126,13 @@ namespace GwambaPrimeAdventure.Enemy
 		}
 		public void Receive(DataConnection data, object additionalData)
 		{
-			if (additionalData != null || additionalData is EnemyProvider[] || additionalData as EnemyProvider[] != null || (additionalData as EnemyProvider[]).Length > 0)
+			if (additionalData != null && additionalData is EnemyProvider[] && additionalData as EnemyProvider[] != null && (additionalData as EnemyProvider[]).Length > 0)
 				foreach (EnemyProvider enemy in additionalData as EnemyProvider[])
-					if (enemy == this && data.StateForm == StateForm.Event && _statistics.ReactToDamage)
+					if (enemy && enemy == this && data.StateForm == StateForm.Event && _statistics.ReactToDamage)
+					{
 						Shoot();
+						return;
+					}
 		}
 	};
 };
