4b100039c446d526bbabb29a8efe1eb86e883f49
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-17 15:34:31 -0300

    Update Moving Enemy.cs

--- Enemies/Moving Enemy.cs
+++ Enemies/Moving Enemy.cs
@@ -29,7 +29,7 @@ namespace GwambaPrimeAdventure.Enemy
 		protected bool GroundCheck()
 		{
 			_originCast = new Vector2(transform.position.x + _collider.offset.x, transform.position.y + _collider.offset.y);
-			_originCast += new Vector2(0f, (_collider.bounds.extents.y + WorldBuild.SNAPLENGTH / 2f) * -transform.up.y);
+			_originCast.y += (_collider.bounds.extents.y + WorldBuild.SNAPLENGTH / 2f) * -transform.up.y;
 			_sizeCast = new Vector2(_collider.bounds.size.x - WorldBuild.SNAPLENGTH, WorldBuild.SNAPLENGTH);
 			return Physics2D.BoxCast(_originCast, _sizeCast, 0f, -transform.up, WorldBuild.SNAPLENGTH, _moving.Physics.GroundLayer);
 		}
