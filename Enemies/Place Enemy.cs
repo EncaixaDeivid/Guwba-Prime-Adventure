5d45ff0368e8fc6315d8c9d09ca6e864429fd9db
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-17 20:10:03 -0300

    Update Place Enemy.cs

--- Enemies/Place Enemy.cs
+++ Enemies/Place Enemy.cs
@@ -9,6 +9,7 @@ namespace GwambaPrimeAdventure.Enemy
 	{
 		private Tilemap _tilemap;
 		private TilemapCollider2D _tilemapCollider;
+		private IEnumerator _appearFade;
 		[Header("Interactions")]
 		[SerializeField, Tooltip("If anything can be hurt.")] private bool _hurtEveryone;
 		[SerializeField, Tooltip("If this enemy will react to any damage taken.")] private bool _reactToDamage;
@@ -22,38 +23,47 @@ namespace GwambaPrimeAdventure.Enemy
 		private new void OnDestroy()
 		{
 			base.OnDestroy();
-			StopAllCoroutines();
+			if (_appearFade is not null)
+				_appearFade = null;
 			Sender.Exclude(this);
 		}
+		private void Update() => _appearFade?.MoveNext();
 		public void Receive(DataConnection data, object additionalData)
 		{
-			if (additionalData == null || additionalData is not EnemyProvider[] || additionalData as EnemyProvider[] == null || (additionalData as EnemyProvider[]).Length <= 0)
-				return;
-			foreach (EnemyProvider enemy in (EnemyProvider[])additionalData)
-				if (enemy != this)
-					return;
-			if (data.StateForm == StateForm.State && data.ToggleValue.HasValue)
-				StartCoroutine(AppearFade(data.ToggleValue.Value));
-			else if (data.StateForm == StateForm.Event && _reactToDamage)
-				StartCoroutine(AppearFade(_tilemap.color.a <= 0f));
-			IEnumerator AppearFade(bool appear)
-			{
-				Color color;
-				IEnumerator Opacity(float alpha)
-				{
-					color = _tilemap.color;
-					color.a = alpha;
-					_tilemap.color = color;
-					yield return new WaitUntil(() => isActiveAndEnabled && !IsStunned);
-				}
-				if (appear)
-					for (float i = 0f; _tilemap.color.a < 1f; i += 0.1f)
-						yield return Opacity(i);
-				else
-					for (float i = 1f; _tilemap.color.a > 0f; i -= 0.1f)
-						yield return Opacity(i);
-				_tilemapCollider.enabled = appear;
-			}
+			if (additionalData != null || additionalData is EnemyProvider[] || additionalData as EnemyProvider[] != null || (additionalData as EnemyProvider[]).Length > 0)
+				foreach (EnemyProvider enemy in additionalData as EnemyProvider[])
+					if (enemy == this)
+					{
+						if (data.StateForm == StateForm.State && data.ToggleValue.HasValue)
+							_appearFade = AppearFade(data.ToggleValue.Value);
+						else if (data.StateForm == StateForm.Event && _reactToDamage)
+							_appearFade = AppearFade(_tilemap.color.a <= 0f);
+						IEnumerator AppearFade(bool appear)
+						{
+							Color color;
+							IEnumerator Opacity(float alpha)
+							{
+								color = _tilemap.color;
+								color.a = alpha;
+								_tilemap.color = color;
+								yield return null;
+							}
+							if (appear)
+								for (float i = 0f; _tilemap.color.a < 1f; i += 0.1f)
+								{
+									yield return Opacity(i);
+									yield return new WaitWhile(() => IsStunned);
+								}
+							else
+								for (float i = 1f; _tilemap.color.a > 0f; i -= 0.1f)
+								{
+									yield return Opacity(i);
+									yield return new WaitWhile(() => IsStunned);
+								}
+							_tilemapCollider.enabled = appear;
+						}
+						return;
+					}
 		}
 	};
 };
