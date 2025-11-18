1d594b4537f0d0dec721587816a37f2bd39fa498
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-18 00:20:36 -0300

    Update Hidden Object.cs

--- System/Hidden Object.cs
+++ System/Hidden Object.cs
@@ -6,6 +6,7 @@ namespace GwambaPrimeAdventure
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(BoxCollider2D))]
 	public sealed class HiddenObject : MonoBehaviour, IConnector
 	{
+		private static readonly WaitForSeconds _waitForSeconds1e = new(1e-3f);
 		[Header("Interactions")]
 		[SerializeField, Tooltip("If this object will activate the children.")] private bool _initialActive;
 		[SerializeField, Tooltip("If this object will turn off the collisions.")] private bool _offCollision;
@@ -19,7 +20,7 @@ namespace GwambaPrimeAdventure
 		private IEnumerator Start()
 		{
 			yield return new WaitWhile(() => SceneInitiator.IsInTrancision());
-			yield return new WaitForSeconds(1e-3f);
+			yield return _waitForSeconds1e;
 			if (!_initialActive)
 				for (ushort i = 0; i < transform.childCount; i++)
 					transform.GetChild(i).gameObject.SetActive(false);
