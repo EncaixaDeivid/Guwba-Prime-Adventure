2ac28987ff232bb2085aaf1c7d8e210b8ac44d4f
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-20 14:49:31 -0300

    Update Object Loader.cs

--- System/Object Loader.cs
+++ System/Object Loader.cs
@@ -6,25 +6,25 @@ namespace GwambaPrimeAdventure
 	internal sealed class ObjectLoader : MonoBehaviour
 	{
 		[SerializeField, Tooltip("The objects to be lodaed.")] private ObjectLoader[] _objectLoaders;
-		public IEnumerator Load(ProgressBar progressBar, ushort totalComplete)
+		public IEnumerator Load(ProgressBar progressBar, bool coverProgress, ushort totalComplete)
 		{
-			ushort index = 0;
-			float stillProgress;
 			ILoader[] loaders = GetComponentsInChildren<ILoader>();
-			foreach (ObjectLoader loader in _objectLoaders)
+			for (ushort i = 0; i < _objectLoaders.Length; i++)
 			{
-				stillProgress = index++ / (_objectLoaders.Length + loaders.Length + totalComplete);
-				progressBar.value += stillProgress;
-				yield return StartCoroutine(Instantiate(loader).Load(progressBar, (ushort)(_objectLoaders.Length + loaders.Length + totalComplete)));
-				progressBar.value -= stillProgress;
+				progressBar.value += (i + 1f) / (_objectLoaders.Length + loaders.Length + totalComplete);
+				yield return StartCoroutine(Instantiate(_objectLoaders[i]).Load(progressBar, false, (ushort)(_objectLoaders.Length + loaders.Length)));
+				if (i < _objectLoaders.Length - 1)
+					progressBar.value -= (i + 1f) / (_objectLoaders.Length + loaders.Length + totalComplete);
 			}
-			foreach (ILoader loader in loaders)
+			for (ushort i = 0; i < loaders.Length; i++)
 			{
-				stillProgress = index++ / (_objectLoaders.Length + loaders.Length + totalComplete);
-				progressBar.value += stillProgress;
-				yield return StartCoroutine(loader.Load());
-				progressBar.value -= stillProgress;
+				progressBar.value += (i + 1f) / (_objectLoaders.Length + loaders.Length + totalComplete);
+				yield return StartCoroutine(loaders[i].Load());
+				if (i < loaders.Length - 1)
+					progressBar.value -= (i + 1f) / (_objectLoaders.Length + loaders.Length + totalComplete);
 			}
+			if (coverProgress && _objectLoaders.Length + loaders.Length <= 0)
+				progressBar.value += 1f;
 			transform.DetachChildren();
 			Destroy(gameObject);
 		}
