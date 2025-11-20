51e247400cdc894adf22ee5df54fa933ac2dcd77
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-20 15:39:44 -0300

    Update Surface.cs

--- System/Surface.cs
+++ System/Surface.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using System.Collections;
 namespace GwambaPrimeAdventure
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
-	public sealed class Surface : MonoBehaviour
+	public sealed class Surface : MonoBehaviour, ILoader
 	{
 		[SerializeField] private bool _isScene;
 		public bool IsScene => _isScene;
+		public IEnumerator Load() => null;
 	};
 };
