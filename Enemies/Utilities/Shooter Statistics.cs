5d5db7d61cf831050c96f3902a8f52924418f690
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-22 22:28:58 -0300

    Update Shooter Statistics.cs

--- Enemies/Utilities/Shooter Statistics.cs
+++ Enemies/Utilities/Shooter Statistics.cs
@@ -1,43 +1,29 @@
 using UnityEngine;
+using NaughtyAttributes;
 namespace GwambaPrimeAdventure.Enemy.Utility
 {
 	[CreateAssetMenu(fileName = "Shooter Enemy", menuName = "Enemy Statistics/Shooter", order = 5)]
 	public sealed class ShooterStatistics : ScriptableObject
 	{
-		[Header("Shooter Enemy")]
-		[SerializeField, Tooltip("The physics of the enemy.")] private EnemyPhysics _physics;
-		[SerializeField, Tooltip("The projectiles that this enemy can instantiate.")] private Projectile[] _projectiles;
-		[SerializeField, Tooltip("The distance this enemy can detect the target.")] private float _perceptionDistance;
-		[SerializeField, Tooltip("The angle fo the direction of ray of the detection.")] private float _rayAngleDirection;
-		[SerializeField, Tooltip("The amount of time to wait to execute another shoot.")] private float _intervalToShoot;
-		[SerializeField, Tooltip("The amount of time to stop this enemy to move.\nRequires: Moving Enemy")] private float _stopTime;
-		[SerializeField, Tooltip("If this enemy will turn the ray to the looking side.")] private bool _turnRay;
-		[SerializeField, Tooltip("If this enemy will become invencible while shooting.\nRequires: Defender Enemy")]	private bool _invencibleShoot;
-		[SerializeField, Tooltip("If this enemy gets hurt it will shoot.")] private bool _shootDamaged;
-		[SerializeField, Tooltip("If this enemy will stop moving when shoot.\nRequires: Moving Enemy")]	private bool _stop;
-		[SerializeField, Tooltip("If this enemy will paralyze moving.\nRequires: Moving Enemy")] private bool _paralyze;
-		[SerializeField, Tooltip("If this enemy will return the gravity after paralyze.\nRequires: Moving Enemy")] private bool _returnParalyze;
-		[SerializeField, Tooltip("If the detection will be circular.")] private bool _circularDetection;
-		[SerializeField, Tooltip("Will shoot to infinity without any detection.")] private bool _shootInfinity;
-		[SerializeField, Tooltip("If this enemy won't interfere in the projectile.")] private bool _pureInstance;
-		[SerializeField, Tooltip("If the projectile will be instantiate on the same point as this enemy.")] private bool _instanceOnSelf;
-		[SerializeField, Tooltip("If this enemy will react to any damage taken.")] private bool _reactToDamage;
-		public EnemyPhysics Physics => _physics;
-		public Projectile[] Projectiles => _projectiles;
-		public float PerceptionDistance => _perceptionDistance;
-		public float RayAngleDirection => _rayAngleDirection;
-		public float IntervalToShoot => _intervalToShoot;
-		public float StopTime => _stopTime;
-		public bool TurnRay => _turnRay;
-		public bool InvencibleShoot => _invencibleShoot;
-		public bool ShootDamaged => _shootDamaged;
-		public bool Stop => _stop;
-		public bool Paralyze => _paralyze;
-		public bool ReturnParalyze => _returnParalyze;
-		public bool CircularDetection => _circularDetection;
-		public bool ShootInfinity => _shootInfinity;
-		public bool PureInstance => _pureInstance;
-		public bool InstanceOnSelf => _instanceOnSelf;
-		public bool ReactToDamage => _reactToDamage;
+		[field: SerializeField, Tooltip("The physics of the enemy."), Header("Shooter Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
+		public EnemyPhysics Physics { get; private set; }
+		[field: SerializeField, Tooltip("The projectiles that this enemy can instantiate.")] public Projectile[] Projectiles { get; private set; }
+		[field: SerializeField, Tooltip("Will shoot to infinity without any detection.")] public bool ShootInfinity { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), Min(0f), Tooltip("The distance this enemy can detect the target.")] public float PerceptionDistance { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), Tooltip("If the detection will be circular.")] public bool CircularDetection { get; private set; }
+		[field: SerializeField, HideIf(EConditionOperator.Or ,nameof(ShootInfinity), nameof(CircularDetection)), Min(0f), Tooltip("The angle fo the direction of ray of the detection.")]
+		public float RayAngleDirection { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), Min(0f), Tooltip("The amount of time to wait to execute another shoot.")] public float IntervalToShoot { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), Tooltip("If this enemy will stop moving when shoot.\nRequires: Moving Enemy.")] public bool Stop { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), ShowIf(nameof(Stop)), Min(1e-3f), Tooltip("The amount of time to stop this enemy to move.\nRequires: Moving Enemy.")]
+		public float StopTime { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), Tooltip("If this enemy will turn the ray to the looking side.")] public bool TurnRay { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), Tooltip("If this enemy will become invencible while shooting.\nRequires: Defender Enemy.")]
+		public bool InvencibleShoot { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), ShowIf(nameof(Stop)), Tooltip("If this enemy will paralyze moving.\nRequires: Moving Enemy.")] public bool Paralyze { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy won't interfere in the projectile.")] public bool PureInstance { get; private set; }
+		[field: SerializeField, Tooltip("If the projectile will be instantiate on the same point as this enemy.")] public bool InstanceOnSelf { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy gets hurt it will shoot.")] public bool ShootDamaged { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy will react to any damage taken.")] public bool ReactToDamage { get; private set; }
 	};
 };
