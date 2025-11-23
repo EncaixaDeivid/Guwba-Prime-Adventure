19847d636a71e22feaff5116dec4b11343f93395
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-23 16:00:47 -0300

    Update Data Connections.cs

--- System/Connection/Data Connections.cs
+++ System/Connection/Data Connections.cs
@@ -1,15 +1,32 @@
 namespace GwambaPrimeAdventure.Connection
 {
+	public interface IConnector
+	{
+		public PathConnection PathConnection { get; }
+		public void Receive(DataConnection data);
+	};
 	public struct DataConnection
 	{
-		public StateForm StateForm { get; private set; }
-		public bool? ToggleValue { get; private set; }
-		public uint? NumberValue { get; private set; }
-		internal DataConnection(StateForm stateForm, bool? toggleValue, uint? numberValue)
-		{
-			StateForm = stateForm;
-			ToggleValue = toggleValue;
-			NumberValue = numberValue;
-		}
+		public StateForm StateForm { get; internal set; }
+		public object AdditionalData { get; internal set; }
+		public bool? ToggleValue { get; internal set; }
+		public ushort? NumberValue { get; internal set; }
+	};
+	public enum PathConnection
+	{
+		None,
+		System,
+		Hud,
+		Character,
+		Enemy,
+		Item,
+		EventItem,
+		Story
+	};
+	public enum StateForm
+	{
+		None,
+		State,
+		Event
 	};
 };
