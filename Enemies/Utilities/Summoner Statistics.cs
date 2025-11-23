e7b02507122cc711fa165b3ba36af4d0cc1de170
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-22 22:29:11 -0300

    Update Summoner Statistics.cs

--- Enemies/Utilities/Summoner Statistics.cs
+++ Enemies/Utilities/Summoner Statistics.cs
@@ -5,28 +5,19 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 	[CreateAssetMenu(fileName = "Summoner Enemy", menuName = "Enemy Statistics/Summoner", order = 6)]
 	public sealed class SummonerStatistics : ScriptableObject
 	{
-		[Header("Summoner Enemy")]
-		[SerializeField, Tooltip("The summons that will be activate on an event.")] private SummonObject[] _eventSummons;
-		[SerializeField, Tooltip("The summons that will be activate with time.")] private SummonObject[] _timedSummons;
-		[SerializeField, Tooltip("The collection of the summon point structure.")] private SummonPointStructure[] _summonPointStructures;
-		[SerializeField, Tooltip("If this enemy will summon randomized in the react.")] private bool _randomReactSummons;
-		[SerializeField, Tooltip("If this enemy will summon randomized timed.")] private bool _randomTimedSummons;
-		[SerializeField, Tooltip("If this enemy will react to any damage taken.")] private bool _hasEventSummon;
-		public SummonObject[] EventSummons => _eventSummons;
-		public SummonObject[] TimedSummons => _timedSummons;
-		public SummonPointStructure[] SummonPointStructures => _summonPointStructures;
-		public bool RandomReactSummons => _randomReactSummons;
-		public bool RandomTimedSummons => _randomTimedSummons;
-		public bool HasEventSummon => _hasEventSummon;
+		[field: SerializeField, Tooltip("The summons that will be activate on an event."), Header("Summoner Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
+		public SummonObject[] EventSummons { get; private set; }
+		[field: SerializeField, Tooltip("The summons that will be activate with time.")] public SummonObject[] TimedSummons { get; private set; }
+		[field: SerializeField, Tooltip("The collection of the summon point structure.")] public SummonPointStructure[] SummonPointStructures { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy will summon randomized in the react.")] public bool RandomReactSummons { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy will summon randomized timed.")] public bool RandomTimedSummons { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy will react to any damage taken.")] public bool HasEventSummon { get; private set; }
 	};
 	[Serializable]
 	public struct SummonPointStructure
 	{
-		[SerializeField, Tooltip("The object to activate the summon.")] private SummonPoint _summonPointObject;
-		[SerializeField, Tooltip("Which summon event the summon point will activate.")] private SummonObject _objectToSummon;
-		[SerializeField, Tooltip("The point where the summon point will be.")] private Vector2 _point;
-		public readonly SummonPoint SummonPointObject => _summonPointObject;
-		public readonly SummonObject Summon => _objectToSummon;
-		public readonly Vector2 Point => _point;
+		[field: SerializeField, Tooltip("The object to activate the summon.")] public SummonPoint SummonPointObject { get; private set; }
+		[field: SerializeField, Tooltip("Which summon event the summon point will activate.")] public SummonObject Summon { get; private set; }
+		[field: SerializeField, Tooltip("The point where the summon point will be.")] public Vector2 Point { get; private set; }
 	};
 };
