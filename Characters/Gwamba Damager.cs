b42868c7e4b18244fc5cde8a893e81775ee814fa
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-23 04:07:35 -0300

    Update Gwamba Damager.cs

--- Characters/Gwamba Damager.cs
+++ Characters/Gwamba Damager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 using System;
+using System.Collections.Generic;
 using NaughtyAttributes;
 namespace GwambaPrimeAdventure.Character
 {
@@ -11,6 +12,7 @@ namespace GwambaPrimeAdventure.Character
 		private Predicate<ushort> _damagerHurt;
 		private UnityAction<ushort, float> _damagerStun;
 		private UnityAction<GwambaDamager, IDestructible> _damagerAttack;
+		private readonly List<IDestructible> _damagerDamaged = new();
 		[SerializeField, BoxGroup("Stats"), Tooltip("If this Gwamba's part will take damage."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f)] private bool _takeDamage;
 		[field: SerializeField, BoxGroup("Stats"), HideIf(nameof(_takeDamage)), Tooltip("The velocity of the screen shake on the attack.")] internal Vector2 AttackShake { get; private set; }
 		[field: SerializeField, BoxGroup("Stats"), HideIf(nameof(_takeDamage)), Tooltip("The amount of damage that the attack of Gwamba hits.")] internal ushort AttackDamage { get; private set; }
@@ -21,6 +23,7 @@ namespace GwambaPrimeAdventure.Character
 		internal Predicate<ushort> DamagerHurt { get => null; set => _damagerHurt = value; }
 		internal UnityAction<ushort, float> DamagerStun { get => null; set => _damagerStun = value; }
 		internal UnityAction<GwambaDamager, IDestructible> DamagerAttack { get => null; set => _damagerAttack = value; }
+		internal List<IDestructible> DamagerDamaged => _damagerDamaged;
 		internal float Alpha { get => _spriteRenderer.color.a; set => _spriteRenderer.color = new(1f, 1f, 1f, value); }
 		public short Health => 0;
 		private new void Awake()
@@ -42,7 +45,8 @@ namespace GwambaPrimeAdventure.Character
 		private void OnTriggerEnter2D(Collider2D other)
 		{
 			if (!_takeDamage && other.TryGetComponent<IDestructible>(out var destructible))
-				_damagerAttack.Invoke(this, destructible);
+				if (!_damagerDamaged.Contains(destructible))
+					_damagerAttack.Invoke(this, destructible);
 		}
 	};
 };
