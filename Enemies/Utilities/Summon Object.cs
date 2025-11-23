c76c5e38fc3aa858d11d0520174c867c84079a40
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-23 11:14:09 -0300

    Update Summon Object.cs

--- Enemies/Utilities/Summon Object.cs
+++ Enemies/Utilities/Summon Object.cs
@@ -13,11 +13,14 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 		[field: SerializeField, Tooltip("The amount of time to execute the instance.")] public ushort SummonTime { get; private set; }
 		[field: SerializeField, Tooltip("The amount of time to wait to execute the next instance.")] public ushort PostSummonTime { get; private set; }
 		[field: SerializeField, Tooltip("If the instanciation will be in the same point as the summoner.")] public bool Self { get; private set; }
-		[field: SerializeField, Tooltip("If the instantiation will be randomized at one of the points.")] public bool Random { get; private set; }
+		[field: SerializeField, HideIf(nameof(Self)), Tooltip("If the instantiation will be randomized at one of the points.")] public bool Random { get; private set; }
 		[field: SerializeField, Tooltip("If the instantiator will stop during the summon.")] public bool StopToSummon { get; private set; }
 		[field: SerializeField, ShowIf(nameof(StopToSummon)), Tooltip("If the instantiator will paralyze during the summon.")] public bool ParalyzeToSummon { get; private set; }
 		[field: SerializeField, ShowIf(nameof(StopToSummon)), Tooltip("If the timed summon will stop permanently.")] public bool StopPermanently { get; private set; }
 		[field: SerializeField, ShowIf(nameof(StopToSummon)), HideIf(nameof(StopPermanently)), Min(0f), Tooltip("The amount of time to stop the instantiator.")]
 		public float TimeToStop { get; private set; }
+		[field: SerializeField, ShowIf(nameof(StopToSummon)), Tooltip("If the instanciation will be instantly after stop.")] public bool InstantlySummon { get; private set; }
+		[field: SerializeField, HideIf(nameof(InstantlySummon)), ShowIf(nameof(StopToSummon)), Tooltip("If the instanciation will wait return from the stop to summon.")]
+		public bool WaitStop { get; private set; }
 	};
 };
