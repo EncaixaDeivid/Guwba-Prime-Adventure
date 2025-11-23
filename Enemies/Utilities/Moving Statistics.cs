dbbc6156834aa20ead63238a152d21be2f3537cf
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-22 22:27:28 -0300

    Update Moving Statistics.cs

--- Enemies/Utilities/Moving Statistics.cs
+++ Enemies/Utilities/Moving Statistics.cs
@@ -1,24 +1,18 @@
 using UnityEngine;
+using NaughtyAttributes;
 namespace GwambaPrimeAdventure.Enemy.Utility
 {
 	public abstract class MovingStatistics : ScriptableObject
 	{
-		[Header("Moving Statistics")]
-		[SerializeField, Tooltip("The physics of the enemy.")] private EnemyPhysics _physics;
-		[SerializeField, Tooltip("The speed of the enemy to moves towards.")] private float _movementSpeed;
-		[SerializeField, Tooltip("The amount of speed of the dash.")] private float _dashSpeed;
-		[SerializeField, Tooltip("The distance of the detection of target.")] private float _lookDistance;
-		[SerializeField, Tooltip("If this enemy will do some action when look to a target.")] private bool _lookPerception;
-		[SerializeField, Tooltip("If this enemy will stop on detection of the target.")] private bool _detectionStop;
-		[SerializeField, Tooltip("If this enemy will react to any damage taken.")] private bool _reactToDamage;
-		[SerializeField, Tooltip("The amount of time this enemy will stop on detection.")] private float _stopTime;
-		public EnemyPhysics Physics => _physics;
-		public float MovementSpeed => _movementSpeed;
-		public float DashSpeed => _dashSpeed;
-		public float LookDistance => _lookDistance;
-		public bool LookPerception => _lookPerception;
-		public bool DetectionStop => _detectionStop;
-		public bool ReactToDamage => _reactToDamage;
-		public float StopTime => _stopTime;
+		[field: SerializeField, Tooltip("physics."), Header("Moving Statistics", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)] public EnemyPhysics Physics { get; private set; }
+		[field: SerializeField, Min(0f), Tooltip("The speed of the enemy to moves towards.")] public float MovementSpeed { get; private set; }
+		[field: SerializeField, Min(0f), Tooltip("The amount of speed of the dash.")] public float DashSpeed { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy will do some action when look to a target.")] public bool LookPerception { get; private set; }
+		[field: SerializeField, ShowIf(nameof(LookPerception)), Min(0f), Tooltip("The distance of the detection of target."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f)]
+		public float LookDistance { get; private set; }
+		[field: SerializeField, ShowIf(nameof(LookPerception)), Tooltip("If this enemy will stop on detection of the target.")] public bool DetectionStop { get; private set; }
+		[field: SerializeField, ShowIf(nameof(LookPerception)), Min(1e-3f), Tooltip("The amount of time this enemy will stop on detection.")]
+		public float StopTime { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy will react to any damage taken.")] public bool ReactToDamage { get; private set; }
 	};
 };
