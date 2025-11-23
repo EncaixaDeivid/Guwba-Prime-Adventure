08470190ad1744a00c33881ac59c8afcdf92e6d0
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-23 16:00:33 -0300

    Update Sender.cs

--- System/Connection/Sender.cs
+++ System/Connection/Sender.cs
@@ -5,16 +5,16 @@ namespace GwambaPrimeAdventure.Connection
 	{
 		private Sender()
 		{
-			_stateForm = StateForm.None;
-			_additionalData = null;
-			_toggleValue = null;
-			_numberValue = null;
+			_dataConnection = new()
+			{
+				StateForm = StateForm.None,
+				AdditionalData = null,
+				ToggleValue = null,
+				NumberValue = null
+			};
 		}
 		private static readonly Dictionary<PathConnection, List<IConnector>> _connectors = new();
-		private StateForm _stateForm;
-		private object _additionalData;
-		private bool? _toggleValue;
-		private uint? _numberValue;
+		private DataConnection _dataConnection;
 		public static void Include(IConnector connector)
 		{
 			if (!_connectors.ContainsKey(connector.PathConnection))
@@ -28,16 +28,16 @@ namespace GwambaPrimeAdventure.Connection
 				_connectors[connector.PathConnection].Remove(connector);
 		}
 		public static Sender Create() => new();
-		public void SetAdditionalData(object additionalData) => _additionalData = additionalData;
-		public void SetStateForm(StateForm stateForm) => _stateForm = stateForm;
-		public void SetToggle(bool value) => _toggleValue = value;
-		public void SetNumber(int value) => _numberValue = (uint)(value < 0f ? -value : value);
+		public void SetAdditionalData(object additionalData) => _dataConnection.AdditionalData = additionalData;
+		public void SetStateForm(StateForm stateForm) => _dataConnection.StateForm = stateForm;
+		public void SetToggle(bool value) => _dataConnection.ToggleValue = value;
+		public void SetNumber(int value) => _dataConnection.NumberValue = (ushort)(value < 0f ? -value : value);
 		public void Send(PathConnection path)
 		{
 			if (_connectors.ContainsKey(path))
 				foreach (IConnector connector in _connectors[path].ToArray())
 					if (connector != null && connector.PathConnection == path)
-						connector.Receive(new DataConnection(_stateForm, _toggleValue, _numberValue), _additionalData);
+						connector.Receive(_dataConnection);
 		}
 	};
 };
