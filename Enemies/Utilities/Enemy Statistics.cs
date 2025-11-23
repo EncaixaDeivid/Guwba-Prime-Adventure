7a379d686989ac7ecde3a15b562bfde0d676070f
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-22 22:27:09 -0300

    Update Enemy Statistics.cs

--- Enemies/Utilities/Enemy Statistics.cs
+++ Enemies/Utilities/Enemy Statistics.cs
@@ -1,43 +1,29 @@
 using UnityEngine;
+using NaughtyAttributes;
 namespace GwambaPrimeAdventure.Enemy.Utility
 {
 	[CreateAssetMenu(fileName = "Provider Enemy", menuName = "Enemy Statistics/Provider", order = 1)]
 	public sealed class EnemyStatistics : ScriptableObject
 	{
-		[Header("Providence Statistics")]
-		[SerializeField, Tooltip("The physics of the enemy.")] private EnemyPhysics _physics;
-		[SerializeField, Tooltip("The velocity of the screen shake on the hurt.")] private Vector2 _hurtShake;
-		[SerializeField, Tooltip("The vitality of the enemy.")] private ushort _vitality;
-		[SerializeField, Tooltip("The amount of stun that this enemy can resists.")] private ushort _hitResistance;
-		[SerializeField, Tooltip("The amount of damage that the enemy hit.")] private ushort _damage;
-		[SerializeField, Tooltip("If this enemy receives no type of damage.")] private bool _noDamage;
-		[SerializeField, Tooltip("If this enemy won't do damage at contact.")] private bool _noHit;
-		[SerializeField, Tooltip("If this enemy won't get stunned.")] private bool _noHitStun;
-		[SerializeField, Tooltip("If this enemy won't get stunned.")] private bool _noStun;
-		[SerializeField, Tooltip("If this enemy will fade away over time.")] private bool _fadeOverTime;
-		[SerializeField, Tooltip("The amount of time this enemy will fade away.")] private float _timeToFadeAway;
-		[SerializeField, Tooltip("The amount of time this enemy will stun.")] private float _stunTime;
-		[SerializeField, Tooltip("The amount of time this enemy will be stunned when armor be broken.")] private float _stunnedTime;
-		[SerializeField, Tooltip("If this enemy will react to any damage taken.")] private bool _reactToDamage;
-		[SerializeField, Tooltip("If this enemy has a index atribute to use.")] private bool _hasIndex;
-		[SerializeField, Tooltip("The index to a event to a enemy make.")] private ushort _indexEvent;
-		[SerializeField, Tooltip("If this object will be saved as already existent object.")] private bool _saveOnSpecifics;
-		public EnemyPhysics Physics => _physics;
-		public Vector2 HurtShake => _hurtShake;
-		public ushort Vitality => _vitality;
-		public ushort HitResistance => _hitResistance;
-		public ushort Damage => _damage;
-		public bool NoDamage => _noDamage;
-		public bool NoHit => _noHit;
-		public bool NoHitStun => _noHitStun;
-		public bool NoStun => _noStun;
-		public bool FadeOverTime => _fadeOverTime;
-		public float TimeToFadeAway => _timeToFadeAway;
-		public float StunTime => _stunTime;
-		public float StunnedTime => _stunnedTime;
-		public bool ReactToDamage => _reactToDamage;
-		public bool HasIndex => _hasIndex;
-		public ushort IndexEvent => _indexEvent;
-		public bool SaveOnSpecifics => _saveOnSpecifics;
+		[field: SerializeField, Tooltip("The physics of the enemy."), Header("Providence Statistics", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
+		public EnemyPhysics Physics { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy receives no type of damage.")] public bool NoDamage { get; private set; }
+		[field: SerializeField, HideIf(nameof(NoDamage)), Tooltip("The vitality of the enemy.")] public ushort Vitality { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy won't get stunned.")] public bool NoStun { get; private set; }
+		[field: SerializeField, HideIf(nameof(NoStun)), Tooltip("The amount of stun that this enemy can resists."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f)]
+		public ushort HitResistance { get; private set; }
+		[field: SerializeField, HideIf(nameof(NoStun)), Tooltip("If this enemy won't get stunned when hitted.")] public bool NoHitStun { get; private set; }
+		[field: SerializeField, HideIf(nameof(NoStun)), Tooltip("The amount of time this enemy will be stunned when armor be broken.")] public float StunnedTime { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy won't do damage at contact.")] public bool NoHit { get; private set; }
+		[field: SerializeField, HideIf(nameof(NoHit)), Tooltip("The velocity of the screen shake on the hurt."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f)]
+		public Vector2 HurtShake { get; private set; }
+		[field: SerializeField, HideIf(nameof(NoHit)), Tooltip("The amount of damage that the enemy hit.")] public ushort Damage { get; private set; }
+		[field: SerializeField, HideIf(nameof(NoHit)), Tooltip("The amount of time this enemy will stun.")] public float StunTime { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy will fade away over time.")] public bool FadeOverTime { get; private set; }
+		[field: SerializeField, ShowIf(nameof(FadeOverTime)), Tooltip("The amount of time this enemy will fade away.")] public float TimeToFadeAway { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy will react to any damage taken.")] public bool ReactToDamage { get; private set; }
+		[field: SerializeField, ShowIf(nameof(ReactToDamage)), Tooltip("If this enemy has a index atribute to use.")] public bool HasIndex { get; private set; }
+		[field: SerializeField, ShowIf(nameof(HasIndex)), Tooltip("The index to a event to a enemy make.")] public ushort IndexEvent { get; private set; }
+		[field: SerializeField, Tooltip("If this object will be saved as already existent object.")] public bool SaveOnSpecifics { get; private set; }
 	};
 };
