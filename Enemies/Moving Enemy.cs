0b358c0bb244d5c131785486ee90ad33a40943ee
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-27 02:23:39 -0300

    Update Moving Enemy.cs

--- Enemies/Moving Enemy.cs
+++ Enemies/Moving Enemy.cs
@@ -7,6 +7,7 @@ namespace GwambaPrimeAdventure.Enemy
 	{
 		protected Vector2 _originCast;
 		protected Vector2 _sizeCast;
+		private bool _onGround = false;
 		protected bool _detected = false;
 		protected bool _isDashing = false;
 		protected float _stoppedTime = 0f;
@@ -14,6 +15,7 @@ namespace GwambaPrimeAdventure.Enemy
 		[Header("Moving Enemy")]
 		[SerializeField, Tooltip("The moving statitics of this enemy.")] private MovingStatistics _moving;
 		[SerializeField, Tooltip("If this enemy will moves firstly to the left.")] private bool _invertMovementSide;
+		protected bool OnGround => _onGround;
 		protected new void Awake()
 		{
 			base.Awake();
@@ -26,13 +28,16 @@ namespace GwambaPrimeAdventure.Enemy
 			base.OnDestroy();
 			Sender.Exclude(this);
 		}
-		protected bool GroundCheck()
+		protected void FixedUpdate() => _onGround = false;
+		private void GroundCheck()
 		{
 			_originCast = new Vector2(transform.position.x + _collider.offset.x, transform.position.y + _collider.offset.y);
 			_originCast.y += (_collider.bounds.extents.y + WorldBuild.SNAP_LENGTH / 2f) * -transform.up.y;
 			_sizeCast = new Vector2(_collider.bounds.size.x - WorldBuild.SNAP_LENGTH, WorldBuild.SNAP_LENGTH);
-			return Physics2D.BoxCast(_originCast, _sizeCast, 0f, -transform.up, WorldBuild.SNAP_LENGTH, _moving.Physics.GroundLayer);
+			_onGround = Physics2D.BoxCast(_originCast, _sizeCast, 0f, -transform.up, WorldBuild.SNAP_LENGTH, _moving.Physics.GroundLayer);
 		}
+		private void OnCollisionEnter2D(Collision2D collision) => GroundCheck();
+		private void OnCollisionStay2D(Collision2D collision) => GroundCheck();
 		public void Receive(DataConnection data)
 		{
 			if (data.StateForm == StateForm.State && data.ToggleValue.HasValue)
