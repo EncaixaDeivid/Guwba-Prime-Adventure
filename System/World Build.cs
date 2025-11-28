4c92ed454e14a5351477974cf67c2a6bb69ea138
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-28 01:21:22 -0300

    Update World Build.cs

--- System/World Build.cs
+++ System/World Build.cs
@@ -6,6 +6,28 @@ namespace GwambaPrimeAdventure
 		public const float FIELD_SPACE_LENGTH = 8f;
 		public const float SNAP_LENGTH = 1f / 16f;
 		private static float _localScaleX;
+		public enum Layers
+		{
+			System,
+			Character,
+			Scene,
+			Item,
+			Enemy,
+			Boss
+		};
+		public static readonly LayerMask SystemMask = GetMask(Layers.System);
+		public static readonly LayerMask CharacterMask = GetMask(Layers.Character);
+		public static readonly LayerMask SceneMask = GetMask(Layers.Scene);
+		public static readonly LayerMask ItemMask = GetMask(Layers.Item);
+		public static readonly LayerMask EnemyMask = GetMask(Layers.Enemy);
+		public static readonly LayerMask BossMask = GetMask(Layers.Boss);
+		private static LayerMask GetMask(Layers layerName)
+		{
+			for (int i = 0; i < 32; i++)
+				if (LayerMask.LayerToName(i) != string.Empty && LayerMask.LayerToName(i).ToUpper() == layerName.ToString().ToUpper())
+					return 1 << i;
+			return 0;
+		}
 		public static void TurnScaleX(this Transform transform, float valueChanger)
 		{
 			_localScaleX = Mathf.Abs(transform.localScale.x) * valueChanger;
