ce9958a03e61c3f231cf92e776241b8ff81af151
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-28 01:27:23 -0300

    Update Enemy Statistics.cs

--- Enemies/Utilities/Enemy Statistics.cs
+++ Enemies/Utilities/Enemy Statistics.cs
@@ -5,9 +5,8 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 	[CreateAssetMenu(fileName = "Provider Enemy", menuName = "Enemy Statistics/Provider", order = 1)]
 	public sealed class EnemyStatistics : ScriptableObject
 	{
-		[field: SerializeField, Tooltip("The physics of the enemy."), Header("Providence Statistics", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
-		public EnemyPhysics Physics { get; private set; }
-		[field: SerializeField, Tooltip("If this enemy receives no type of damage.")] public bool NoDamage { get; private set; }
+		[field: SerializeField, Tooltip("If this enemy receives no type of damage."), Header("Providence Statistics", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
+		public bool NoDamage { get; private set; }
 		[field: SerializeField, HideIf(nameof(NoDamage)), Tooltip("The vitality of the enemy.")] public ushort Vitality { get; private set; }
 		[field: SerializeField, Tooltip("If this enemy won't get stunned.")] public bool NoStun { get; private set; }
 		[field: SerializeField, HideIf(nameof(NoStun)), Tooltip("The amount of stun that this enemy can resists."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f)]
@@ -19,6 +18,8 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 		public Vector2 HurtShake { get; private set; }
 		[field: SerializeField, HideIf(nameof(NoHit)), Tooltip("The amount of damage that the enemy hit.")] public ushort Damage { get; private set; }
 		[field: SerializeField, HideIf(nameof(NoHit)), Tooltip("The amount of time this enemy will stun.")] public float StunTime { get; private set; }
+		[field: SerializeField, HideIf(nameof(NoHit)), Tooltip("The amount of time to stop the game when hit is given.")] public float HitStopTime { get; private set; }
+		[field: SerializeField, HideIf(nameof(NoHit)), Tooltip("The amount of time to slow the game when hit is given.")] public float HitSlowTime { get; private set; }
 		[field: SerializeField, Tooltip("If this enemy will fade away over time.")] public bool FadeOverTime { get; private set; }
 		[field: SerializeField, ShowIf(nameof(FadeOverTime)), Tooltip("The amount of time this enemy will fade away.")] public float TimeToFadeAway { get; private set; }
 		[field: SerializeField, Tooltip("If this enemy will react to any damage taken.")] public bool ReactToDamage { get; private set; }
