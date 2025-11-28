863f15204bdaa16fa3fe096bf63971c323c19802
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-28 06:36:40 -0300

    Update Moving Statistics.cs

--- Enemies/Utilities/Moving Statistics.cs
+++ Enemies/Utilities/Moving Statistics.cs
@@ -8,8 +8,7 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 		public float MovementSpeed { get; private set; }
 		[field: SerializeField, Min(0f), Tooltip("The amount of speed of the dash.")] public float DashSpeed { get; private set; }
 		[field: SerializeField, Tooltip("If this enemy will do some action when look to a target.")] public bool LookPerception { get; private set; }
-		[field: SerializeField, ShowIf(nameof(LookPerception)), Min(0f), Tooltip("The distance of the detection of target."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f)]
-		public float LookDistance { get; private set; }
+		[field: SerializeField, ShowIf(nameof(LookPerception)), Min(0f), Tooltip("The distance of the detection of target.")] public float LookDistance { get; private set; }
 		[field: SerializeField, ShowIf(nameof(LookPerception)), Tooltip("If this enemy will stop on detection of the target.")] public bool DetectionStop { get; private set; }
 		[field: SerializeField, ShowIf(nameof(LookPerception)), Min(1e-3f), Tooltip("The amount of time this enemy will stop on detection.")]
 		public float StopTime { get; private set; }
