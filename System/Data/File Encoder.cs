5d15326edc418951177d77547b7d2786119e1c5b
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-27 21:54:00 -0300

    Update File Encoder.cs

--- System/Data/File Encoder.cs
+++ System/Data/File Encoder.cs
@@ -5,7 +5,7 @@ namespace GwambaPrimeAdventure.Data
 {
 	internal static class FileEncoder
 	{
-		private const string ScriptPassword = "";
+		private const string ScriptPassword = "BoLo%De%CeNoUrA%cOm%CoBeRtUrA%dE%cHoCoLaTe%AmArGo%!";
 		private static string ScriptData(string data)
 		{
 			string scriptedData = "";
@@ -13,8 +13,8 @@ namespace GwambaPrimeAdventure.Data
 				scriptedData += (char)(data[i] ^ ScriptPassword[i % ScriptPassword.Length]);
 			return scriptedData;
 		}
-		internal static StructData ReadData<StructData>(string path) where StructData : struct => JsonUtility.FromJson<StructData>(ScriptData(File.ReadAllText(path, Encoding.UTF8)));
-		internal static void WriteData<StructData>(StructData structData, string path) where StructData : struct
+		internal static Data ReadData<Data>(string path) where Data : struct => JsonUtility.FromJson<Data>(ScriptData(File.ReadAllText(path, Encoding.UTF8)));
+		internal static void WriteData<Data>(Data structData, string path) where Data : struct
 		{
 			if (File.Exists(path))
 				File.Delete(path);
