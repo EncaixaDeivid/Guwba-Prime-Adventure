74358d28c5b2de82eb8bc18741ad0f0cf69d1e8e
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-29 03:25:52 -0300

    Add files via upload

---
+++ System/Info Logger.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.IO;
+using System.Runtime.CompilerServices;
+namespace GwambaPrimeAdventure
+{
+	public interface IInfoLogger
+	{
+		public void LogInfo(object message, [CallerMemberName] string member = "", [CallerFilePath] string file = "", [CallerLineNumber] int line = 0)
+			=> Debug.Log($"[{Path.GetFileName(file)} : {line} - {member}] " + message);
+	};
+	public sealed class InfoLogger : IInfoLogger
+	{
+		public static IInfoLogger Informer = new InfoLogger();
+		private InfoLogger() { }
+	};
+};
+#endif
