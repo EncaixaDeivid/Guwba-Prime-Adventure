8ccf38ac0f742b6944497e1521cde1bd08b9af84
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-30 03:14:34 -0300

    Update Death Statistics.cs

--- Enemies/Utilities/Death Statistics.cs
+++ Enemies/Utilities/Death Statistics.cs
@@ -7,6 +7,7 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 		[field: SerializeField, Tooltip("The enemy that this enemy will spawn in death."), Header("Death Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
 		public Control ChildEnemy { get; private set; }
 		[field: SerializeField, Tooltip("The projectile that this enemy will spawn in death.")] public Projectile ChildProjectile { get; private set; }
+		[field: SerializeField, Tooltip("The point to where spawn the object on death relative to this enemy.")] public Vector2 SpawnPoint { get; private set; }
 		[field: SerializeField, Tooltip("If this enemy will die on touch.")] public bool OnTouch { get; private set; }
 		[field: SerializeField, Tooltip("The time to this enemy die.")] public float TimeToDie { get; private set; }
 	};
