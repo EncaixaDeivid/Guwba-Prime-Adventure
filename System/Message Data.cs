96111769ca27a2b6991d1f6aee4b08f5556fa815
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-02 10:08:07 -0300

    Add files via upload

---
+++ System/Message Data.cs
@@ -0,0 +1,27 @@
+namespace GwambaPrimeAdventure
+{
+	public struct MessageData
+	{
+		public MessageFormat Format { get; internal set; }
+		public object AdditionalData { get; internal set; }
+		public bool? ToggleValue { get; internal set; }
+		public ushort? NumberValue { get; internal set; }
+	};
+	public enum MessagePath
+	{
+		None,
+		System,
+		Hud,
+		Character,
+		Enemy,
+		Item,
+		EventItem,
+		Story
+	};
+	public enum MessageFormat
+	{
+		None,
+		State,
+		Event
+	};
+};
