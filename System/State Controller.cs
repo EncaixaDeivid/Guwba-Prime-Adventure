2ae5b7b9d25b768cd443193747870c8c05a204a9
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 12:12:39 -0300

    Update State Controller.cs

--- System/State Controller.cs
+++ System/State Controller.cs
@@ -12,22 +12,22 @@ namespace GwambaPrimeAdventure
 		protected sealed class WaitTime : CustomYieldInstruction
 		{
 			private readonly StateController _instance;
-			private readonly bool _isScaled;
+			private readonly bool _unscaled;
 			private float _time;
 			public override bool keepWaiting
 			{
 				get
 				{
 					if (_time > 0f && _instance.isActiveAndEnabled)
-						_time -= Time.deltaTime / (_isScaled ? Time.timeScale : 1f);
+						_time -= Time.deltaTime / (_unscaled ? Time.timeScale : 1f);
 					return _time > 0f;
 				}
 			}
-			public WaitTime(StateController instance, float time, bool isScaled = false)
+			public WaitTime(StateController instance, float time, bool unscaled = false)
 			{
 				_instance = instance;
 				_time = time;
-				_isScaled = isScaled;
+				_unscaled = unscaled;
 			}
 		};
 	};
