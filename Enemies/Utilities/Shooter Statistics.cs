395f84b342edc915a8aab62864b502fd84ec70f7
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 14:02:11 -0300

    Update Shooter Statistics.cs

--- Enemies/Utilities/Shooter Statistics.cs
+++ Enemies/Utilities/Shooter Statistics.cs
@@ -5,17 +5,17 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 	[CreateAssetMenu(fileName = "Shooter Enemy", menuName = "Enemy Statistics/Shooter", order = 5)]
 	public sealed class ShooterStatistics : ScriptableObject
 	{
-		[field: SerializeField, Tooltip("The projectiles that this enemy can instantiate."), Header("Shooter Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
+		[field: SerializeField, Tooltip("The projectiles that this enemy can instantiate."), Header("Shooter Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F, order = 1)]
 		public Projectile[] Projectiles { get; private set; }
 		[field: SerializeField, Tooltip("The point to where spawn the projectile relative to this enemy.")] public Vector2 SpawnPoint { get; private set; }
 		[field: SerializeField, Tooltip("Will shoot to infinity without any detection.")] public bool ShootInfinity { get; private set; }
-		[field: SerializeField, HideIf(nameof(ShootInfinity)), Min(0f), Tooltip("The distance this enemy can detect the target.")] public float PerceptionDistance { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), Min(0F), Tooltip("The distance this enemy can detect the target.")] public float PerceptionDistance { get; private set; }
 		[field: SerializeField, HideIf(nameof(ShootInfinity)), Tooltip("If the detection will be circular.")] public bool CircularDetection { get; private set; }
 		[field: SerializeField, HideIf(EConditionOperator.Or ,nameof(ShootInfinity), nameof(CircularDetection)), Tooltip("The angle fo the direction of ray of the detection.")]
 		public float RayAngleDirection { get; private set; }
-		[field: SerializeField, HideIf(nameof(ShootInfinity)), Min(0f), Tooltip("The amount of time to wait to execute another shoot.")] public float IntervalToShoot { get; private set; }
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), Min(0F), Tooltip("The amount of time to wait to execute another shoot.")] public float IntervalToShoot { get; private set; }
 		[field: SerializeField, HideIf(nameof(ShootInfinity)), Tooltip("If this enemy will stop moving when shoot.\nRequires: Moving Enemy.")] public bool Stop { get; private set; }
-		[field: SerializeField, HideIf(nameof(ShootInfinity)), ShowIf(nameof(Stop)), Min(1e-3f), Tooltip("The amount of time to stop this enemy to move.\nRequires: Moving Enemy.")]
+		[field: SerializeField, HideIf(nameof(ShootInfinity)), ShowIf(nameof(Stop)), Min(WorldBuild.MINIMUM_TIME_SPACE_LIMIT), Tooltip("The amount of time to stop this enemy to move.\nRequires: Moving Enemy.")]
 		public float StopTime { get; private set; }
 		[field: SerializeField, HideIf(nameof(ShootInfinity)), Tooltip("If this enemy will turn the ray to the looking side.")] public bool TurnRay { get; private set; }
 		[field: SerializeField, HideIf(nameof(ShootInfinity)), Tooltip("If this enemy will become invencible while shooting.\nRequires: Defender Enemy.")]
