46d176267f9ca9d7074970fa9f677c1c8f8afeff
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 14:00:11 -0300

    Update Death Statistics.cs

--- Enemies/Utilities/Death Statistics.cs
+++ Enemies/Utilities/Death Statistics.cs
@@ -4,7 +4,7 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 	[CreateAssetMenu(fileName = "Death Enemy", menuName = "Enemy Statistics/Death", order = 10)]
 	public sealed class DeathStatistics : ScriptableObject
 	{
-		[field: SerializeField, Tooltip("The enemy that this enemy will spawn in death."), Header("Death Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
+		[field: SerializeField, Tooltip("The enemy that this enemy will spawn in death."), Header("Death Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F, order = 1)]
 		public Control ChildEnemy { get; private set; }
 		[field: SerializeField, Tooltip("The projectile that this enemy will spawn in death.")] public Projectile ChildProjectile { get; private set; }
 		[field: SerializeField, Tooltip("The point to where spawn the object on death relative to this enemy.")] public Vector2 SpawnPoint { get; private set; }
