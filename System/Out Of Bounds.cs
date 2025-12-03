e25511edce746f1eed5c8cce90d13ca30f13e3c4
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-02 23:29:10 -0300

    Add files via upload

---
+++ System/Out Of Bounds.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+namespace GwambaPrimeAdventure
+{
+	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
+    internal sealed class OutOfBounds : MonoBehaviour
+    {
+		private void OnTriggerEnter2D(Collider2D collision) => Destroy(collision.gameObject);
+	};
+};
---
+++ System/Surface Sound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+namespace GwambaPrimeAdventure
+{
+	[CreateAssetMenu(fileName = "Surface Sound", menuName = "Surface/Sound", order = 0)]
+	internal class SurfaceSound : ScriptableObject
+   {
+		[field: SerializeField] internal TilesSound[] TilesSounds { get; private set; }
+   };
+	[Serializable]
+	internal struct TilesSound
+	{
+		[field: SerializeField] internal Tile[] Tiles { get; private set; }
+		[field: SerializeField] internal AudioSource Source { get; private set; }
+	};
+};
