63819245623e75e68930cde4d54998b6bf8c8af8
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 13:37:49 -0300

    Update Gwamba Damager.cs

--- Characters/Gwamba Damager.cs
+++ Characters/Gwamba Damager.cs
@@ -13,20 +13,16 @@ namespace GwambaPrimeAdventure.Character
 		private UnityAction<ushort, float> _damagerStun;
 		private UnityAction<GwambaDamager, IDestructible> _damagerAttack;
 		private readonly List<IDestructible> _damagerDamaged = new();
-		[SerializeField, BoxGroup("Statistics"), Tooltip("If this Gwamba's part will take damage."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f)] private bool _takeDamage;
-		[field: SerializeField, BoxGroup("Statistics"), HideIf(nameof(_takeDamage)), Tooltip("The velocity of the screen shake on the attack.")]
-		internal Vector2 AttackShake { get; private set; }
-		[field: SerializeField, BoxGroup("Statistics"), HideIf(nameof(_takeDamage)), Tooltip("The amount of damage that the attack of Gwamba hits.")]
-		internal ushort AttackDamage { get; private set; }
-		[field: SerializeField, BoxGroup("Statistics"), HideIf(nameof(_takeDamage)), Min(0f), Tooltip("The amount of time the attack screen shake will be applied.")]
-		internal float AttackShakeTime { get; private set; }
-		[field: SerializeField, BoxGroup("Statistics"), HideIf(nameof(_takeDamage)), Min(0f), Tooltip("The amount of time that this Gwamba's attack stun does.")]
-		internal float StunTime { get; private set; }
+		[SerializeField, BoxGroup("Statistics"), Tooltip("If this Gwamba's part will take damage."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F)] private bool _takeDamage;
+		[field: SerializeField, BoxGroup("Statistics"), HideIf(nameof(_takeDamage)), Tooltip("The velocity of the screen shake on the attack.")] internal Vector2 AttackShake { get; private set; }
+		[field: SerializeField, BoxGroup("Statistics"), HideIf(nameof(_takeDamage)), Tooltip("The amount of damage that the attack of Gwamba hits.")] internal ushort AttackDamage { get; private set; }
+		[field: SerializeField, BoxGroup("Statistics"), HideIf(nameof(_takeDamage)), Min(0F), Tooltip("The amount of time the attack screen shake will be applied.")] internal float AttackShakeTime { get; private set; }
+		[field: SerializeField, BoxGroup("Statistics"), HideIf(nameof(_takeDamage)), Min(0F), Tooltip("The amount of time that this Gwamba's attack stun does.")] internal float StunTime { get; private set; }
 		internal Predicate<ushort> DamagerHurt { get => null; set => _damagerHurt = value; }
 		internal UnityAction<ushort, float> DamagerStun { get => null; set => _damagerStun = value; }
 		internal UnityAction<GwambaDamager, IDestructible> DamagerAttack { get => null; set => _damagerAttack = value; }
 		internal List<IDestructible> DamagerDamaged => _damagerDamaged;
-		internal float Alpha { get => _spriteRenderer.color.a; set => _spriteRenderer.color = new(1f, 1f, 1f, value); }
+		internal float Alpha { get => _spriteRenderer.color.a; set => _spriteRenderer.color = new(1F, 1F, 1F, value); }
 		public short Health => 0;
 		private new void Awake()
 		{
