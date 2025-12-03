dd876da4bc4a3c5e61eabb181db2574ad91ff198
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 14:02:50 -0300

    Update Teleporter Statistics.cs

--- Enemies/Utilities/Teleporter Statistics.cs
+++ Enemies/Utilities/Teleporter Statistics.cs
@@ -5,7 +5,7 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 	[CreateAssetMenu(fileName = "Teleporter Enemy", menuName = "Enemy Statistics/Teleporter", order = 9)]
 	public sealed class TeleporterStatistics : ScriptableObject
 	{
-		[field: SerializeField, Tooltip("The collection of the summon places."), Header("Teleporter Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
+		[field: SerializeField, Tooltip("The collection of the summon places."), Header("Teleporter Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F, order = 1)]
 		public TeleportPointStructure[] TeleportPointStructures { get; private set; }
 		[field: SerializeField, Tooltip("The amount of time to use the teleport again.")] public float TimeToUse { get; private set; }
 	};
