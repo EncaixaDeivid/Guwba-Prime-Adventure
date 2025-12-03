91c70c87551bc602dae822ac4033e6ce914c68cc
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 12:12:59 -0300

    Update World Build.cs

--- System/World Build.cs
+++ System/World Build.cs
@@ -8,6 +8,7 @@ namespace GwambaPrimeAdventure
 		public const ushort LEVELS_COUNT = 10;
 		public const float FIELD_SPACE_LENGTH = 8F;
 		public const float SNAP_LENGTH = 1F / 16F;
+		public const float DESTROY_COPY_TIME = 1E-3F;
 		public enum Layers
 		{
 			System,
@@ -23,7 +24,6 @@ namespace GwambaPrimeAdventure
 		public static readonly LayerMask ItemMask = GetMask(Layers.Item);
 		public static readonly LayerMask EnemyMask = GetMask(Layers.Enemy);
 		public static readonly LayerMask BossMask = GetMask(Layers.Boss);
-		private static float _localScaleX;
 		private static LayerMask GetMask(Layers layerName)
 		{
 			for (int i = 0; i < 32; i++)
@@ -31,15 +31,7 @@ namespace GwambaPrimeAdventure
 					return 1 << i;
 			return 0;
 		}
-		public static void TurnScaleX(this Transform transform, float valueChanger)
-		{
-			_localScaleX = Mathf.Abs(transform.localScale.x) * valueChanger;
-			transform.localScale = new Vector3(_localScaleX, transform.localScale.y, transform.localScale.z);
-		}
-		public static void TurnScaleX(this Transform transform, bool conditionChanger)
-		{
-			_localScaleX = Mathf.Abs(transform.localScale.x) * (conditionChanger ? -1f : 1f);
-			transform.localScale = new Vector3(_localScaleX, transform.localScale.y, transform.localScale.z);
-		}
+		public static void TurnScaleX(this Transform transform, float valueChanger) => transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * valueChanger, transform.localScale.y, transform.localScale.z);
+		public static void TurnScaleX(this Transform transform, bool conditionChanger) => TurnScaleX(transform, conditionChanger ? -1F : 1F);
 	};
 };
