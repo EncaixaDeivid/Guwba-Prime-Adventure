a3b240ab031159b44af69f7c9515a68e6b4f0fa0
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-02 23:30:42 -0300

    Update Surface.cs

--- System/Surface.cs
+++ System/Surface.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using System.Collections;
+using System.Linq;
 namespace GwambaPrimeAdventure
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
+	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Tilemap), typeof(TilemapRenderer))]
+	[RequireComponent(typeof(TilemapCollider2D))]
 	public sealed class Surface : MonoBehaviour, ILoader
 	{
-		[SerializeField] private bool _isScene;
-		public bool IsScene => _isScene;
+		private Tilemap _tilemap;
+		private void Awake() => _tilemap = GetComponent<Tilemap>();
 		public IEnumerator Load() { yield return null; }
+		internal bool CheckForTile(Tile[] tiles, Vector2 originPosition) => tiles.Contains(_tilemap.GetTile(_tilemap.WorldToCell(originPosition)));
 	};
 };
