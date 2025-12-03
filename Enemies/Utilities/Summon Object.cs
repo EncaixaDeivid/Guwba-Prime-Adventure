0ccd8e8c386a9ae7447a2905406037601c20bbb9
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 14:02:27 -0300

    Update Summon Object.cs

--- Enemies/Utilities/Summon Object.cs
+++ Enemies/Utilities/Summon Object.cs
@@ -5,7 +5,7 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 	[CreateAssetMenu(fileName = "Enemy Summon", menuName = "Enemy Statistics/Summon", order = 7)]
 	public sealed class SummonObject : ScriptableObject
 	{
-		[field: SerializeField, Tooltip("The enemy that will be instantiate."), Header("Components Statistics", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2f, order = 1)]
+		[field: SerializeField, Tooltip("The enemy that will be instantiate."), Header("Components Statistics", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F, order = 1)]
 		public GameObject[] Summons { get; private set; }
 		[field: SerializeField, Tooltip("The points that the instance can be instantiate.")] public Vector2[] SummonPoints { get; private set; }
 		[field: SerializeField, Tooltip("If the points to summon are relative to it's parent.")] public bool LocalPoints { get; private set; }
@@ -17,7 +17,7 @@ namespace GwambaPrimeAdventure.Enemy.Utility
 		[field: SerializeField, Tooltip("If the instantiator will stop during the summon.")] public bool StopToSummon { get; private set; }
 		[field: SerializeField, ShowIf(nameof(StopToSummon)), Tooltip("If the instantiator will paralyze during the summon.")] public bool ParalyzeToSummon { get; private set; }
 		[field: SerializeField, ShowIf(nameof(StopToSummon)), Tooltip("If the timed summon will stop permanently.")] public bool StopPermanently { get; private set; }
-		[field: SerializeField, ShowIf(nameof(StopToSummon)), HideIf(nameof(StopPermanently)), Min(0f), Tooltip("The amount of time to stop the instantiator.")]
+		[field: SerializeField, ShowIf(nameof(StopToSummon)), HideIf(nameof(StopPermanently)), Min(0F), Tooltip("The amount of time to stop the instantiator.")]
 		public float TimeToStop { get; private set; }
 		[field: SerializeField, ShowIf(nameof(StopToSummon)), Tooltip("If the instanciation will be instantly after stop.")] public bool InstantlySummon { get; private set; }
 		[field: SerializeField, HideIf(nameof(InstantlySummon)), ShowIf(nameof(StopToSummon)), Tooltip("If the instanciation will wait return from the stop to summon.")]
