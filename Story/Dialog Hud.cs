e22e78e70ece66d19f7de2285e72a826a9a5a86e
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 19:30:19 -0300

    Update Dialog Hud.cs

--- Story/Dialog Hud.cs
+++ Story/Dialog Hud.cs
@@ -20,7 +20,7 @@ namespace GwambaPrimeAdventure.Story
 		{
 			if (_instance)
 			{
-				Destroy(gameObject, 1e-3f);
+				Destroy(gameObject, WorldBuild.MINIMUM_TIME_SPACE_LIMIT);
 				return;
 			}
 			_instance = this;
