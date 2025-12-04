d845997d6291fde805f8c7e420e567aca58fd733
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 21:07:40 -0300

    Update Occlusion Object.cs

--- System/Occlusion Object.cs
+++ System/Occlusion Object.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 namespace GwambaPrimeAdventure
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(BoxCollider2D))]
 	public sealed class OcclusionObject : MonoBehaviour, IConnector
 	{
+		private readonly List<Transform> _children = new();
 		[Header("Interactions")]
 		[SerializeField, Tooltip("If this object will activate the children.")] private bool _initialActive;
 		[SerializeField, Tooltip("If this object will turn off the collisions.")] private bool _offCollision;
@@ -19,21 +21,22 @@ namespace GwambaPrimeAdventure
 		private IEnumerator Start()
 		{
 			yield return new WaitWhile(() => SceneInitiator.IsInTrancision());
-			StateController[] children = GetComponentsInChildren<StateController>();
+			GetComponentsInChildren<Transform>(_children);
+			StateController[] states = GetComponentsInChildren<StateController>();
 			yield return new WaitUntil(() =>
 			{
-				for (ushort i = 0; i < children.Length; i++)
-					if (!children[i].enabled)
+				for (ushort i = 0; i < states.Length; i++)
+					if (!states[i])
 						return false;
 				return true;
 			});
 			for (ushort i = 0; i < transform.childCount; i++)
-				transform.GetChild(i).gameObject.SetActive(_initialActive);
+				_children[i].gameObject.SetActive(_initialActive);
 		}
 		internal void Execution(bool activate)
 		{
 			for (ushort i = 0; i < transform.childCount; i++)
-				transform.GetChild(i).gameObject.SetActive(activate);
+				_children[i].gameObject.SetActive(activate);
 		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
@@ -44,7 +47,7 @@ namespace GwambaPrimeAdventure
 		{
 			if (this == message.AdditionalData as OcclusionObject && message.Format == MessageFormat.State && message.ToggleValue.HasValue)
 				for (ushort i = 0; i < transform.childCount; i++)
-					transform.GetChild(i).gameObject.SetActive(message.ToggleValue.Value);
+					_children[i].gameObject.SetActive(message.ToggleValue.Value);
 		}
 	};
 };
