152b64aebe6722fac7e26c833f1285eb6cd86176
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 22:48:02 -0300

    Update Surface Sound.cs

--- System/Surface Sound.cs
+++ System/Surface Sound.cs
@@ -5,7 +5,7 @@ namespace GwambaPrimeAdventure
 	[CreateAssetMenu(fileName = "Surface Sound", menuName = "Surface/Sound", order = 0)]
 	public class SurfaceSound : ScriptableObject
    {
-		[field: SerializeField, Tooltip("The tiles that contain the sound clip specified.")] public Tile[] Tiles { get; private set; }
+		[field: SerializeField, Tooltip("The tiles that contain the sound clip specified."), Header("Surfaces"), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F)] public Tile[] Tiles { get; private set; }
 		[field: SerializeField, Tooltip("The sound clip that the tiles contains to be played.")] public AudioClip Clip { get; private set; }
 	};
 };
