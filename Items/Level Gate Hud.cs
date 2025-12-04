5361f046766950a7e9c7f6ca1efe417a4310bab8
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 22:05:32 -0300

    Update Level Gate Hud.cs

--- Items/Level Gate Hud.cs
+++ Items/Level Gate Hud.cs
@@ -5,19 +5,15 @@ namespace GwambaPrimeAdventure.Item
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(UIDocument))]
 	internal sealed class LevelGateHud : MonoBehaviour
 	{
-		[Header("Elements")]
-		[SerializeField, Tooltip("User interface element.")] private string _levelButton;
-		[SerializeField, Tooltip("User interface element.")] private string _bossButton;
-		[SerializeField, Tooltip("User interface element.")] private string _scenesButton;
 		internal Button Level { get; private set; }
 		internal Button Boss { get; private set; }
 		internal Button Scenes { get; private set; }
 		private void Awake()
 		{
 			VisualElement root = GetComponent<UIDocument>().rootVisualElement;
-			Level = root.Q<Button>(_levelButton);
-			Boss = root.Q<Button>(_bossButton);
-			Scenes = root.Q<Button>(_scenesButton);
+			Level = root.Q<Button>(nameof(Level));
+			Boss = root.Q<Button>(nameof(Boss));
+			Scenes = root.Q<Button>(nameof(Scenes));
 		}
 	};
 };
