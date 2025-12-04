d5a16fedffcda59c308bdb829a89a634e4b3dd7e
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-03 23:48:13 -0300

    Update Occlusion Object.cs

--- System/Occlusion Object.cs
+++ System/Occlusion Object.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 namespace GwambaPrimeAdventure
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(BoxCollider2D))]
 	public sealed class OcclusionObject : MonoBehaviour, IConnector
 	{
-		private readonly List<Transform> _children = new();
 		[Header("Interactions")]
 		[SerializeField, Tooltip("If this object will activate the children.")] private bool _initialActive;
 		[SerializeField, Tooltip("If this object will turn off the collisions.")] private bool _offCollision;
-		[SerializeField, Tooltip("If this object will occlude any other object that enter the collision.")] private bool _collisionOcclusion;
+		[SerializeField, Tooltip("If this object will occlude any other object that enter the collision.")] private bool _collisionOcclusion = true;
 		public MessagePath Path => MessagePath.System;
 		private void Awake()
 		{
@@ -21,22 +19,21 @@ namespace GwambaPrimeAdventure
 		private IEnumerator Start()
 		{
 			yield return new WaitWhile(() => SceneInitiator.IsInTrancision());
-			GetComponentsInChildren<Transform>(_children);
-			StateController[] states = GetComponentsInChildren<StateController>();
+			StateController[] states = GetComponentsInChildren<StateController>(true);
 			yield return new WaitUntil(() =>
 			{
 				for (ushort i = 0; i < states.Length; i++)
-					if (!states[i])
+					if (states[i] && !states[i].enabled)
 						return false;
 				return true;
 			});
 			for (ushort i = 0; i < transform.childCount; i++)
-				_children[i].gameObject.SetActive(_initialActive);
+				transform.GetChild(i).gameObject.SetActive(_initialActive);
 		}
 		internal void Execution(bool activate)
 		{
 			for (ushort i = 0; i < transform.childCount; i++)
-				_children[i].gameObject.SetActive(activate);
+				transform.GetChild(i).gameObject.SetActive(activate);
 		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
@@ -47,7 +44,7 @@ namespace GwambaPrimeAdventure
 		{
 			if (this == message.AdditionalData as OcclusionObject && message.Format == MessageFormat.State && message.ToggleValue.HasValue)
 				for (ushort i = 0; i < transform.childCount; i++)
-					_children[i].gameObject.SetActive(message.ToggleValue.Value);
+					transform.GetChild(i).gameObject.SetActive(message.ToggleValue.Value);
 		}
 	};
 };
