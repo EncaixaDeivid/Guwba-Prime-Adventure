bf1f61907e42c73bd9dc1fef54e5b09c1737f79b
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-05 14:20:30 -0300

    Update World Build.cs

--- System/World Build.cs
+++ System/World Build.cs
@@ -7,16 +7,16 @@ namespace GwambaPrimeAdventure
 		public const float FIELD_SPACE_LENGTH = 8F;
 		public const float SNAP_LENGTH = 1F / 16F;
 		public const float MINIMUM_TIME_SPACE_LIMIT = 1E-4F;
+		public const int SYSTEM_MASK = 1 << 3;
+		public const int UI_MASK = 1 << 5;
+		public const int CHARACTER_MASK = 1 << 6;
+		public const int SCENE_MASK = 1 << 7;
+		public const int ITEM_MASK = 1 << 8;
+		public const int ENEMY_MASK = 1 << 9;
+		public const int BOSS_MASK = 1 << 10;
 		public const ushort PIXEL_PERFECT_WIDTH = 320;
 		public const ushort PIXEL_PERFECT_HEIGHT = 180;
 		public const ushort LEVELS_COUNT = 10;
-		public static readonly LayerMask SystemMask = 1 << 3;
-		public static readonly LayerMask UIMask = 1 << 5;
-		public static readonly LayerMask CharacterMask = 1 << 6;
-		public static readonly LayerMask SceneMask = 1 << 7;
-		public static readonly LayerMask ItemMask = 1 << 8;
-		public static readonly LayerMask EnemyMask = 1 << 9;
-		public static readonly LayerMask BossMask = 1 << 10;
 		public static Resolution[] PixelPerfectResolutions()
 		{
 			List<Resolution> resolutions = new();
