9601f12316b006f9d943fbc2d786b90c844e1df0
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-05 10:18:33 -0300

    Update World Build.cs

--- System/World Build.cs
+++ System/World Build.cs
@@ -4,34 +4,19 @@ namespace GwambaPrimeAdventure
 {
 	public static class WorldBuild
 	{
-		public const ushort PIXEL_PERFECT_WIDTH = 320;
-		public const ushort PIXEL_PERFECT_HEIGHT = 180;
-		public const ushort LEVELS_COUNT = 10;
 		public const float FIELD_SPACE_LENGTH = 8F;
 		public const float SNAP_LENGTH = 1F / 16F;
 		public const float MINIMUM_TIME_SPACE_LIMIT = 1E-4F;
-		public enum Layers
-		{
-			System,
-			Character,
-			Scene,
-			Item,
-			Enemy,
-			Boss
-		};
-		public static readonly LayerMask SystemMask = GetMask(Layers.System);
-		public static readonly LayerMask CharacterMask = GetMask(Layers.Character);
-		public static readonly LayerMask SceneMask = GetMask(Layers.Scene);
-		public static readonly LayerMask ItemMask = GetMask(Layers.Item);
-		public static readonly LayerMask EnemyMask = GetMask(Layers.Enemy);
-		public static readonly LayerMask BossMask = GetMask(Layers.Boss);
-		private static LayerMask GetMask(Layers layerName)
-		{
-			for (int i = 0; i < 32; i++)
-				if (LayerMask.LayerToName(i) != string.Empty && LayerMask.LayerToName(i).ToUpper() == layerName.ToString().ToUpper())
-					return 1 << i;
-			return 0;
-		}
+		public const ushort PIXEL_PERFECT_WIDTH = 320;
+		public const ushort PIXEL_PERFECT_HEIGHT = 180;
+		public const ushort LEVELS_COUNT = 10;
+		public static readonly LayerMask SystemMask = 1 << 3;
+		public static readonly LayerMask UIMask = 1 << 5;
+		public static readonly LayerMask CharacterMask = 1 << 6;
+		public static readonly LayerMask SceneMask = 1 << 7;
+		public static readonly LayerMask ItemMask = 1 << 8;
+		public static readonly LayerMask EnemyMask = 1 << 9;
+		public static readonly LayerMask BossMask = 1 << 10;
 		public static Resolution[] PixelPerfectResolutions()
 		{
 			List<Resolution> resolutions = new();
@@ -41,7 +26,10 @@ namespace GwambaPrimeAdventure
 			return resolutions.ToArray();
 		}
 		public static void TurnScaleX(this Transform transform, float valueChanger)
-			=> transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * valueChanger, transform.localScale.y, transform.localScale.z);
+		{
+			float scaleX = Mathf.Abs(transform.localScale.x) * valueChanger;
+			transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+		}
 		public static void TurnScaleX(this Transform transform, bool conditionChanger) => TurnScaleX(transform, conditionChanger ? -1F : 1F);
 	};
 };
