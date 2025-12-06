4e1cec7cd23df88d169305c827f7e0303b2a86ea
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-05 23:11:10 -0300

    Update Progress Obstacle.cs

--- Items/Progress Obstacle.cs
+++ Items/Progress Obstacle.cs
@@ -4,15 +4,14 @@ using GwambaPrimeAdventure.Connection;
 namespace GwambaPrimeAdventure.Item
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
-	internal sealed class ProgressObstacle : StateController
+	internal sealed class ProgressObstacle : StateController, ILoader
 	{
 		[Header("Progress Interactions")]
 		[SerializeField, Tooltip("The index that this object will check if theres anything completed.")] private ushort _progressIndex;
 		[SerializeField, Tooltip("If the index is about the boss.")] private bool _isBossProgress;
 		[SerializeField, Tooltip("If this object will be saved as already existent object.")] private bool _saveOnSpecifics;
-		private IEnumerator Start()
+		public IEnumerator Load()
 		{
-			yield return new WaitWhile(() => SceneInitiator.IsInTrancision());
 			SaveController.Load(out SaveFile saveFile);
 			if (_isBossProgress ? saveFile.DeafetedBosses[_progressIndex - 1] : saveFile.LevelsCompleted[_progressIndex - 1])
 			{
@@ -23,6 +22,7 @@ namespace GwambaPrimeAdventure.Item
 				}
 				Destroy(gameObject);
 			}
+			yield return null;
 		}
 	};
 };
