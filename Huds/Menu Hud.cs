27a64cbb9982c3f4c7169a2df9164e7f1349f822
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-06 10:01:28 -0300

    Update Menu Hud.cs

--- Huds/Menu Hud.cs
+++ Huds/Menu Hud.cs
@@ -13,6 +13,7 @@ namespace GwambaPrimeAdventure.Hud
 		internal Button Quit { get; private set; }
 		internal Button Back { get; private set; }
 		internal TextField[] SaveName { get; private set; }
+		internal Button[] RenameFile { get; private set; }
 		internal Button[] Load { get; private set; }
 		internal Button[] Delete { get; private set; }
 		private void Awake()
@@ -32,13 +33,16 @@ namespace GwambaPrimeAdventure.Hud
 			Back = root.Q<Button>(nameof(Back));
 			SaveName = new TextField[4];
 			for (ushort i = 0; i < SaveName.Length; i++)
-				SaveName[i] = root.Q<TextField>(nameof(SaveName) + $"{i + 1}");
+				SaveName[i] = root.Q<TextField>($"{nameof(SaveName)}{i + 1}");
+			RenameFile = new Button[4];
+			for (ushort i = 0; i < RenameFile.Length; i++)
+				RenameFile[i] = root.Q<Button>($"{nameof(RenameFile)}{i + 1}");
 			Load = new Button[4];
 			for (ushort i = 0; i < Load.Length; i++)
-				Load[i] = root.Q<Button>(nameof(Load) + $"{i + 1}");
+				Load[i] = root.Q<Button>($"{nameof(Load)}{i + 1}");
 			Delete = new Button[4];
 			for (ushort i = 0; i < Delete.Length; i++)
-				Delete[i] = root.Q<Button>(nameof(Delete) + $"{i + 1}");
+				Delete[i] = root.Q<Button>($"{nameof(Delete)}{i + 1}");
 		}
 	};
 };
