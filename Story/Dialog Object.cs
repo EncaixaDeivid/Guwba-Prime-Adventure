0b06a2ff415c95e7e261791287d69d50a75f93fa
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-06 14:05:28 -0300

    Update Dialog Object.cs

--- Story/Dialog Object.cs
+++ Story/Dialog Object.cs
@@ -9,18 +9,18 @@ namespace GwambaPrimeAdventure.Story
 		[field: SerializeField, Tooltip("The collection of the object that contais the speach."), Header("Dialog Components"), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F)] internal Speach[] Speachs { get; private set; }
 		[field: SerializeField, Tooltip("If the interaction have to be desactivated after the speach.")] internal bool DesactiveInteraction { get; private set; }
 		[field: SerializeField, Tooltip("If the object will destruct.")] internal bool EndDestroy { get; private set; }
-		[field: SerializeField, HideIf(nameof(EndDestroy)), Tooltip("The amount of time to wait to destroy after the speach.")] internal float TimeToDestroy { get; private set; }
+		[field: SerializeField, ShowIf(nameof(EndDestroy)), Tooltip("The amount of time to wait to destroy after the speach.")] internal float TimeToDestroy { get; private set; }
 		[field: SerializeField, Tooltip("If the trancision have to be activated after the speach.")] internal bool Transition { get; private set; }
-		[field: SerializeField, HideIf(nameof(Transition)), Tooltip("The scene to trancision to after the speach.")] internal SceneField SceneToTransition { get; private set; }
+		[field: SerializeField, ShowIf(nameof(Transition)), Tooltip("The scene to trancision to after the speach.")] internal SceneField SceneToTransition { get; private set; }
 		[field: SerializeField, Tooltip("If the animation will play.")] internal bool ActivateAnimation { get; private set; }
-		[field: SerializeField, HideIf(nameof(ActivateAnimation)), Tooltip("The animation to play after the speach.")] internal string Animation { get; private set; }
+		[field: SerializeField, ShowIf(nameof(ActivateAnimation)), Tooltip("The animation to play after the speach.")] internal string Animation { get; private set; }
 		[field: SerializeField, Tooltip("If this object will be saved as already existent object.")] internal bool SaveOnEspecific { get; private set; }
 		[Serializable]
 		internal struct Speach
 		{
 			[field: SerializeField, Tooltip("The image icon of the character that is speaking."), Header("Speach Components"), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F)] internal Sprite Model { get; private set; }
 			[field: SerializeField, Tooltip("The name of the character that is speaking.")] internal string CharacterName { get; private set; }
-			[field: SerializeField, TextArea(1, 12), Tooltip("The speach of the character that is speaking.")] internal string SpeachText { get; private set; }
+			[field: SerializeField, TextArea(0, 12), Tooltip("The speach of the character that is speaking.")] internal string SpeachText { get; private set; }
 			[field: SerializeField, Tooltip("If after the speach the next slide of story scene have to come.")] internal bool NextSlide { get; private set; }
 		};
 	};
