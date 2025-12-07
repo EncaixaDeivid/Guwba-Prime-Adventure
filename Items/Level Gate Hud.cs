5b4579fcba5bfb800210924775d33fc6e17dd019
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-07 09:42:32 -0300

    Update Level Gate Hud.cs

--- Items/Level Gate Hud.cs
+++ Items/Level Gate Hud.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 using UnityEngine.UIElements;
 namespace GwambaPrimeAdventure.Item
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(UIDocument))]
+	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(MeshRenderer), typeof(UIDocument))]
 	internal sealed class LevelGateHud : MonoBehaviour
 	{
+		[SerializeField, Tooltip("The material used to show the UI.")] private Material _uiMaterial;
 		internal Button Level { get; private set; }
 		internal Button Boss { get; private set; }
 		internal Button Scenes { get; private set; }
@@ -14,6 +15,8 @@ namespace GwambaPrimeAdventure.Item
 			Level = root.Q<Button>(nameof(Level));
 			Boss = root.Q<Button>(nameof(Boss));
 			Scenes = root.Q<Button>(nameof(Scenes));
+			GetComponent<MeshRenderer>().material = _uiMaterial;
+			transform.localScale = new Vector3(_uiMaterial.mainTexture.width / WorldBuild.PIXELS_PER_UNIT, _uiMaterial.mainTexture.height / WorldBuild.PIXELS_PER_UNIT);
 		}
 	};
 };
