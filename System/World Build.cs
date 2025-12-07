5eff92e6c41240789f6a8b3e4c784e76ee05051a
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-07 09:43:10 -0300

    Update World Build.cs

--- System/World Build.cs
+++ System/World Build.cs
@@ -5,7 +5,8 @@ namespace GwambaPrimeAdventure
 	public static class WorldBuild
 	{
 		public const float FIELD_SPACE_LENGTH = 8F;
-		public const float SNAP_LENGTH = 1F / 16F;
+		public const float PIXELS_PER_UNIT = 16F;
+		public const float SNAP_LENGTH = 1F / PIXELS_PER_UNIT;
 		public const float MINIMUM_TIME_SPACE_LIMIT = 1E-4F;
 		public const int SYSTEM_LAYER = 1 << 3;
 		public const int UI_LAYER = 1 << 5;
