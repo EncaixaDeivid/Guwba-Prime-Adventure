331a654aacd027e0c2b1c681331c6a5f67b8df30
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-08 01:17:18 -0300

    Update Scene Initiator.cs

--- System/Scene Initiator.cs
+++ System/Scene Initiator.cs
@@ -8,7 +8,7 @@ namespace GwambaPrimeAdventure
 		private static SceneInitiator _instance;
 		[SerializeField, Tooltip("The object that handles the hud of the trancision.")] private TransicionHud _transicionHud;
 		[SerializeField, Tooltip("The objects to be lodaed.")] private ObjectLoader[] _objectLoaders;
-		public static bool IsInTrancision() => _instance;
+		internal static ushort ProgressIndex = 0;
 		private void Awake()
 		{
 			if (_instance)
@@ -23,11 +23,12 @@ namespace GwambaPrimeAdventure
 			if (!_instance || _instance != this)
 				yield break;
 			TransicionHud transicionHud = Instantiate(_transicionHud, transform);
-			(transicionHud.RootElement.style.opacity, transicionHud.LoadingBar.highValue) = (1F, _objectLoaders.Length);
+			(transicionHud.RootElement.style.opacity, transicionHud.LoadingBar.highValue, ProgressIndex) = (1F, _objectLoaders.Length, 0);
 			for (ushort i = 0; i < _objectLoaders.Length; i++)
 				yield return StartCoroutine(Instantiate(_objectLoaders[i]).Load(transicionHud.LoadingBar));
 			Destroy(gameObject);
 			StateController.SetState(true);
 		}
+		public static bool IsInTrancision() => _instance;
 	};
 };
