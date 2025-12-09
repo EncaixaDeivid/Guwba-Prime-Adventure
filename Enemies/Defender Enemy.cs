ec71883a80d57501d27e4bf523090f83040b4a73
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-09 12:00:11 -0300

    Update Defender Enemy.cs

--- Enemies/Defender Enemy.cs
+++ Enemies/Defender Enemy.cs
@@ -71,7 +71,7 @@ namespace GwambaPrimeAdventure.Enemy
 		}
 		public void Receive(MessageData message)
 		{
-			if (message.AdditionalData != null && message.AdditionalData is EnemyProvider[] && (message.AdditionalData as EnemyProvider[]).Length > 0)
+			if (message.AdditionalData is not null && message.AdditionalData is EnemyProvider[] && (message.AdditionalData as EnemyProvider[]).Length > 0)
 				foreach (EnemyProvider enemy in message.AdditionalData as EnemyProvider[])
 					if (enemy && enemy == this)
 					{
