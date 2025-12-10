c50bcb484820a415969944c2907e1ab340a3ba43
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-10 02:05:21 -0300

    Update Transitioner.cs

--- System/Connection/Transitioner.cs
+++ System/Connection/Transitioner.cs
@@ -6,26 +6,24 @@ namespace GwambaPrimeAdventure.Connection
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform))]
 	public sealed class Transitioner : MonoBehaviour
 	{
-		private static bool _block = false;
 		[Header("Scene Interaction")]
 		[SerializeField, Tooltip("The object that handles the hud of the trancision.")] private TransicionHud _transicionHud;
 		[SerializeField, Tooltip("The scene that will be trancisionate to.")] private SceneField _sceneTransicion;
 		[SerializeField, Tooltip("The scene of the menu.")] private SceneField _menuScene;
 		public void Transicion(SceneField scene = null)
 		{
-			if (_block)
+			if (TransicionHud.Exists())
 				return;
 			StartCoroutine(SceneTransicion());
 			IEnumerator SceneTransicion()
 			{
-				_block = true;
-				SaveController.Load(out SaveFile saveFile);
 				StateController.SetState(false);
 				TransicionHud transicionHud = Instantiate(_transicionHud);
 				for (float i = 0F; 1F > transicionHud.RootElement.style.opacity.value; i += 1E-1F)
 					yield return transicionHud.RootElement.style.opacity = i;
 				SceneField newScene = scene ?? _sceneTransicion;
-				if (gameObject.scene.name != newScene)
+				SaveController.Load(out SaveFile saveFile);
+				if (SceneManager.GetActiveScene().name != newScene)
 					if (newScene.SceneName.Contains($"{1..(WorldBuild.LEVELS_COUNT + 1)}"))
 						saveFile.LastLevelEntered = newScene;
 				AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Single);
@@ -37,7 +35,7 @@ namespace GwambaPrimeAdventure.Connection
 					while (!asyncOperation.isDone)
 						yield return transicionHud.LoadingBar.value = asyncOperation.progress * 100F;
 				}
-				_block = !(asyncOperation.allowSceneActivation = true);
+				asyncOperation.allowSceneActivation = true;
 			}
 		}
 	};
